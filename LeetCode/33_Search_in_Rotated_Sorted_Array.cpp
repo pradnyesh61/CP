d40f5db57c02ba87d792ca94d761b66184cde398
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2022-12-31 22:00:53 +0530

    daily CP

---
+++ LeetCode/33_Search_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+        for(int i = 0; i < n ; i++)
+        {
+            if(nums[i] == target)
+                return i;
+        }
+
+        return -1;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> nums = {4,5,6,7,0,1,2};
+    int target = 0;
+    cout<<s.search(nums,target);
+}

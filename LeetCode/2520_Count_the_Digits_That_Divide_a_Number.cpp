5f8f78b66cb40ecef3d3d465838bd0222f5ee706
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-01 13:16:41 +0530

    weekly contest 326 jan 1 2023

--- LeetCode/2520_Count_the_Digits_That_Divide_a_Number.cpp
+++ LeetCode/2520_Count_the_Digits_That_Divide_a_Number.cpp
@@ -1,6 +1,9 @@
 // Given an integer num, return the number of digits in num that divide num.
 // An integer val divides nums if nums % val == 0.
 
+
+// Weekly Contest 326 1st problem jan 1 2023
+
 #include<bits/stdc++.h>
 using namespace std;
 

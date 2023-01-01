6b83b9f4bb0a368b4421b3a2d6ad23ebde873c03
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-01 13:08:57 +0530

    Weekly Contest 326 1st problem jan 1 2023

---
+++ LeetCode/2520_Count_the_Digits_That_Divide_a_Number.cpp
@@ -0,0 +1,34 @@
+// Given an integer num, return the number of digits in num that divide num.
+
+// An integer val divides nums if nums % val == 0.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countDigits(int num) {
+        
+        int count = 0 ;
+        int n = num;
+         
+        while(num)
+        {
+            int rem = num%10;
+            if(n%rem == 0)
+            {
+                count++;
+            }
+            num = num/10;
+        }
+        
+        return count;
+        
+    }
+};
+
+int main()
+{
+    Solution s;
+    cout<<s.countDigits(121);
+}

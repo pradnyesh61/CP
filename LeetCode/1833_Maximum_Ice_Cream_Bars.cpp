b9f2b64876541e233a813e2a5d8a386b8f38b68c
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-06 09:02:05 +0530

    6 jan 2023

--- LeetCode/1833_Maximum_Ice_Cream_Bars.cpp
+++ LeetCode/1833_Maximum_Ice_Cream_Bars.cpp
@@ -1,4 +1,9 @@
-//5 jan 2023
+// It is a sweltering summer day, and a boy wants to buy some ice cream bars.
+// At the store, there are n ice cream bars. You are given an array costs of length n, where costs[i] is the price of the ith ice cream bar in coins. The boy initially has coins coins to spend, and he wants to buy as many ice cream bars as possible. 
+// Return the maximum number of ice cream bars the boy can buy with coins coins.
+// Note: The boy can buy the ice cream bars in any order.
+
+//6 jan 2023
 
 #include<bits/stdc++.h>
 using namespace std;

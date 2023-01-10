f1c00fd04d9e5b7485957315fd742ae310ca312f
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-10 11:50:26 +0530

    10 jan 2023

---
+++ LeetCode/100_Same_Tree.cpp
@@ -0,0 +1,49 @@
+// 10 jan 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+  {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+  class Solution
+  {
+  public:
+      /*
+      bool isSameTree(TreeNode* p, TreeNode* q) {
+
+
+          if( (p == NULL && q != NULL) || (p != NULL && q == NULL) )
+          {
+              return false;
+          }
+
+  if(p == NULL && q == NULL ||((p->val == q->val ) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right)))
+          {
+                 return true;
+          }
+
+         return false;
+
+
+      }
+      */
+
+      bool isSameTree(TreeNode *p, TreeNode *q)
+      {
+
+          if (p == NULL || q == NULL)
+          {
+              return p == q;
+          }
+
+          return (p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+      }
+  };

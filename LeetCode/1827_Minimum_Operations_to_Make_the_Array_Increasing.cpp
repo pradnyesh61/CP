7f32824d2da8cfca1654c2b46f40793f25aa28e5
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-11 22:09:36 +0530

    11 Jan 2023

---
+++ LeetCode/1827_Minimum_Operations_to_Make_the_Array_Increasing.cpp
@@ -0,0 +1,33 @@
+
+// 11 jan 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        
+        int count = 0;
+
+        for(int i = 0 ; i <nums.size()-1 ;i++)
+        {
+           if(nums[i] >= nums[i+1])
+            {
+                int t = (nums[i]-nums[i+1])+1;
+                nums[i+1] += t;
+                count += t;
+            }
+        }
+
+        return count;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> v ={1,5,2,4,1};
+    cout<<s.minOperations(v);
+}

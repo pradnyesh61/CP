8375175d72a8dfe77de07d6e5515787190f60321
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-12 21:28:06 +0530

    12 Jan 2023

---
+++ LeetCode/561_Array_Partition.cpp
@@ -0,0 +1,32 @@
+// Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn)
+// such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+
+// 12 jan 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end());
+
+        int sum = 0;
+
+        for(int i = 0 ; i < nums.size() ;i += 2)
+        {
+            sum += min(nums[i],nums[i+1]);
+        }
+
+        return sum;
+
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> v = {6,2,6,5,1,2};
+    cout<<s.arrayPairSum(v);
+}

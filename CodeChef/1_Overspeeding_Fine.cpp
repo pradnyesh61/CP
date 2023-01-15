ea8bfabb315e2028cc1c36dfb86ec84038540876
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-15 22:03:01 +0530

    Jan Long Challenge

---
+++ CodeChef/1_Overspeeding_Fine.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    
+	    if(x <= 70)
+	    {
+	        cout<<0<<endl;
+	    }else if(x <= 100)
+	    {
+	        cout<<500<<endl;
+	    }
+	    else{
+	        cout<<2000<<endl;
+	    }
+	}
+	return 0;
+}

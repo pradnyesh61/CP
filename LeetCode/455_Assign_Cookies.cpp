95726d7519106018692cb1e6d50e6fdd08ee2c82
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-17 19:08:28 +0530

    17 Jan 2023

---
+++ LeetCode/455_Assign_Cookies.cpp
@@ -0,0 +1,23 @@
+//17 jan 2023
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+
+        int i = 0 ,j = 0;
+
+        while( i < g.size() && j < s.size() )
+        {
+            if(s[j] >= g[i])
+                i++;
+            j++;
+        }
+
+        return i ;
+    }
+};

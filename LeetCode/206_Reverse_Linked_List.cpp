3dfc7475084e62ac0919d4a02ac07b271fb400b7
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-19 18:59:20 +0530

    19 Jan 2023

--- LeetCode/206_Reverse_Linked_List.cpp
+++ LeetCode/206_Reverse_Linked_List.cpp
@@ -3,44 +3,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-   struct ListNode {
-      int val;
-      ListNode *next;
-      ListNode() : val(0), next(nullptr) {}
-      ListNode(int x) : val(x), next(nullptr) {}
-      ListNode(int x, ListNode *next) : val(x), next(next) {}
-  };
- 
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
-class Solution {
+class Solution
+{
 public:
+    vector<int> v;
 
-vector<int> v;
+    ListNode *reverseList(ListNode *head)
+    {
 
-    ListNode* reverseList(ListNode* head) {
-
-        if(head == NULL)
+        if (head == NULL)
         {
             return head;
         }
 
-        if(head->next == NULL )
+        if (head->next == NULL)
         {
             v.push_back(head->val);
             head->val = v[0];
-          
+
             return head;
         }
 
         v.push_back(head->val);
 
         reverseList(head->next);
-      
-         v.erase(v.begin());
+
+        v.erase(v.begin());
         head->val = v[0];
-       
 
         return head;
-
     }
 };

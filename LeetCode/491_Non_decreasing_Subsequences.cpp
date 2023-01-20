3b8a57aeacf8186b540a70a67becfda97ff093d6
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-20 23:25:18 +0530

    20 Jan 2023

--- LeetCode/491_Non_decreasing_Subsequences.cpp
+++ LeetCode/491_Non_decreasing_Subsequences.cpp
@@ -1,15 +1,64 @@
-// First method will work but gives TLE 
+// First method will work but gives TLE
 // In second method TLE is not coming
 // we can use set<vector<int>> for distinct sequence
 // and while returning we need to return 2D vector
-// so for that we can just convert / TypeCast the set<vector<int> 
+// so for that we can just convert / TypeCast the set<vector<int>
 // check the return type
 
 // 20 Jan 2023
-
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution
+{
+public:
+// THis is using address of vectors so that space use is less
+    void nds(int i, int n, vector<int> &nums, set<vector<int>> &ans, vector<int> &temp)
+    {
+        if (i >= n)
+        {
+            if (temp.size() >= 2)
+            {
+                ans.insert(temp);
+            }
+            return;
+        }
+
+        if (i < n)
+        {
+            if (temp.size() > 0)
+            {
+                if (temp[temp.size() - 1] <= nums[i])
+                {
+                    temp.push_back(nums[i]);
+                    nds(i + 1, n, nums, ans, temp);
+                    temp.pop_back();
+                }
+            }
+            else
+            {
+                temp.push_back(nums[i]);
+                nds(i + 1, n, nums, ans, temp);
+                temp.pop_back();
+            }
+        }
+
+        nds(i + 1, n, nums, ans, temp);
+    }
+
+    vector<vector<int>> findSubsequences(vector<int> &nums)
+    {
+
+        vector<int> temp;
+        set<vector<int>> ans;
+
+        nds(0, nums.size(), nums, ans, temp);
+
+        return vector(ans.begin(), ans.end());
+    }
+};
+
+//---------------------------------------------------------------------------------------------------------
 class Solution
 {
 public:

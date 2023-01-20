25b2c85b4f38f0aaac6b2534b723043d146dff5b
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-20 23:14:46 +0530

    20 Jan 2023

---
+++ LeetCode/491_Non_decreasing_Subsequences.cpp
@@ -0,0 +1,82 @@
+// 20 Jan 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    /*    vector<vector<int>> v2;
+        void nds( int i ,vector<int>& nums,int n,vector<int> v)
+        {
+            if(i >= n )
+            {
+                if(v.size()>=2){
+               for(int i = 0; i < v.size()-1; i++)
+               {
+                   if(v[i]>v[i+1])
+                   {
+                       return ;
+                   }
+               }
+                if(find(v2.begin(),v2.end(),v) != v2.end())
+                {
+                    return ;
+                }
+                v2.push_back(v);
+                }
+                return;
+            }
+
+            v.push_back(nums[i]);
+            nds(i+1,nums,n,v);
+            v.pop_back();
+            nds(i+1,nums,n,v);
+
+        }
+    */
+    set<vector<int>> v2;
+    void nds(int i, vector<int> &nums, int n, vector<int> v)
+    {
+        if (i >= n)
+        {
+            if (v.size() >= 2)
+            {
+
+                v2.insert(v);
+            }
+            return;
+        }
+
+        if (i < n)
+        {
+            if (v.size() > 0)
+            {
+                if (v[v.size() - 1] <= nums[i])
+                {
+                    v.push_back(nums[i]);
+                    nds(i + 1, nums, n, v);
+                    v.pop_back();
+                }
+            }
+            else
+            {
+                v.push_back(nums[i]);
+                nds(i + 1, nums, n, v);
+                v.pop_back();
+            }
+        }
+
+        nds(i + 1, nums, n, v);
+    }
+
+    vector<vector<int>> findSubsequences(vector<int> &nums)
+    {
+
+        vector<int> v1;
+
+        nds(0, nums, nums.size(), v1);
+
+        return vector(v2.begin(), v2.end());
+    }
+};

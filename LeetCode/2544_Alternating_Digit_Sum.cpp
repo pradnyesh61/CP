4b10841e8ca1c9cae01a1bc8270c61884271da0c
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-22 22:25:56 +0530

    22 Jan 2023

--- LeetCode/2544_Alternating_Digit_Sum.cpp
+++ LeetCode/2544_Alternating_Digit_Sum.cpp
@@ -1,32 +1,34 @@
 // 22 Jan 2023
-//Weekly Contest 329
+// Weekly Contest 329
 
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int alternateDigitSum(int n) {
+    int alternateDigitSum(int n)
+    {
         int sum = 0;
 
         string s = to_string(n);
 
         bool t = true;
 
-        for(int i = 0 ; i < s.length() ; i++)
+        for (int i = 0; i < s.length(); i++)
         {
-            if(t){
-            sum += s[i]-'0';   
-            t = false;
+            if (t)
+            {
+                sum += s[i] - '0';
+                t = false;
             }
             else
             {
-            sum -= s[i]-'0';
-            t = true;
+                sum -= s[i] - '0';
+                t = true;
             }
         }
 
         return sum;
-
     }
 };

2658d9f6002dee334dda365fc07f93b0d1713eeb
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-22 22:17:21 +0530

    22 Jan 2023

---
+++ LeetCode/1863_Sum_of_All_Subset_XOR_Totals.cpp
@@ -0,0 +1,26 @@
+// 22 Jan 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+
+    int subset(int i , int n , vector<int> &nums,int axor)
+    {
+        if(i >= n)
+        { 
+            return axor;
+        }
+       
+        axor ^= nums[i];
+       return  subset(i+1,n,nums,axor) + subset(i+1,n,nums,axor^nums[i]);
+        
+    }
+
+    int subsetXORSum(vector<int>& nums) {
+        
+        return subset(0,nums.size(),nums,0);
+    }
+};

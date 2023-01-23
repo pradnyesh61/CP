142d2e3ee3d11d1ae1ae2bce2323cc6b04a0b8b6
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-23 22:26:21 +0530

    23 Jan 2023

---
+++ LeetCode/77_Combinations.cpp
@@ -0,0 +1,35 @@
+// 23 Jan 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    void comb(int i , int n ,vector<int> &temp , set<vector<int>> &ans , int &k)
+    {
+        if(i > n)
+        {
+            if(temp.size() == k)
+            {
+                ans.insert(temp);
+            }
+
+            return;
+        }
+
+        temp.push_back(i);
+        comb(i+1,n,temp,ans,k);
+        temp.pop_back();
+        comb(i+1,n,temp,ans,k);
+
+
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        set<vector<int>> ans;
+        vector<int> temp;    
+        comb(1,n,temp,ans,k);
+        return vector(ans.begin(),ans.end());
+    }
+};

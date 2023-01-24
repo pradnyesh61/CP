126aac8e2ec72a505a6f2ca4fab43a25b067285a
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-24 19:17:58 +0530

    24 Jan 2023

--- LeetCode/78_Subsets.cpp
+++ LeetCode/78_Subsets.cpp
@@ -3,30 +3,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-
-    void subset(int i , int n ,vector<int> &nums,set<vector<int>> &ans,vector<int> temp)
+    void subset(int i, int n, vector<int> &nums, set<vector<int>> &ans, vector<int> temp)
     {
-        if(i >= n)
+        if (i >= n)
         {
             ans.insert(temp);
             return;
         }
 
         temp.push_back(nums[i]);
-        subset(i+1,n,nums,ans,temp);
+        subset(i + 1, n, nums, ans, temp);
         temp.pop_back();
-        subset(i+1,n,nums,ans,temp);
+        subset(i + 1, n, nums, ans, temp);
     }
 
-    vector<vector<int>> subsets(vector<int>& nums) {
-        
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+
         vector<int> temp;
         set<vector<int>> ans;
 
-        subset(0,nums.size(),nums,ans,temp);
+        subset(0, nums.size(), nums, ans, temp);
 
-        return vector(ans.begin(),ans.end());
+        return vector(ans.begin(), ans.end());
     }
 };

3ab0a5560e1cbc44e394713df6bddd87727fb91c
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-25 16:26:28 +0530

    25 Jan 2023

--- LeetCode/90_Subsets_II.cpp
+++ LeetCode/90_Subsets_II.cpp
@@ -3,31 +3,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-
-    void subsets(int i , int n , vector<int> &nums, vector<int> &temp , set<vector<int>> &ans)
+    void subsets(int i, int n, vector<int> &nums, vector<int> &temp, set<vector<int>> &ans)
     {
-        if( i >= n)
+        if (i >= n)
         {
             ans.insert(temp);
-            return ;
+            return;
         }
 
         temp.push_back(nums[i]);
-        subsets(i+1,n,nums,temp,ans);
+        subsets(i + 1, n, nums, temp, ans);
         temp.pop_back();
-        subsets(i+1,n,nums,temp,ans);
+        subsets(i + 1, n, nums, temp, ans);
     }
 
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        
-        sort(nums.begin(),nums.end());
+    vector<vector<int>> subsetsWithDup(vector<int> &nums)
+    {
+
+        sort(nums.begin(), nums.end());
         set<vector<int>> ans;
         vector<int> temp;
 
-        subsets(0,nums.size(),nums,temp,ans);
-        return vector(ans.begin(),ans.end());
+        subsets(0, nums.size(), nums, temp, ans);
+        return vector(ans.begin(), ans.end());
     }
 };

cec43e96ed5b7a4bb4354aa70c171c9fa2454975
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-26 22:20:51 +0530

    26 Jan 2023

--- LeetCode/216_Combination_Sum_III.cpp
+++ LeetCode/216_Combination_Sum_III.cpp
@@ -3,15 +3,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-
-    void subset(int i , int last,vector<int> &temp, set<vector<int>> &ans,int sum,int k,int n)
+    void subset(int i, int last, vector<int> &temp, set<vector<int>> &ans, int sum, int k, int n)
     {
-        if(i > last)
+        if (i > last)
         {
-            if(temp.size() == k && sum == n)
+            if (temp.size() == k && sum == n)
             {
                 ans.insert(temp);
             }
@@ -20,21 +19,20 @@ public:
 
         temp.push_back(i);
         sum += i;
-        subset(i+1,last,temp,ans,sum,k,n);
+        subset(i + 1, last, temp, ans, sum, k, n);
         temp.pop_back();
-        sum-= i;
-        subset(i+1,last,temp,ans,sum,k,n);
-        
-
+        sum -= i;
+        subset(i + 1, last, temp, ans, sum, k, n);
     }
 
-    vector<vector<int>> combinationSum3(int k, int n) {
-        
+    vector<vector<int>> combinationSum3(int k, int n)
+    {
+
         set<vector<int>> ans;
         vector<int> temp;
 
-        subset(1,9,temp,ans,0,k,n);
+        subset(1, 9, temp, ans, 0, k, n);
 
-        return vector(ans.begin(),ans.end());
+        return vector(ans.begin(), ans.end());
     }
 };

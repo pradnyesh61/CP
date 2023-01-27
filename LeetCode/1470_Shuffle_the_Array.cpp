64e63590e1b5df9fa9eb0f7b0dea8a295b3a3049
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-27 22:33:46 +0530

    27 Jan 2023

--- LeetCode/1470_Shuffle_the_Array.cpp
+++ LeetCode/1470_Shuffle_the_Array.cpp
@@ -1,18 +1,19 @@
-
 // 27 Jan 2023
 
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> shuffle(vector<int>& nums, int n) {
+    vector<int> shuffle(vector<int> &nums, int n)
+    {
         vector<int> v;
 
-        for(int i = 0 ; i < n;i++)
+        for (int i = 0; i < n; i++)
         {
             v.push_back(nums[i]);
-            v.push_back(nums[i+n]);
+            v.push_back(nums[i + n]);
         }
 
         return v;

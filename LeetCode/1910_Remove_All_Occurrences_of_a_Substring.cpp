9123c7cccba5954f76b0d08720144621a30f690d
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-28 20:43:17 +0530

    28 Jan  2023

--- LeetCode/1910_Remove_All_Occurrences_of_a_Substring.cpp
+++ LeetCode/1910_Remove_All_Occurrences_of_a_Substring.cpp
@@ -3,7 +3,7 @@ Remove the string part from string s
 
 so first of need to check whether the first character of part string is equal to string s
 if yes then only I will check the further for remaining characters of part string
-    because if first character does not match then we dont need to go through whole string 
+    because if first character does not match then we dont need to go through whole string
 Now when the first letter matches the we need to assign the index value to new variable of string s ie. -> i
 why ?
 because we need to check the next characters are matching/equal to part string's characters
@@ -14,21 +14,20 @@ if it does not match then simply break from the loop because we wont be getting 
 now check if the j is having the same value as part length
 if it does , then I can say that we have traversed through the part string up till end
 and if we have pass through the end then it means we have a substring in s matching part string
-so I will just erase that substring 
+so I will just erase that substring
 here when we use erase function for string
-we will have to give 2 parameters 
+we will have to give 2 parameters
 first parameter  - starting index
-second parameter - how many character's 
+second parameter - how many character's
 
 put this in mind that the second parameter in string regarding erase function its count of character not
 the ending index -1  position
 
-and I will check from the starting index again because there is possibility the charaters before string index 
-we will get new substring matching / equals to part string 
+and I will check from the starting index again because there is possibility the charaters before string index
+we will get new substring matching / equals to part string
 
 
 */
-
 
 // 28 Jan 2023
 

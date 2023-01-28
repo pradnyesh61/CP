0dde58f42717991e04491622be70289d6d811f28
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-28 20:42:46 +0530

    28 Jan  2023

--- LeetCode/1910_Remove_All_Occurrences_of_a_Substring.cpp
+++ LeetCode/1910_Remove_All_Occurrences_of_a_Substring.cpp
@@ -1,3 +1,34 @@
+/*
+Remove the string part from string s
+
+so first of need to check whether the first character of part string is equal to string s
+if yes then only I will check the further for remaining characters of part string
+    because if first character does not match then we dont need to go through whole string 
+Now when the first letter matches the we need to assign the index value to new variable of string s ie. -> i
+why ?
+because we need to check the next characters are matching/equal to part string's characters
+if dont use the same variable i will be incremented and we want to remove / erase the substring from s
+so we wont be having the starting index ie. why i will be our starting index if the substring finds in s equal to part string
+now increment k and j as the characters match
+if it does not match then simply break from the loop because we wont be getting the substring s as part
+now check if the j is having the same value as part length
+if it does , then I can say that we have traversed through the part string up till end
+and if we have pass through the end then it means we have a substring in s matching part string
+so I will just erase that substring 
+here when we use erase function for string
+we will have to give 2 parameters 
+first parameter  - starting index
+second parameter - how many character's 
+
+put this in mind that the second parameter in string regarding erase function its count of character not
+the ending index -1  position
+
+and I will check from the starting index again because there is possibility the charaters before string index 
+we will get new substring matching / equals to part string 
+
+
+*/
+
 
 // 28 Jan 2023
 

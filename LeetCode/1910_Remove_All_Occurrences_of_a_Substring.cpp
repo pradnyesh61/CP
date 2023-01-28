bd8807431238fc057e8219e650de3639eeced0ee
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-28 20:29:47 +0530

    28 Jan  2023

---
+++ LeetCode/1910_Remove_All_Occurrences_of_a_Substring.cpp
@@ -0,0 +1,38 @@
+
+// 28 Jan 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        
+        for(int i = 0 ; i < s.length();i++)
+        {
+           
+            int j = 0;
+            if(s[i] == part[j])
+            {   int k = i;
+                while(j < part.length())
+                {
+                    
+                    if(s[k] == part[j])
+                    {
+                        k++;
+                        j++;
+                    }
+                    else{
+                        break;
+                    }
+                }
+                if(j == part.length())
+                {
+                    s.erase(i,j);
+                    i=-1;
+                }
+            }
+        }
+        return s;
+    }
+};

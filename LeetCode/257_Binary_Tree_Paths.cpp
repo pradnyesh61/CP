c508e5373825f84ae7362ba46d99c2911f7e3d6e
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-29 09:51:01 +0530

    29 Jan 2023

--- LeetCode/257_Binary_Tree_Paths.cpp
+++ LeetCode/257_Binary_Tree_Paths.cpp
@@ -4,68 +4,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
 public:
-    
-    void backtrack(TreeNode* root, vector<string> &v,string &s)
+    void backtrack(TreeNode *root, vector<string> &v, string &s)
     {
-       
-        if(root->left == NULL && root->right ==NULL)
+
+        if (root->left == NULL && root->right == NULL)
         {
             v.push_back(s);
             return;
         }
 
-        if(root->left != NULL)
+        if (root->left != NULL)
         {
             string sl = to_string(root->left->val);
-            s+= "->"+sl;
-            backtrack(root->left,v,s);
-           int n = sl.length()+2;
-            while(n--)
+            s += "->" + sl;
+            backtrack(root->left, v, s);
+            int n = sl.length() + 2;
+            while (n--)
             {
                 s.pop_back();
             }
         }
-        
-        if(root->right != NULL)
+
+        if (root->right != NULL)
         {
             string sl = to_string(root->right->val);
-            s+= "->"+sl;
-            backtrack(root->right,v,s);
-            int n = sl.length()+2;
-            while(n--)
+            s += "->" + sl;
+            backtrack(root->right, v, s);
+            int n = sl.length() + 2;
+            while (n--)
             {
                 s.pop_back();
             }
         }
-      
-           return; 
+
+        return;
     }
-    
-    vector<string> binaryTreePaths(TreeNode* root) {
-        
+
+    vector<string> binaryTreePaths(TreeNode *root)
+    {
+
         vector<string> v;
-        
-        if(root == NULL)
+
+        if (root == NULL)
         {
             return v;
         }
-        
+
         string s;
         s = to_string(root->val);
-        backtrack(root,v,s);
+        backtrack(root, v, s);
 
         return v;
     }

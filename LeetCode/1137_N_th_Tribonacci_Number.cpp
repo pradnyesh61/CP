7470f3eda7d8991ccc4cce361dc4aa56c9580523
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-01-30 08:47:03 +0530

    30 Jan 2023

---
+++ LeetCode/1137_N_th_Tribonacci_Number.cpp
@@ -0,0 +1,33 @@
+// 30 Jan 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        int t0 = 0;
+        int t1 = 1;
+        int t2 = 1;
+        int sum = 0;
+        
+        if(n < 1)
+        {
+            return 0;
+        }
+        if(n <3)
+        {
+            return 1;
+        }
+
+        for(int i= 3;i <= n;i++)
+        {
+            sum = t0+t1+t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = sum;
+        }
+
+        return t2;
+    }
+};

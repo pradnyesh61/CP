e37dfb778359ac27ec71b8696084207ecbd3dbba
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-02-01 23:11:46 +0530

    1 Feb 2023

--- LeetCode/1071_Greatest_Common_Divisor_of_Strings.cpp
+++ LeetCode/1071_Greatest_Common_Divisor_of_Strings.cpp
@@ -3,11 +3,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    string gcdOfStrings(string str1, string str2) {
+    string gcdOfStrings(string str1, string str2)
+    {
 
-       return (str1 + str2 == str2 + str1)? 
-        str1.substr(0, gcd(size(str1),size(str2))): "";
+        return (str1 + str2 == str2 + str1) ? str1.substr(0, gcd(size(str1), size(str2))) : "";
     }
 };

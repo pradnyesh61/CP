4fdc01661fb6e9989563114db3c7f56fa0c4fc36
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-02-05 15:03:45 +0530

    5 Feb 2023

--- LeetCode/2558_Take_Gifts_From_the_Richest_Pile.cpp
+++ LeetCode/2558_Take_Gifts_From_the_Richest_Pile.cpp
@@ -3,29 +3,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-    long long pickGifts(vector<int>& gifts, int k) {
-        
-         multiset<int,greater<int>> s(gifts.begin(), gifts.end());
-        
-        
-        for(int i = 1; i <= k ; i++)
+    long long pickGifts(vector<int> &gifts, int k)
+    {
+
+        multiset<int, greater<int>> s(gifts.begin(), gifts.end());
+
+        for (int i = 1; i <= k; i++)
         {
             int temp = *s.begin();
             s.erase(s.begin());
-            
+
             s.insert(floor(sqrt(temp)));
         }
-        
+
         long sum = 0;
-        
-        for(auto it : s)
+
+        for (auto it : s)
         {
             sum += it;
         }
-        
+
         return sum;
     }
 };

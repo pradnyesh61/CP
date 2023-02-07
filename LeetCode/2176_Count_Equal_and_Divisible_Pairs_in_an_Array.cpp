c9d12ff771986fd3a18684e26e97e353119b7516
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-02-07 23:40:27 +0530

    7 Feb 2023

--- LeetCode/2176_Count_Equal_and_Divisible_Pairs_in_an_Array.cpp
+++ LeetCode/2176_Count_Equal_and_Divisible_Pairs_in_an_Array.cpp
@@ -3,18 +3,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int countPairs(vector<int>& nums, int k) {
-        
-       
+    int countPairs(vector<int> &nums, int k)
+    {
+
         int n = nums.size();
         int count = 0;
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
-            for(int j = i+1 ; j < n ; j++)
+            for (int j = i + 1; j < n; j++)
             {
-                if(nums[i] == nums[j] && (i*j)%k == 0)
+                if (nums[i] == nums[j] && (i * j) % k == 0)
                 {
                     count++;
                 }

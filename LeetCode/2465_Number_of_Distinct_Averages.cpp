98daf8ae84e73fce8583dca7a19b80601eb7b58d
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-02-08 22:09:41 +0530

    8 Feb 2023

--- LeetCode/2465_Number_of_Distinct_Averages.cpp
+++ LeetCode/2465_Number_of_Distinct_Averages.cpp
@@ -3,20 +3,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-    int distinctAverages(vector<int>& nums) {
-        
-        sort(nums.begin(),nums.end());
+    int distinctAverages(vector<int> &nums)
+    {
+
+        sort(nums.begin(), nums.end());
 
         set<float> s;
 
         int n = nums.size();
 
-        for(int  i =0  ; i < n/2; i++)
+        for (int i = 0; i < n / 2; i++)
         {
-            s.insert(float(nums[i]+nums[n-i-1])/2);
+            s.insert(float(nums[i] + nums[n - i - 1]) / 2);
         }
 
         return s.size();

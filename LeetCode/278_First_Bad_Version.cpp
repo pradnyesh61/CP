378301430e053d0ba93edc0807958dff77d4338b
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-02-10 23:21:47 +0530

    10 Feb 2023

--- LeetCode/278_First_Bad_Version.cpp
+++ LeetCode/278_First_Bad_Version.cpp
@@ -6,14 +6,19 @@ using namespace std;
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
 
-class Solution {
+class Solution
+{
 public:
-    int firstBadVersion(int n) {
-      int start = 1, end = n, mid;
-        while(start < end) {
+    int firstBadVersion(int n)
+    {
+        int start = 1, end = n, mid;
+        while (start < end)
+        {
             mid = start + (end - start) / 2;
-            if(!isBadVersion(mid)) start = mid + 1;   
-            else end = mid;
+            if (!isBadVersion(mid))
+                start = mid + 1;
+            else
+                end = mid;
         }
         return start;
     }

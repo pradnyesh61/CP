c92c6318fc3e0e415dbeeee6fb80b19a41e5a2c4
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-02-11 22:02:55 +0530

    11 Feb 2023

---
+++ LeetCode/2273_Find_Resultant_Array_After_Removing_Anagrams.cpp
@@ -0,0 +1,36 @@
+// 11 Feb 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> removeAnagrams(vector<string>& words) {
+        
+        vector<string> v;
+        int n = words.size();
+        
+        for(int i = 0 ; i < n; i++)
+        {
+            string s = words[i];
+            sort(s.begin(),s.end());
+           v.push_back(s);
+        }
+
+        vector<string> q;
+        q.push_back(words[0]);
+        
+        for(int  i = 0 ; i < n-1;i++)
+        {
+           if(v[i] != v[i+1])
+           {        
+               q.push_back(words[i+1]);      
+           }
+
+          
+        }
+
+
+        return q;
+    }
+};

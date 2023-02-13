998cd106caaed30dc635bce6fe1ac180892fc573
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-02-13 11:52:52 +0530

    13 Feb 2023

--- LeetCode/1523_Count_Odd_Numbers_in_an_Interval_Range.cpp
+++ LeetCode/1523_Count_Odd_Numbers_in_an_Interval_Range.cpp
@@ -4,25 +4,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-  /*  int countOdds(int low, int high) {
-        int count  = 0;
-        if(low %2 == 0)
-            low += 1;
-        
-        for(int i = low ; i <= high ; i += 2)
-            ++count;
-        
-        return count;
+    /*  int countOdds(int low, int high) {
+          int count  = 0;
+          if(low %2 == 0)
+              low += 1;
+
+          for(int i = low ; i <= high ; i += 2)
+              ++count;
+
+          return count;
+      }
+
+      */
+
+    int countOdds(int low, int high)
+    {
+        int res = (high - low) / 2;
+        if (low % 2 != 0 || high % 2 != 0)
+            res++;
+        return res;
     }
-
-    */
-
-    int countOdds(int low, int high) {
-    int res = (high-low)/2;
-    if(low %2 != 0 || high%2 != 0)
-        res++;
-    return res;
-}
 };

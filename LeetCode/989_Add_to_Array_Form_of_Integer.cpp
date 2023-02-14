391e07c253a85c72e17945657fb31501268ff70b
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-02-14 17:33:02 +0530

    14 Feb 2023

---
+++ LeetCode/989_Add_to_Array_Form_of_Integer.cpp
@@ -0,0 +1,40 @@
+
+
+// 10 jan 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        int carry = 0;
+        string s = to_string(k);
+        int j = s.length()-1;
+        int i = num.size()-1;
+        while(i >= 0 || j >= 0)
+        {
+            if(j >= 0)
+            {
+                carry += (s[j]-'0');
+                j--;
+            }
+            if(i >= 0)
+            {
+                carry += num[i];
+                num[i] = carry%10;
+                i--;
+            }
+            else{
+                 num.insert(num.begin(),carry%10);
+            }  
+            carry /= 10;
+        }
+        while(carry)
+        {
+            num.insert(num.begin(),carry%10);
+            carry /= 10;
+        }
+        return num;
+    }
+};

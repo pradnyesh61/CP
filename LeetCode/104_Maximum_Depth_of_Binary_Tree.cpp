36fef57cc447dbba8f9d0c153756b7bd709d04aa
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-02-16 18:54:53 +0530

    16 Feb 2023

--- LeetCode/104_Maximum_Depth_of_Binary_Tree.cpp
+++ LeetCode/104_Maximum_Depth_of_Binary_Tree.cpp
@@ -4,49 +4,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-  struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode() : val(0), left(nullptr), right(nullptr) {}
-      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-  };
- 
-class Solution {
- private:
-  int count = 0;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+private:
+    int count = 0;
 
 public:
+    queue<TreeNode *> q;
 
-    queue<TreeNode*> q;
-    
-   
-    int maxDepth(TreeNode* root) {
-        if(root == NULL)
+    int maxDepth(TreeNode *root)
+    {
+        if (root == NULL)
         {
             return 0;
         }
         q.push(root);
         q.push(NULL);
-        
-        while(!q.empty())
+
+        while (!q.empty())
         {
-            while(q.front()!= NULL)
+            while (q.front() != NULL)
             {
-                TreeNode* temp = q.front();
+                TreeNode *temp = q.front();
                 q.pop();
-                if(temp->left != NULL)
-                q.push(temp->left);
-                if(temp->right!= NULL)
-                q.push(temp->right);
+                if (temp->left != NULL)
+                    q.push(temp->left);
+                if (temp->right != NULL)
+                    q.push(temp->right);
             }
-            if(q.front() == NULL)
+            if (q.front() == NULL)
             {
                 q.pop();
                 count++;
-               if(!q.empty())
-                q.push(NULL);
+                if (!q.empty())
+                    q.push(NULL);
             }
         }
 

e4cd2ca5710b3c89cbc7c3cc656edefbae76f8d5
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-02-16 18:54:33 +0530

    16 Feb 2023

---
+++ LeetCode/104_Maximum_Depth_of_Binary_Tree.cpp
@@ -0,0 +1,55 @@
+
+// 14 Feb 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+ private:
+  int count = 0;
+
+public:
+
+    queue<TreeNode*> q;
+    
+   
+    int maxDepth(TreeNode* root) {
+        if(root == NULL)
+        {
+            return 0;
+        }
+        q.push(root);
+        q.push(NULL);
+        
+        while(!q.empty())
+        {
+            while(q.front()!= NULL)
+            {
+                TreeNode* temp = q.front();
+                q.pop();
+                if(temp->left != NULL)
+                q.push(temp->left);
+                if(temp->right!= NULL)
+                q.push(temp->right);
+            }
+            if(q.front() == NULL)
+            {
+                q.pop();
+                count++;
+               if(!q.empty())
+                q.push(NULL);
+            }
+        }
+
+        return count;
+    }
+};

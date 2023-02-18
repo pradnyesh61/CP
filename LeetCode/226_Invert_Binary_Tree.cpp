eef433b0f6575eaa46f45cf85c6a0fcc877d1d1c
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-02-18 11:04:58 +0530

    18 Feb 2023

--- LeetCode/226_Invert_Binary_Tree.cpp
+++ LeetCode/226_Invert_Binary_Tree.cpp
@@ -1,32 +1,33 @@
-
-
-// 17 Feb 2023
+// 18 Feb 2023
 
 #include <bits/stdc++.h>
 using namespace std;
 
-  struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode() : val(0), left(nullptr), right(nullptr) {}
-      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-  };
- 
-class Solution {
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
 public:
-    TreeNode* invertTree(TreeNode* root) {
-        
-        if(root == NULL)
+    TreeNode *invertTree(TreeNode *root)
+    {
+
+        if (root == NULL)
         {
             return root;
         }
-        TreeNode* temp = root->left;
+        TreeNode *temp = root->left;
         root->left = root->right;
         root->right = temp;
         invertTree(root->left);
         invertTree(root->right);
-      return root;
+        return root;
     }
 };

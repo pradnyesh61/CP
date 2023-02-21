2caa13bea32f594d52c69c7b205fc45f2ddc809f
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-02-21 12:27:25 +0530

    20 Feb 2023

--- LeetCode/540_Single_Element_in_a_Sorted_Array.cpp
+++ LeetCode/540_Single_Element_in_a_Sorted_Array.cpp
@@ -3,24 +3,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-        
+    int singleNonDuplicate(vector<int> &nums)
+    {
+
         int n = nums.size();
 
         int i = 0;
-        if(n == 1)
+        if (n == 1)
         {
             return nums[0];
-        } 
-        while(i < n)
+        }
+        while (i < n)
         {
-            if(nums[i] == nums[i+1])
+            if (nums[i] == nums[i + 1])
             {
                 i += 2;
             }
-            else{
+            else
+            {
                 break;
             }
         }

8336cb5969bd80e0069c290ac9b7648152d3a6e0
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-02-22 09:39:12 +0530

    21 Feb 2023

--- LeetCode/39_Combination_Sum.cpp
+++ LeetCode/39_Combination_Sum.cpp
@@ -3,34 +3,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
+    void combsum(int i, int target, int n, vector<int> &candidates, vector<vector<int>> &ans, vector<int> &v)
+    {
+        if (i == n)
+        {
+            if (target == 0)
+            {
+                ans.push_back(v);
+            }
+            return;
+        }
 
-   void combsum(int i , int target , int n, vector<int> &candidates, vector<vector<int>> &ans, vector<int> &v)
-   {
-       if(i == n)
-       {
-           if(target == 0)
-           {
-               ans.push_back(v);
-           }
-           return ;
-       }
+        if (candidates[i] <= target)
+        {
+            v.push_back(candidates[i]);
+            combsum(i, target - candidates[i], n, candidates, ans, v);
+            v.pop_back();
+        }
+        combsum(i + 1, target, n, candidates, ans, v);
+    }
 
-       if(candidates[i] <= target)
-       {
-        v.push_back(candidates[i]);
-        combsum(i,target-candidates[i], n,candidates,ans,v);
-        v.pop_back();
-       }
-       combsum(i+1,target,n,candidates,ans,v);
-   }
-
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+    {
         vector<vector<int>> ans;
         vector<int> v;
 
-        combsum(0,target,candidates.size(),candidates, ans,v);
+        combsum(0, target, candidates.size(), candidates, ans, v);
 
         return ans;
     }

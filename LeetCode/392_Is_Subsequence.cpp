0fd84b68dcdaf15b327f9c2e77c871a76248771a
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-02-23 22:03:12 +0530

    23 Feb 2023

--- LeetCode/392_Is_Subsequence.cpp
+++ LeetCode/392_Is_Subsequence.cpp
@@ -3,34 +3,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-
-    
-    bool isSubsequence(string s, string t) {
+    bool isSubsequence(string s, string t)
+    {
         int j = 0;
-        int i =0;
-       while(i < s.length())
+        int i = 0;
+        while (i < s.length())
         {
-            while(t[j] != s[i] && j < t.length())
+            while (t[j] != s[i] && j < t.length())
             {
                 j++;
             }
 
-            if(j == t.length())
-            {   if(i < s.length())
-                return false;
+            if (j == t.length())
+            {
+                if (i < s.length())
+                    return false;
             }
 
-            if(t[j] == s[i])
+            if (t[j] == s[i])
             {
                 i++;
                 j++;
             }
         }
 
-        if(i < s.length())
-        return false;
+        if (i < s.length())
+            return false;
 
         return true;
     }

dbd4d1470e556daf92c13fda284a83ac95c23b4a
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-02-23 22:02:34 +0530

    23 Feb 2023

---
+++ LeetCode/392_Is_Subsequence.cpp
@@ -0,0 +1,37 @@
+// 23 Feb 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    
+    bool isSubsequence(string s, string t) {
+        int j = 0;
+        int i =0;
+       while(i < s.length())
+        {
+            while(t[j] != s[i] && j < t.length())
+            {
+                j++;
+            }
+
+            if(j == t.length())
+            {   if(i < s.length())
+                return false;
+            }
+
+            if(t[j] == s[i])
+            {
+                i++;
+                j++;
+            }
+        }
+
+        if(i < s.length())
+        return false;
+
+        return true;
+    }
+};

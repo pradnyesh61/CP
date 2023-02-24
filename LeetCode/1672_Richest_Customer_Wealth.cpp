a0bb5d32835a7b36ecdeb5a5b207ed9f672dd339
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-02-24 22:51:41 +0530

    24 Feb 2023

---
+++ LeetCode/1672_Richest_Customer_Wealth.cpp
@@ -0,0 +1,26 @@
+// 24 Feb 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        
+        int max = INT_MIN;
+
+        for(int i = 0 ; i < accounts.size(); i++)
+        {
+            int sum = 0;
+            for(int j = 0 ; j < accounts[i].size() ;j++)
+            {
+                sum += accounts[i][j];
+            }
+
+            if(sum>max){
+                max = sum;
+            }
+        }
+        return max;
+    }
+};

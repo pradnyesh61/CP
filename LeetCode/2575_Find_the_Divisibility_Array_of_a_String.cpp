ee26df4065ac7e8fb85c4baf2ea2adc568595b58
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-02-27 21:59:59 +0530

    27 Feb 2023

--- LeetCode/2575_Find_the_Divisibility_Array_of_a_String.cpp
+++ LeetCode/2575_Find_the_Divisibility_Array_of_a_String.cpp
@@ -1,27 +1,29 @@
-// 25 Feb 2023
+// 27 Feb 2023
 
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
+    vector<int> divisibilityArray(string word, int m)
+    {
 
-    vector<int> divisibilityArray(string word, int m) {
-        
         vector<int> v;
         string s;
 
-        long long q =0 ;
-        for(int i = 0; i < word.size() ; i++)
+        long long q = 0;
+        for (int i = 0; i < word.size(); i++)
         {
             s += word[i];
-            q = (q * 10 + (word[i]-'0'))%m;
-         
-            if(q== 0)
+            q = (q * 10 + (word[i] - '0')) % m;
+
+            if (q == 0)
             {
                 v.push_back(1);
-            }else{
+            }
+            else
+            {
                 v.push_back(0);
             }
         }

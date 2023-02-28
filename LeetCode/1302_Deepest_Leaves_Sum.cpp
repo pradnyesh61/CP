fbdcdbd9383ba8b96fe7c89d2aa8adcb44798371
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-02-28 23:06:26 +0530

    28 Feb 2023

--- LeetCode/1302_Deepest_Leaves_Sum.cpp
+++ LeetCode/1302_Deepest_Leaves_Sum.cpp
@@ -1,61 +1,60 @@
-// 27 Feb 2023
+// 28 Feb 2023
 
 #include <bits/stdc++.h>
 using namespace std;
 
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
-  struct TreeNode {
-     int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode() : val(0), left(nullptr), right(nullptr) {}
-      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- };
- 
-class Solution {
+class Solution
+{
 public:
+    int deepestLeavesSum(TreeNode *root)
+    {
 
-
-    int deepestLeavesSum(TreeNode* root) {
-
-        if(root == NULL)
+        if (root == NULL)
         {
             return 0;
         }
 
         int sum = 0;
-        queue<TreeNode*> q;
+        queue<TreeNode *> q;
         q.push(root);
         q.push(NULL);
 
-        while(!q.empty())
+        while (!q.empty())
         {
-            while(!q.front() == NULL)
+            while (!q.front() == NULL)
             {
-                TreeNode* temp = q.front();
+                TreeNode *temp = q.front();
                 q.pop();
                 sum += temp->val;
 
-                if(temp->left != NULL) 
-                q.push(temp->left);
+                if (temp->left != NULL)
+                    q.push(temp->left);
 
-                if(temp->right != NULL)
-                q.push(temp->right);
+                if (temp->right != NULL)
+                    q.push(temp->right);
             }
 
-            if(q.front() == NULL)
+            if (q.front() == NULL)
             {
                 q.pop();
-                if(q.empty())
+                if (q.empty())
                 {
                     return sum;
                 }
-                
+
                 q.push(NULL);
                 sum = 0;
             }
-
         }
 
         return 0;

364a236390b6c9d42662616a97a9a81cbcc9282d
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-03-03 12:09:47 +0530

    3 March 2023

---
+++ LeetCode/28_Find_the_Index_of_the_First_Occurrence_in_a_String.cpp
@@ -0,0 +1,43 @@
+// 2 March 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        
+        if(haystack.length() < needle.length())
+        {
+            return -1;
+        }
+
+        char s = needle[0];
+
+        for(int i = 0 ; i< haystack.length() ; i++)
+        {
+            if(haystack[i] == s)
+            {
+                int k = i;
+                int j ;
+                for(j = 0 ; j < needle.length() && k < haystack.length() ; j++)
+                {
+                    if(needle[j] != haystack[k])
+                    {
+                        break;
+                    }
+                    k++;
+                }
+
+                if(j == needle.length())
+                {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+
+    }
+};

94a9cb2f500bac4474b9840d95bffcd826859e91
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-03-05 10:54:48 +0530

    5 March 2023

--- LeetCode/2583_Kth_Largest_Sum_in_a_Binary_Tree.cpp
+++ LeetCode/2583_Kth_Largest_Sum_in_a_Binary_Tree.cpp
@@ -3,69 +3,67 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-  struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode() : val(0), left(nullptr), right(nullptr) {}
-      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-  };
- 
-class Solution {
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
 public:
-    long long kthLargestLevelSum(TreeNode* root, int k) {
-        
-        if(root == NULL)
+    long long kthLargestLevelSum(TreeNode *root, int k)
+    {
+
+        if (root == NULL)
         {
             return 0;
         }
-        
+
         vector<long long> v;
-        
-        queue<TreeNode* > q;
+
+        queue<TreeNode *> q;
         q.push(root);
         q.push(NULL);
-        
-        while(!q.empty())
+
+        while (!q.empty())
         {
             long long sum = 0;
-            while(q.front() != NULL)
+            while (q.front() != NULL)
             {
                 sum += q.front()->val;
-                if(q.front()->left != NULL)
-                q.push(q.front()->left);
-                
-                if(q.front()->right != NULL)
-                q.push(q.front()->right);
-                
+                if (q.front()->left != NULL)
+                    q.push(q.front()->left);
+
+                if (q.front()->right != NULL)
+                    q.push(q.front()->right);
+
                 q.pop();
             }
-            
-            if(q.front() == NULL)
+
+            if (q.front() == NULL)
             {
                 q.pop();
-                 v.push_back(sum);
-                if(q.size() == 0)
-                break;
-                    q.push(NULL);
-                
-               
-                
+                v.push_back(sum);
+                if (q.size() == 0)
+                    break;
+                q.push(NULL);
             }
         }
-        
-        sort(v.begin(),v.end());
-        
-        for(auto it : v)
+
+        sort(v.begin(), v.end());
+
+        for (auto it : v)
         {
-            cout<<it<<" ";
+            cout << it << " ";
         }
-        
-        if(v.size() < k)
+
+        if (v.size() < k)
             return -1;
-        return v[v.size()-k];
-        
-        
+        return v[v.size() - k];
     }
 };

9bd59d62e603612e64d711b6738d93a4a7475e8a
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-03-06 09:22:37 +0530

    6 March 2023

---
+++ LeetCode/1539_Kth_Missing_Positive_Number.cpp
@@ -0,0 +1,39 @@
+// 6 March 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // int findKthPositive(vector<int>& arr, int k) {
+        
+    //     int i = 1, j =0;
+
+    //     while(k)
+    //     {
+    //         if(j<arr.size() && arr[j] == i)
+    //         {
+    //             i++;
+    //             j++;
+    //         }
+    //         else{
+    //             i++;
+    //             k--;
+    //         }
+    //     }
+
+    //     return i-1;
+    // }
+
+     int findKthPositive(vector<int>& arr, int k) {
+         for(auto it : arr)
+         {
+            if(it <= k)
+            {
+                k++;
+            }
+         }
+         return k;
+     }
+
+};

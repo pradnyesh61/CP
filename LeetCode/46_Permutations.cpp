f42c8fee79063731189195e189a60bcee6617a09
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-03-07 11:23:55 +0530

    7 March 2023

---
+++ LeetCode/46_Permutations.cpp
@@ -0,0 +1,33 @@
+// 7 March 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+
+    void per(int idx , int n , vector<int> &nums, vector<vector<int>> &ans)
+    {
+        if(idx == n)
+        {
+            ans.push_back(nums);
+            return ;
+        }
+
+        for(int i = idx ; i < n ; i++)
+        {
+            swap(nums[idx],nums[i]);
+            per(idx+1,n,nums,ans);
+            swap(nums[idx],nums[i]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        
+        vector<vector<int>> ans;
+        per(0,nums.size(),nums,ans);
+
+        return ans;
+    }
+};

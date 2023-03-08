edb292209feed1447e5717d2a0826dc224f3ab0c
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-03-08 10:01:12 +0530

    8 March 2023

---
+++ LeetCode/47_Permutations_II.cpp
@@ -0,0 +1,37 @@
+// 8 March 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    void per(int ind , int n , vector<int> &nums,set<vector<int>> &ans)
+    {
+        if(ind == n)
+        {
+            ans.insert(nums);
+            return ;
+        }
+
+        for(int i = ind ; i < n ; i++)
+        {
+            if(i != ind && nums[i-1] == nums[i])
+            {
+                continue;
+            }
+
+            swap(nums[ind],nums[i]);
+            per(ind+1,n,nums,ans);
+            swap(nums[ind],nums[i]);
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        
+        set<vector<int>> ans;
+        per(0,nums.size(),nums,ans);
+
+        return vector(ans.begin(),ans.end());
+    }
+};

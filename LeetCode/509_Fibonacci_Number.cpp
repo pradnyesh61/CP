828c00ac580eb3a4c58cbf57e2dc4862011cd903
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-03-09 09:24:22 +0530

    9 March 2023

--- LeetCode/509_Fibonacci_Number.cpp
+++ LeetCode/509_Fibonacci_Number.cpp
@@ -3,39 +3,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-    
-//     int sum = 0;
-   
-    
-//    int fib1(int n,int a,int b)
-//     {
-//         if(n == 2)
-//         {
-//             return sum;
-//         }
-//         sum = a+b;
-//        return fib1(--n,b,a+b);
-        
-//     }
-    
-//     int fib(int n) {
-//         if(n == 0)
-//         {
-//             return 0;
-//         }
-//         if(n == 1 || n == 2 )
-//         {
-//             return 1;
-//         }
-       
-//        return fib1(n,1,1);
-//     }
+
+//Brute Force
+    //     int sum = 0;
+
+    //    int fib1(int n,int a,int b)
+    //     {
+    //         if(n == 2)
+    //         {
+    //             return sum;
+    //         }
+    //         sum = a+b;
+    //        return fib1(--n,b,a+b);
+
+    //     }
+
+    //     int fib(int n) {
+    //         if(n == 0)
+    //         {
+    //             return 0;
+    //         }
+    //         if(n == 1 || n == 2 )
+    //         {
+    //             return 1;
+    //         }
+
+    //        return fib1(n,1,1);
+    //     }
 
     // int monotization(int n,vector<int> &dp) {
-       
+
     //    if(n <= 1)
     //    {
     //        return n;
@@ -50,53 +50,51 @@ public:
     // }
 
     // int fib(int n) {
-       
+
     //    vector<int> dp(n+1,-1);
-      
+
     //    int ans = monotization(n,dp);
 
     //    return ans;
     // }
 
-    
-//     int fib(int n) {
-// //Tabulation
-// if(n <= 1)
-// {
-//     return n;
-// }
-//         vector<int> dp(n+1,-1);
+    //     int fib(int n) {
+    // //Tabulation
+    // if(n <= 1)
+    // {
+    //     return n;
+    // }
+    //         vector<int> dp(n+1,-1);
 
-//         dp[0] = 0;
-//         dp[1] = 1;
+    //         dp[0] = 0;
+    //         dp[1] = 1;
 
-//         for(int i = 2 ; i <= n; i++)
-//         {
-//             dp[i] = dp[i-1]+dp[i-2];
-//         } 
-      
-//       return dp[n];
-//     }
-// };
+    //         for(int i = 2 ; i <= n; i++)
+    //         {
+    //             dp[i] = dp[i-1]+dp[i-2];
+    //         }
 
+    //       return dp[n];
+    //     }
+    // };
 
-  
-    int fib(int n) {
-//space optimization
-        if(n <= 1)
+    int fib(int n)
+    {
+        // space optimization
+        if (n <= 1)
         {
             return n;
         }
         int prev2 = 0;
         int prev = 1;
 
-        for(int i = 2 ; i <= n; i++)
+        for (int i = 2; i <= n; i++)
         {
-            int curi = prev2+prev;
+            int curi = prev2 + prev;
             prev2 = prev;
             prev = curi;
-        } 
-      
-      return prev;
+        }
+
+        return prev;
     }
 };

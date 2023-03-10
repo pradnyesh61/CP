109abdd5650317fe8ca8d106acd4f8b76fac82fe
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-03-10 10:05:43 +0530

    10 March 2023

--- LeetCode/40_Combination_Sum_II.cpp
+++ LeetCode/40_Combination_Sum_II.cpp
@@ -3,38 +3,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-
-    void comb(int ind, int target,vector<int> candidates, vector<vector<int>> &ans ,vector<int> v)
+    void comb(int ind, int target, vector<int> candidates, vector<vector<int>> &ans, vector<int> v)
     {
-        if(target == 0){
-        ans.push_back(v);
-        return;
+        if (target == 0)
+        {
+            ans.push_back(v);
+            return;
         }
 
-        for(int  i = ind ; i < candidates.size() ; i++)
+        for (int i = ind; i < candidates.size(); i++)
         {
-            if(i > ind &&  candidates[i] == candidates[i-1])
+            if (i > ind && candidates[i] == candidates[i - 1])
             {
                 continue;
             }
 
-            if(target < candidates[i]){
+            if (target < candidates[i])
+            {
                 break;
             }
 
             v.push_back(candidates[i]);
-            comb(i+1,target-candidates[i],candidates,ans,v);
+            comb(i + 1, target - candidates[i], candidates, ans, v);
             v.pop_back();
         }
     }
 
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
+    vector<vector<int>> combinationSum2(vector<int> &candidates, int target)
+    {
+        sort(candidates.begin(), candidates.end());
         vector<vector<int>> ans;
         vector<int> v;
-        comb(0,target,candidates,ans,v);
+        comb(0, target, candidates, ans, v);
 
         return ans;
     }

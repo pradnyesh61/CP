189bcaa318c36a5c7095fa18374d4d4a038b704f
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-03-11 21:34:16 +0530

    11 March 2023

---
+++ LeetCode/2579_Count_Total_Number_of_Colored_Cells.cpp
@@ -0,0 +1,31 @@
+// 11 March 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long coloredCells(int n) {
+        
+        if(n == 1)
+        {
+            return 1;
+        }
+        
+         long long temp = 1;
+         long long sum = 0;
+        
+        for(int i = 1; i < n ; i++)
+        {
+             long long k = 4 * i;
+            sum = k + temp;
+          
+           // cout<<"k="<<k<<" = "<<"temp="<<temp<<" = "<<"sum="<<sum;
+              temp = sum;
+            //cout<<"  =  aftertemp="<<temp<<endl;
+            
+        }
+        
+        return sum;
+    }
+};

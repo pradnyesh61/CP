96535b72da0d34a594732915064b2c63dd86cc16
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-03-12 23:05:10 +0530

    12 March 2023

--- LeetCode/2582_Pass_the_Pillow.cpp
+++ LeetCode/2582_Pass_the_Pillow.cpp
@@ -3,30 +3,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int passThePillow(int n, int time) {
-        
-        int i =1;
+    int passThePillow(int n, int time)
+    {
+
+        int i = 1;
         int flag = 0;
-        while(time--){
-            if(i == n)
+        while (time--)
+        {
+            if (i == n)
             {
                 flag = 1;
             }
-            else if(i == 1)
+            else if (i == 1)
             {
                 flag = 0;
             }
-            if(flag == 0)
+            if (flag == 0)
             {
-                i+= 1;
+                i += 1;
             }
-            else if(flag == 1){
+            else if (flag == 1)
+            {
                 i -= 1;
             }
         }
         return i;
-    
     }
 };

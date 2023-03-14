6c357a89cb1c4e52ff937121ea6420e7b5fa1d10
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-03-14 10:52:16 +0530

    14 March 2023

--- LeetCode/129_Sum_Root_to_Leaf_Numbers.cpp
+++ LeetCode/129_Sum_Root_to_Leaf_Numbers.cpp
@@ -3,18 +3,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-  struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode() : val(0), left(nullptr), right(nullptr) {}
-      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-  };
- 
-class Solution {
-public:
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
+class Solution
+{
+public:
     // void helper(TreeNode* root, int &sum,int temp)
     // {
     //     if(root == NULL)
@@ -35,35 +36,34 @@ public:
 
     //     temp = temp / 10;
     // }
-    
+
     //  int sumNumbers(TreeNode* root) {
     //     int sum = 0;
     //     int temp = 0;
     //   return helper(root,temp);
     //     return sum;
     // }
-    
-    int helper(TreeNode* root,int temp)
+
+    int helper(TreeNode *root, int temp)
     {
-        if(root == NULL)
+        if (root == NULL)
         {
             return 0;
         }
 
         temp = temp * 10 + root->val;
 
-        if(root->left == NULL && root -> right == NULL)
+        if (root->left == NULL && root->right == NULL)
         {
             return temp;
         }
 
-        return helper(root->left,temp) + helper(root->right,temp);
-
+        return helper(root->left, temp) + helper(root->right, temp);
     }
 
-    int sumNumbers(TreeNode* root) {
-       
-      return helper(root,0);
-       
+    int sumNumbers(TreeNode *root)
+    {
+
+        return helper(root, 0);
     }
 };

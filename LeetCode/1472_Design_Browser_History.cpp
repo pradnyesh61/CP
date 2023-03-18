a6ccf8487a13c9a8a832aeb47df8ea306db95474
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-03-18 10:24:50 +0530

    18 March 2023

---
+++ LeetCode/1472_Design_Browser_History.cpp
@@ -0,0 +1,64 @@
+// 18 March 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class BrowserHistory {
+
+private:
+    stack<string> cur,backword;
+
+
+public:
+    BrowserHistory(string homepage) {
+        cur.push(homepage);
+       
+    }
+    
+    void visit(string url) {
+
+        while(!backword.empty())
+        {
+            backword.pop();
+          
+        }
+        cur.push(url);
+    }
+    
+    string back(int steps) {
+
+        while(steps--)
+        {
+           
+            if(cur.size() > 1)
+            {
+                backword.push(cur.top());
+                cur.pop();
+            }
+
+             
+        }
+        return cur.top();
+    }
+    
+    string forward(int steps) {
+       
+         while(steps-- && backword.size() > 0)
+        {
+                cur.push(backword.top());
+                backword.pop();
+            
+        }
+
+        return cur.top();
+    }
+};
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */

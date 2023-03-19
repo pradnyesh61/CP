3f747ee8723fa6e1c5373be91ecf1e01a8bea0ee
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-03-19 23:43:24 +0530

    19 March 2023

--- LeetCode/2595_Number_of_Even_and_Odd_Bits.cpp
+++ LeetCode/2595_Number_of_Even_and_Odd_Bits.cpp
@@ -3,36 +3,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> evenOddBit(int n) {
-        
+    vector<int> evenOddBit(int n)
+    {
+
         vector<int> v;
-        
-        while(n)
+
+        while (n)
         {
-            int rem = n%2;
+            int rem = n % 2;
             v.push_back(rem);
-            n=n/2;
+            n = n / 2;
         }
-        
+
         int odd = 0;
         int even = 0;
-       for(int i = v.size()-1 ; i >=0 ;i--)
-       {
-          
-           if((i%2 == 0) && v[i]==1)
-           {
-              
-               even++;
-           }
-           else if((i%2 != 0) && v[i]==1){
-              
-               odd++;
-           }
-       }
-        
-        cout<<even<<" == "<<odd<<endl;
+        for (int i = v.size() - 1; i >= 0; i--)
+        {
+
+            if ((i % 2 == 0) && v[i] == 1)
+            {
+
+                even++;
+            }
+            else if ((i % 2 != 0) && v[i] == 1)
+            {
+
+                odd++;
+            }
+        }
+
+        cout << even << " == " << odd << endl;
         vector<int> a;
         a.push_back(even);
         a.push_back(odd);

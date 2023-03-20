83e5cac8e11392e7a0f463047fb8d764eafce996
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-03-20 15:27:51 +0530

    20 March 2023

--- LeetCode/605_Can_Place_Flowers.cpp
+++ LeetCode/605_Can_Place_Flowers.cpp
@@ -3,33 +3,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-
-        if(n==0)
+    bool canPlaceFlowers(vector<int> &flowerbed, int n)
+    {
+        if (n == 0)
             return true;
 
-
-        for(int i = 0 ; i < flowerbed.size() ; i++)
+        for (int i = 0; i < flowerbed.size(); i++)
         {
-            if(flowerbed[i] == 0){
-                bool left = (i == 0) || (flowerbed[i-1] == 0);
-                bool right = (i == flowerbed.size()-1) || (flowerbed[i+1] == 0);
+            if (flowerbed[i] == 0)
+            {
+                bool left = (i == 0) || (flowerbed[i - 1] == 0);
+                bool right = (i == flowerbed.size() - 1) || (flowerbed[i + 1] == 0);
 
-                if(left && right){
+                if (left && right)
+                {
                     n--;
                     flowerbed[i] = 1;
 
-                    if(n==0)
-                    return true;
+                    if (n == 0)
+                        return true;
                 }
-
             }
-        }    
+        }
 
-        
-           return !n;
-    
+        return !n;
     }
 };

bc913d4b696bbcf3cd4c4122d084cbef921c4fb9
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-03-21 10:41:23 +0530

    21 March 2023

--- LeetCode/2348_Number_of_Zero_Filled_Subarrays.cpp
+++ LeetCode/2348_Number_of_Zero_Filled_Subarrays.cpp
@@ -3,30 +3,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
+    long long zeroFilledSubarray(vector<int> &nums)
+    {
 
-   long long zeroFilledSubarray(vector<int>& nums) {
-        
-        long long  subarray = 0;
-        long long  count = 0;
-        
-        for(int i = 0  ; i < nums.size() ; i++)
+        long long subarray = 0;
+        long long count = 0;
+
+        for (int i = 0; i < nums.size(); i++)
         {
-            if(nums[i] == 0)
+            if (nums[i] == 0)
             {
                 subarray++;
                 count += subarray;
             }
-            else{
+            else
+            {
                 subarray = 0;
             }
         }
-      return count;
+        return count;
     }
     // long long zeroFilledSubarray(vector<int>& nums) {
-        
+
     //     long long  count = 0;
     //     for(int i = 0  ; i < nums.size() ; i++)
     //     {
@@ -35,7 +36,7 @@ public:
     //         for(int j = i ;  j < nums.size();j++)
     //         {
     //             bool check = true;
-    //             v.push_back(nums[j]);   
+    //             v.push_back(nums[j]);
     //             for(auto it : v)
     //             {
     //                 if(it != 0)
@@ -51,7 +52,6 @@ public:
     //             }
     //         }
 
-          
     //     }
     //   return count;
     // }

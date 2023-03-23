0ab8acf8e18df8d06e8fe197bab299c0c92ff0d3
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-03-23 10:47:06 +0530

    23 March 2023

--- LeetCode/62_Unique_Paths.cpp
+++ LeetCode/62_Unique_Paths.cpp
@@ -1,84 +1,84 @@
-// 22 March 2023
+// 23 March 2023
 
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-
-    int recursion(int i,int j)
+    int recursion(int i, int j)
     {
 
-        if(i < 0 || j < 0)
+        if (i < 0 || j < 0)
         {
             return 0;
         }
 
-        if(i == 0 && j == 0)
+        if (i == 0 && j == 0)
         {
             return 1;
         }
 
-       int up =  recursion(i-1,j);
-       int left =  recursion(i,j-1);
+        int up = recursion(i - 1, j);
+        int left = recursion(i, j - 1);
 
-       return up+left;
+        return up + left;
     }
 
-    int memoization(int i,int j,vector<vector<int>> &dp)
+    int memoization(int i, int j, vector<vector<int>> &dp)
     {
 
-        if(i < 0 || j < 0)
+        if (i < 0 || j < 0)
         {
             return 0;
         }
 
-        if(i == 0 && j == 0)
+        if (i == 0 && j == 0)
         {
             return 1;
         }
 
-        if(dp[i][j] != -1)
+        if (dp[i][j] != -1)
         {
             return dp[i][j];
         }
 
-       int up =  memoization(i-1,j,dp);
-       int left =  memoization(i,j-1,dp);
+        int up = memoization(i - 1, j, dp);
+        int left = memoization(i, j - 1, dp);
 
-       return dp[i][j] = up+left;
+        return dp[i][j] = up + left;
     }
 
-    int tablulation(int m ,int n)
+    int tablulation(int m, int n)
     {
-        vector<vector<int>> dp(m, vector<int>(n,0));
-       
-        for(int i = 0 ; i < m ; i++)
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        for (int i = 0; i < m; i++)
         {
-            for(int j = 0; j < n; j++)
+            for (int j = 0; j < n; j++)
             {
-                if(i ==0 && j ==0)
+                if (i == 0 && j == 0)
                 {
-                    dp[i][j] =1;
+                    dp[i][j] = 1;
                 }
-                else{
-                    int left = 0 , up = 0;
-                    if(i-1 >= 0 )
+                else
+                {
+                    int left = 0, up = 0;
+                    if (i - 1 >= 0)
                     {
-                        left = dp[i-1][j];
+                        left = dp[i - 1][j];
                     }
 
-                    if(j-1 >= 0)
+                    if (j - 1 >= 0)
                     {
-                        up = dp[i][j-1];
+                        up = dp[i][j - 1];
                     }
 
-
-                    dp[i][j] = left + up;       
-                } 
+                    dp[i][j] = left + up;
+                }
             }
         }
-        
+
         // for(auto it : dp)
         // {
         //     for(auto v : it)
@@ -89,37 +89,37 @@ public:
         //     cout<<endl;
         // }
 
-        return dp[m-1][n-1];
-
+        return dp[m - 1][n - 1];
     }
 
-    int spaceOptimization(int m,int n)
+    int spaceOptimization(int m, int n)
     {
-        vector<int> prev(n,0);
+        vector<int> prev(n, 0);
 
-        for(int i = 0 ; i < m ; i++)
+        for (int i = 0; i < m; i++)
         {
             vector<int> cur(n);
-            for(int j = 0 ; j < n ; j++)
+            for (int j = 0; j < n; j++)
             {
-                if(i ==0 && j ==0)
+                if (i == 0 && j == 0)
                 {
                     cur[j] = 1;
                 }
-                else{
+                else
+                {
                     int left = 0;
-                    if(i-1 >= 0)
+                    if (i - 1 >= 0)
                     {
-                       left = prev[j];
+                        left = prev[j];
                     }
 
                     int up = 0;
-                    if(j-1 >= 0)
+                    if (j - 1 >= 0)
                     {
-                        up = cur[j-1];
+                        up = cur[j - 1];
                     }
 
-                    cur[j] = left+up;
+                    cur[j] = left + up;
                 }
             }
             prev = cur;
@@ -130,16 +130,16 @@ public:
             // cout<<endl;
         }
 
-        return prev[n-1];
-        
+        return prev[n - 1];
     }
 
-    int uniquePaths(int m, int n) {
-        
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-       // return recursion(m-1,n-1);
-       // return memoization(m-1,n-1,dp);
-       // return tablulation(m,n);
-       return spaceOptimization(m,n);
+    int uniquePaths(int m, int n)
+    {
+
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        // return recursion(m-1,n-1);
+        // return memoization(m-1,n-1,dp);
+        // return tablulation(m,n);
+        return spaceOptimization(m, n);
     }
 };

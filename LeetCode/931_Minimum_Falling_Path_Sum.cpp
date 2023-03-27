b92e3e188042a7cd797bc55256a96bf9e0eb9913
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-03-27 12:44:00 +0530

    27 March 2023

--- LeetCode/931_Minimum_Falling_Path_Sum.cpp
+++ LeetCode/931_Minimum_Falling_Path_Sum.cpp
@@ -3,124 +3,123 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-
-    int recursion(int i, int j, vector<vector<int>>& matrix)
+    int recursion(int i, int j, vector<vector<int>> &matrix)
     {
-        if(j < 0 || j >= matrix.size())
+        if (j < 0 || j >= matrix.size())
         {
             return 1e9;
         }
 
-        if(i == matrix.size()-1)
+        if (i == matrix.size() - 1)
         {
             return matrix[i][j];
         }
 
-        int leftDiagonal = matrix[i][j] + recursion(i+1,j-1,matrix);
-        int down = matrix[i][j] + recursion(i+1,j,matrix);
-        int rightDiagonal = matrix[i][j] + recursion(i+1,j+1,matrix);
+        int leftDiagonal = matrix[i][j] + recursion(i + 1, j - 1, matrix);
+        int down = matrix[i][j] + recursion(i + 1, j, matrix);
+        int rightDiagonal = matrix[i][j] + recursion(i + 1, j + 1, matrix);
 
-        return min(leftDiagonal,min(down,rightDiagonal));
+        return min(leftDiagonal, min(down, rightDiagonal));
     }
-    
-    int memoization(int i, int j, vector<vector<int>>& matrix,vector<vector<int>>& dp)
+
+    int memoization(int i, int j, vector<vector<int>> &matrix, vector<vector<int>> &dp)
     {
-        if(j < 0 || j >= matrix.size())
+        if (j < 0 || j >= matrix.size())
         {
             return 1e9;
         }
 
-        if(i == matrix.size()-1)
+        if (i == matrix.size() - 1)
         {
             return matrix[i][j];
         }
 
-        if(dp[i][j] != -1)
+        if (dp[i][j] != -1)
         {
             return dp[i][j];
         }
 
-        int leftDiagonal = matrix[i][j] + memoization(i+1,j-1,matrix,dp);
-        int down = matrix[i][j] + memoization(i+1,j,matrix,dp);
-        int rightDiagonal = matrix[i][j] + memoization(i+1,j+1,matrix,dp);
+        int leftDiagonal = matrix[i][j] + memoization(i + 1, j - 1, matrix, dp);
+        int down = matrix[i][j] + memoization(i + 1, j, matrix, dp);
+        int rightDiagonal = matrix[i][j] + memoization(i + 1, j + 1, matrix, dp);
 
-        return dp[i][j] =  min(leftDiagonal,min(down,rightDiagonal));
+        return dp[i][j] = min(leftDiagonal, min(down, rightDiagonal));
     }
 
-    int tabulation(int m,int n,vector<vector<int>>& matrix)
+    int tabulation(int m, int n, vector<vector<int>> &matrix)
     {
-        vector<vector<int>> dp(matrix.size(),vector<int> (matrix[0].size(),0));
-        
-        for(int j = 0 ; j < m ; j++){
-            dp[m-1][j] = matrix[m-1][j];
+        vector<vector<int>> dp(matrix.size(), vector<int>(matrix[0].size(), 0));
+
+        for (int j = 0; j < m; j++)
+        {
+            dp[m - 1][j] = matrix[m - 1][j];
         }
 
-        for(int i = m-2 ; i >=0 ; i--)
+        for (int i = m - 2; i >= 0; i--)
         {
-            for(int j = 0; j < n; j++)
+            for (int j = 0; j < n; j++)
             {
                 int leftDiagonal = 1e9;
-                if(j-1 >= 0)
+                if (j - 1 >= 0)
                 {
-                    leftDiagonal = matrix[i][j] + dp[i+1][j-1];
-                }
-                
-                int down = matrix[i][j] + dp[i+1][j];
-                
-                int rightDiagonal = 1e9;
-                if(j+1 <m)
-                {
-                    rightDiagonal = matrix[i][j] + dp[i+1][j+1];
+                    leftDiagonal = matrix[i][j] + dp[i + 1][j - 1];
                 }
 
-                dp[i][j] = min(leftDiagonal,min(down,rightDiagonal));
+                int down = matrix[i][j] + dp[i + 1][j];
+
+                int rightDiagonal = 1e9;
+                if (j + 1 < m)
+                {
+                    rightDiagonal = matrix[i][j] + dp[i + 1][j + 1];
+                }
+
+                dp[i][j] = min(leftDiagonal, min(down, rightDiagonal));
             }
         }
 
-        return *min_element(dp[0].begin(),dp[0].end());
+        return *min_element(dp[0].begin(), dp[0].end());
     }
 
-    int spaceOptimization(int m , int n , vector<vector<int>>& matrix)
+    int spaceOptimization(int m, int n, vector<vector<int>> &matrix)
     {
-        vector<int> prev(n,0),cur(n);
+        vector<int> prev(n, 0), cur(n);
 
-        for(int j = 0 ; j < n ; j++)
+        for (int j = 0; j < n; j++)
         {
-            prev[j] = matrix[m-1][j];
+            prev[j] = matrix[m - 1][j];
         }
-    
 
-        for(int i = m-2 ; i >= 0 ; i--)
+        for (int i = m - 2; i >= 0; i--)
         {
-            for(int j = 0; j < n ; j++)
+            for (int j = 0; j < n; j++)
             {
                 int leftDiagonal = 1e9;
-                if(j-1 >= 0)
-                leftDiagonal = matrix[i][j] + prev[j-1];
+                if (j - 1 >= 0)
+                    leftDiagonal = matrix[i][j] + prev[j - 1];
 
                 int down = matrix[i][j] + prev[j];
 
                 int rightDiagonal = 1e9;
-                if(j+1 < n )
-                rightDiagonal = matrix[i][j] + prev[j+1];
+                if (j + 1 < n)
+                    rightDiagonal = matrix[i][j] + prev[j + 1];
 
-                cur[j] = min(leftDiagonal,min(down,rightDiagonal));
+                cur[j] = min(leftDiagonal, min(down, rightDiagonal));
             }
             prev = cur;
         }
 
-        return *min_element(prev.begin(),prev.end());
+        return *min_element(prev.begin(), prev.end());
     }
 
-  
+    int minFallingPathSum(vector<vector<int>> &matrix)
+    {
 
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        
-        //int min = INT_MAX;
-        //vector<vector<int>> dp(matrix.size(),vector<int> (matrix[0].size(),-1));
-      
+        // int min = INT_MAX;
+        // vector<vector<int>> dp(matrix.size(),vector<int> (matrix[0].size(),-1));
+
         // for(int j = 0 ; j < matrix[0].size() ; j++)
         // {
         //     // int ans = recursion(0,j,matrix);
@@ -140,11 +139,9 @@ public:
         //     cout<<endl;
         // }
         // cout<<endl;
-        //return min;
+        // return min;
 
-      // return  tabulation(matrix.size(),matrix[0].size(),matrix);
-       return spaceOptimization(matrix.size(),matrix[0].size(),matrix);
-
-
+        // return  tabulation(matrix.size(),matrix[0].size(),matrix);
+        return spaceOptimization(matrix.size(), matrix[0].size(), matrix);
     }
 };

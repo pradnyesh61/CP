dc13323e85ea2e2440b2b726decf1e96f594afdc
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-03-28 18:44:01 +0530

    28 March 2023

--- LeetCode/983_Minimum_Cost_For_Tickets.cpp
+++ LeetCode/983_Minimum_Cost_For_Tickets.cpp
@@ -3,143 +3,133 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-
-    int recursion(int i,vector<int> &days, vector<int>& costs, vector<int> &dp)
+    int recursion(int i, vector<int> &days, vector<int> &costs, vector<int> &dp)
     {
-       
-        if(i >= days.size())
+
+        if (i >= days.size())
         {
             return 0;
         }
 
-        if(dp[i] != -1)
+        if (dp[i] != -1)
         {
             return dp[i];
         }
 
-     
-        int day1 = costs[0] + recursion(i+1,days,costs,dp);
+        int day1 = costs[0] + recursion(i + 1, days, costs, dp);
 
-
-        int temp7 = days[i]+7;
+        int temp7 = days[i] + 7;
         int j = i;
-        while(temp7 > days[j] && j < days.size() )
+        while (temp7 > days[j] && j < days.size())
         {
             j++;
         }
 
-        int day7 = costs[1] + recursion(j,days,costs,dp);
+        int day7 = costs[1] + recursion(j, days, costs, dp);
 
-        
-      
-        int temp30 = days[i]+30;
+        int temp30 = days[i] + 30;
         j = i;
-        while(temp30 > days[j] && j < days.size() )
+        while (temp30 > days[j] && j < days.size())
         {
             j++;
         }
 
-        int day30 = costs[2] + recursion(j,days,costs ,dp);        
-       
-        return dp[i] =  min(day1,min(day7,day30));
+        int day30 = costs[2] + recursion(j, days, costs, dp);
+
+        return dp[i] = min(day1, min(day7, day30));
     }
 
-    int memoizaton(int i,vector<int> &days, vector<int>& costs,vector<vector<int>> &dp) 
+    int memoizaton(int i, vector<int> &days, vector<int> &costs, vector<vector<int>> &dp)
     {
 
-        if(i >= days.size())
+        if (i >= days.size())
         {
             return 0;
         }
 
-        if(dp[i][days[i]] != -1)
+        if (dp[i][days[i]] != -1)
         {
             return dp[i][days[i]];
         }
 
-        int day1 = costs[0] + memoizaton(i+1,days,costs,dp);
+        int day1 = costs[0] + memoizaton(i + 1, days, costs, dp);
 
         int j = i;
 
-        for( ; j < days.size() ; j++)
+        for (; j < days.size(); j++)
         {
-            if(days[j] >= days[i]+7)
+            if (days[j] >= days[i] + 7)
             {
                 break;
             }
         }
 
-        int day7 = costs[1] + memoizaton(j,days,costs,dp);
+        int day7 = costs[1] + memoizaton(j, days, costs, dp);
 
-        
-        for(j = i; j < days.size() ; j++)
+        for (j = i; j < days.size(); j++)
         {
-            if(days[j] >= days[i] + 30)
+            if (days[j] >= days[i] + 30)
             {
                 break;
             }
-        } 
+        }
 
-        int day30 = costs[2] + memoizaton(j,days,costs,dp);
+        int day30 = costs[2] + memoizaton(j, days, costs, dp);
 
-        return dp[i][days[i]] = min(day1, min(day7,day30));
+        return dp[i][days[i]] = min(day1, min(day7, day30));
+    }
 
-    }  
-
-    int memoizaton1(int i,vector<int> &days, vector<int>& costs,vector<int> &dp) 
+    int memoizaton1(int i, vector<int> &days, vector<int> &costs, vector<int> &dp)
     {
 
-        if(i >= days.size())
+        if (i >= days.size())
         {
             return 0;
         }
 
-        if(dp[i] != -1)
+        if (dp[i] != -1)
         {
             return dp[i];
         }
 
-        int day1 = costs[0] + memoizaton1(i+1,days,costs,dp);
+        int day1 = costs[0] + memoizaton1(i + 1, days, costs, dp);
 
         int j = i;
 
-        for( ; j < days.size() ; j++)
+        for (; j < days.size(); j++)
         {
-            if(days[j] >= days[i]+7)
+            if (days[j] >= days[i] + 7)
             {
                 break;
             }
         }
 
-        int day7 = costs[1] + memoizaton1(j,days,costs,dp);
+        int day7 = costs[1] + memoizaton1(j, days, costs, dp);
 
-        
-        for(j = i; j < days.size() ; j++)
+        for (j = i; j < days.size(); j++)
         {
-            if(days[j] >= days[i] + 30)
+            if (days[j] >= days[i] + 30)
             {
                 break;
             }
-        } 
+        }
 
-        int day30 = costs[2] + memoizaton1(j,days,costs,dp);
+        int day30 = costs[2] + memoizaton1(j, days, costs, dp);
 
-        return dp[i] = min(day1, min(day7,day30));
+        return dp[i] = min(day1, min(day7, day30));
+    }
 
-    }  
-
-
-
-    int mincostTickets(vector<int>& days, vector<int>& costs) {
-        vector<int> dp(days.size()+1, -1);
-       // return recursion(0,days,costs,dp);
-       // vector<vector<int>> dp(days.size(), vector<int> (days[days.size()-1]+1,-1));
-        //return memoizaton(0,days,costs,dp);
+    int mincostTickets(vector<int> &days, vector<int> &costs)
+    {
+        vector<int> dp(days.size() + 1, -1);
+        // return recursion(0,days,costs,dp);
+        // vector<vector<int>> dp(days.size(), vector<int> (days[days.size()-1]+1,-1));
+        // return memoizaton(0,days,costs,dp);
 
         // vector<int> dp(days.size()+1, -1);
-         return memoizaton1(0,days,costs,dp);
-    
+        return memoizaton1(0, days, costs, dp);
     }
 };

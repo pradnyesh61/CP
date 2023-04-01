a7133269d0689acef6aa2471d79de318d40e09ce
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-04-01 22:53:47 +0530

    1 April 2023

--- LeetCode/704_Binary_Search.cpp
+++ LeetCode/704_Binary_Search.cpp
@@ -3,27 +3,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int search(vector<int>& nums, int target) {
-        
+    int search(vector<int> &nums, int target)
+    {
+
         int start = 0;
-        int end = nums.size()-1;
+        int end = nums.size() - 1;
 
-        while(start <= end)
+        while (start <= end)
         {
-            int mid = (start+end)/2;
+            int mid = (start + end) / 2;
 
-            if(nums[mid] == target)
+            if (nums[mid] == target)
             {
                 return mid;
             }
-            else if(nums[mid] < target)
+            else if (nums[mid] < target)
             {
-                start = mid+1;
+                start = mid + 1;
             }
-            else{
-                end = mid-1;
+            else
+            {
+                end = mid - 1;
             }
         }
 

f4ee8888fc44a8ed39208e03ad9e4767d60fe354
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-04-02 18:30:12 +0530

    2 April 2023

--- LeetCode/2300_Successful_Pairs_of_Spells_and_Potions.cpp
+++ LeetCode/2300_Successful_Pairs_of_Spells_and_Potions.cpp
@@ -3,13 +3,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    // vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) 
+    // vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success)
     // {
     //     vector<int> sucPair(spells.size(),0);
     //     sort(potions.begin(),potions.end());
-      
+
     //     for(long long i = 0; i < spells.size(); i++)
     //     {
     //         long long count = 0;
@@ -18,48 +19,47 @@ public:
     //             long long ans =  (long long)spells[i]*potions[j];
     //             if(ans >= success)
     //             {
-    //                 count = (potions.size()) - j; 
-    //                 break;       
+    //                 count = (potions.size()) - j;
+    //                 break;
     //             }
     //         }
     //         sucPair[i] = count;
-    //     } 
+    //     }
 
-    //     return sucPair;   
+    //     return sucPair;
     // }
 
-    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) 
+    vector<int> successfulPairs(vector<int> &spells, vector<int> &potions, long long success)
     {
 
-        vector<pair<int,int>> v;
+        vector<pair<int, int>> v;
 
-        for(int i = 0; i < spells.size(); i++)
+        for (int i = 0; i < spells.size(); i++)
         {
-            v.push_back({spells[i],i});
+            v.push_back({spells[i], i});
         }
 
-        sort(v.begin(),v.end());
+        sort(v.begin(), v.end());
 
-        vector<int> sucPair(spells.size(),0);
-       
-        sort(potions.begin(),potions.end());
-        
-        int i  = potions.size()-1;
+        vector<int> sucPair(spells.size(), 0);
+
+        sort(potions.begin(), potions.end());
+
+        int i = potions.size() - 1;
         int count = 0;
-       
-       for(auto [spells,index] : v)
-       {
-          
-           while(i >= 0 && (long long)spells*potions[i] >= success)
-           {
-               count++;
-               i--;
-             
-           }
 
-           sucPair[index] = count;
-       }
+        for (auto [spells, index] : v)
+        {
 
-        return sucPair;   
+            while (i >= 0 && (long long)spells * potions[i] >= success)
+            {
+                count++;
+                i--;
+            }
+
+            sucPair[index] = count;
+        }
+
+        return sucPair;
     }
 };

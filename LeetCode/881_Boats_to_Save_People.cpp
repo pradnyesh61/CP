4936a76119e22b3e64afdb1fccd72107f63da666
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-04-03 13:34:14 +0530

    3 April 2023

--- LeetCode/881_Boats_to_Save_People.cpp
+++ LeetCode/881_Boats_to_Save_People.cpp
@@ -3,16 +3,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
     // int numRescueBoats(vector<int>& people, int limit) {
-        
+
     //     long i = 0;
     //     long j = people.size()-1;
 
     //     sort(people.begin(),people.end());
-      
+
     //     int shipCount = 0;
 
     //     while(i <= j)
@@ -23,47 +23,45 @@ public:
     //             l = l - people[j];
     //              j = j - 1;
     //         }
-            
+
     //         if(i <= j && (l - people[i]) >= 0 && people[i] <= l)
     //         {
     //             l -= people[i];
     //             i++;
-            
+
     //         }
 
     //         shipCount++;
-         
+
     //     }
 
-      
     //     return shipCount;
     // }
 
-    int numRescueBoats(vector<int>& people, int limit) {
-        
+    int numRescueBoats(vector<int> &people, int limit)
+    {
+
         // atleast one perons will fit into boat
         // so heavier person we assume he is taken into boat /sitted
         // we just need to check lighter person can fit into same boat or not with heaviest
         long i = 0;
-        long j = people.size()-1;
+        long j = people.size() - 1;
 
-        sort(people.begin(),people.end());
-      
+        sort(people.begin(), people.end());
+
         int shipCount = 0;
 
-        while(i <= j)
+        while (i <= j)
         {
-            if(people[i] + people[j] <= limit )
+            if (people[i] + people[j] <= limit)
             {
                 i++;
             }
-           j--;
+            j--;
 
             shipCount++;
-         
         }
 
-      
         return shipCount;
     }
 };

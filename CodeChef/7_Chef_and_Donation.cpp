94b97fc3ce4db71c37d92b1163b9b7bbd1083c37
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-04-05 22:32:21 +0530

    Starters 84

---
+++ CodeChef/7_Chef_and_Donation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	
+	while(n--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    
+	    cout<<abs(a-b)<<endl;
+	}
+	return 0;
+}

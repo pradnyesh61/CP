4e5ed798903bbceb55ec5cac38cda08cbbaec240
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-04-05 22:22:12 +0530

    5 April 2023

---
+++ LeetCode/338_Counting_Bits.cpp
@@ -0,0 +1,36 @@
+
+// 5 April 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    int bit_count(int n)
+    {
+        int count = 0;
+
+        while(n)
+        {
+            int ans = n % 2;
+            if(ans == 1)
+            {
+                count++;
+            }
+            n = n/2;
+        }
+        return count;
+    }
+    vector<int> countBits(int n) {
+        
+        vector<int> v;
+        
+        for(int i = 0 ; i <= n ; i++)
+        {
+            v.push_back(bit_count(i));
+        }
+
+        return v;
+    }
+};

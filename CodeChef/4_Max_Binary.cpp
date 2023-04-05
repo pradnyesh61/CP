6736a9ae0e723c209937edf2307aa16dcb21f939
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-04-05 22:28:17 +0530

    Starters 84

---
+++ CodeChef/4_Max_Binary.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	
+	while(n--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    
+	    string s;
+	    cin>>s;
+	   
+	   if(s[0] == '0')
+	   {
+	       b--;
+	       s[0] = '1';
+	   }
+	   
+	   while(b--)
+	   {
+	       s.push_back('0');
+	   }
+	   
+	   cout<<s<<endl;
+	}
+	return 0;
+}

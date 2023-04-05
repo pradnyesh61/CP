6e3beb1ca73e7a35fd81931da9d99f74db63261f
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-04-05 22:30:03 +0530

    Starters 84

--- CodeChef/5_Melt_Gold.cpp
+++ CodeChef/5_Melt_Gold.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	// your code goes here
-	int n;
-	cin>>n;
-	
-	while(n--)
-	{
-	    int a,b;
-	    cin>>a>>b;
-	    
-	    int i = 1;
-	    int count = 0;
-	    
-	    while(b < a)
-	    {
-	        b += i;
-	        i++;
-	        count++;
-	        
-	    }
-	    cout<<count<<endl;
-	}
-	return 0;
+int main()
+{
+    // your code goes here
+    int n;
+    cin >> n;
+
+    while (n--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        int i = 1;
+        int count = 0;
+
+        while (b < a)
+        {
+            b += i;
+            i++;
+            count++;
+        }
+        cout << count << endl;
+    }
+    return 0;
 }

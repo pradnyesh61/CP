ae856aaee5f72773c04c92b333119b4f3474ee66
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-04-06 18:03:16 +0530

    6 April 2023

--- LeetCode/746_Min_Cost_Climbing_Stairs.cpp
+++ LeetCode/746_Min_Cost_Climbing_Stairs.cpp
@@ -3,51 +3,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-    
     int Memoization(int i, vector<int> &cost, vector<int> &dp)
     {
 
-        if(i == 0)
+        if (i == 0)
         {
             return cost[0];
         }
 
-        if(i < 0)
+        if (i < 0)
         {
             return 0;
         }
 
-        if(dp[i] != -1)
-        return dp[i];
+        if (dp[i] != -1)
+            return dp[i];
 
-        int singleStep = cost[i] + Memoization(i-1,cost, dp);
+        int singleStep = cost[i] + Memoization(i - 1, cost, dp);
 
-        int twoStep = cost[i] + Memoization(i-2,cost, dp);
+        int twoStep = cost[i] + Memoization(i - 2, cost, dp);
 
-        return dp[i] =  min(singleStep,twoStep);
+        return dp[i] = min(singleStep, twoStep);
     }
 
-    int tabulation(vector<int> & cost)
+    int tabulation(vector<int> &cost)
     {
         int n = cost.size();
 
-        vector<int> dp(n+1,0);
+        vector<int> dp(n + 1, 0);
         dp[0] = 0;
 
-        for(int i = 1 ; i <= n ; i++)
+        for (int i = 1; i <= n; i++)
         {
-            int singleStep = cost[i-1] + dp[i-1];
-            int twoStep = cost[i-1];
-            if(i-2 >= 0)
-            twoStep += dp[i-2];
+            int singleStep = cost[i - 1] + dp[i - 1];
+            int twoStep = cost[i - 1];
+            if (i - 2 >= 0)
+                twoStep += dp[i - 2];
 
-            dp[i] =  min(singleStep,twoStep);
+            dp[i] = min(singleStep, twoStep);
         }
 
-        return min(dp[n-1],dp[n]);
+        return min(dp[n - 1], dp[n]);
     }
 
     int spaceOptimization(vector<int> &cost)
@@ -57,29 +56,28 @@ public:
         int prev = 0;
         int secondPrev = 0;
 
-        for(int i = 1 ; i <= n; i++)
+        for (int i = 1; i <= n; i++)
         {
-            int singleStep = cost[i-1] + prev;
-            int twoStep = cost[i-1] + secondPrev;
+            int singleStep = cost[i - 1] + prev;
+            int twoStep = cost[i - 1] + secondPrev;
 
-            int ans = min(singleStep,twoStep);
+            int ans = min(singleStep, twoStep);
 
             secondPrev = prev;
             prev = ans;
         }
 
-        return min(prev,secondPrev);
+        return min(prev, secondPrev);
     }
 
-
-
-    int minCostClimbingStairs(vector<int>& cost) {
+    int minCostClimbingStairs(vector<int> &cost)
+    {
 
         // cost.push_back(0);
         // vector<int> dp(cost.size(),-1);
         // return Memoization(cost.size()-1, cost, dp);
 
-        //return tabulation(cost);
+        // return tabulation(cost);
         return spaceOptimization(cost);
     }
 };

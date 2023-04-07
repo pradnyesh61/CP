f223418b6878d84cfabbb8129f0e682b91191ee0
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-04-07 21:40:42 +0530

    7 April 2023

---
+++ LeetCode/1025_Divisor_Game.cpp
@@ -0,0 +1,30 @@
+// 7 April 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    bool divisorGame(int n) {
+        
+        bool alice = true;
+
+        while( 1 < n)
+        {
+            n = n - 1;
+            
+            if(alice)
+            alice = false;
+            else
+            alice = true;
+        }
+
+        if(alice)
+        {
+            return false;
+        }
+
+        return true;
+    }
+};

cd1da33100f9d34e423dc8ef4c1a64573bd6bfc9
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-04-08 21:45:32 +0530

    8 April 2023

--- LeetCode/1646_Get_Maximum_in_Generated_Array.cpp
+++ LeetCode/1646_Get_Maximum_in_Generated_Array.cpp
@@ -3,27 +3,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-    int getMaximumGenerated(int n) {
-        
-        vector<int> v(n+2);
+    int getMaximumGenerated(int n)
+    {
+
+        vector<int> v(n + 2);
 
         v[0] = 0;
         v[1] = 1;
 
-        for(int i = 1 ; i <= n; i++)
+        for (int i = 1; i <= n; i++)
         {
-            int temp = 2*i;
+            int temp = 2 * i;
 
-            if(temp >= 2 && temp <= n){
+            if (temp >= 2 && temp <= n)
+            {
                 v[temp] = v[i];
-                v[temp+1] = v[i] + v[i+1];          
+                v[temp + 1] = v[i] + v[i + 1];
             }
         }
 
-        //array is initialize as n+2 that is why v.end()-1
-        return *max_element(v.begin(),v.end()-1);
+        // array is initialize as n+2 that is why v.end()-1
+        return *max_element(v.begin(), v.end() - 1);
     }
 };

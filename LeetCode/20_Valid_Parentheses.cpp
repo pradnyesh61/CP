2bbf7cb7f7ce9820783010aec7e1e7554f8c1f2d
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-04-10 10:32:12 +0530

    10 April 2023

---
+++ LeetCode/20_Valid_Parentheses.cpp
@@ -0,0 +1,52 @@
+// 10 April 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char> st;
+
+        for(int i = 0; i <s.length() ; i++)
+        {
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{')
+            {
+                st.push(s[i]);
+            }
+            else if(s[i] == ')')
+            {
+                if(st.empty() || st.top() != '(')
+                {
+                    return false;
+                }
+                st.pop();
+            }
+            else if(s[i] == '}')
+            {
+                if(st.empty() || st.top() != '{')
+                {
+                    return false;
+                }
+                st.pop();
+            }
+            else if( s[i] == ']')
+            {
+                if(st.empty() || st.top() != '[')
+                {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+
+        if(st.empty())
+        {
+            return true;
+        }
+
+        return false;
+    }
+};

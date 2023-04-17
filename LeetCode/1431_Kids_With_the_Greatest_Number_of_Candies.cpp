31747da10509c14f0dff02a0a7f9066f57419cbe
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-04-17 21:34:42 +0530

    17 April 2023

---
+++ LeetCode/1431_Kids_With_the_Greatest_Number_of_Candies.cpp
@@ -0,0 +1,27 @@
+// 17 April 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        
+        int greater = *max_element(candies.begin(),candies.end());   
+        vector<bool> v;
+        
+        for(int i = 0 ; i < candies.size(); i++){
+            if(candies[i]+extraCandies >= greater)
+            {
+                v.push_back(true);
+            }
+            else{
+                v.push_back(false);
+            }
+        }
+        
+        return v;
+        
+        
+    }
+};

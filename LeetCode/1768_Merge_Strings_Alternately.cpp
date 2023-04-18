25f38f0bbdccce9be2697327c90c2ecef722c6cd
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-04-18 09:14:50 +0530

    18 April 2023

---
+++ LeetCode/1768_Merge_Strings_Alternately.cpp
@@ -0,0 +1,63 @@
+// 18 April 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    // string mergeAlternately(string word1, string word2) {
+
+    //     string s;
+
+    //     int n = word1.size();
+    //     int m = word2.size();
+
+    //     int i = 0;
+    //     int j = 0;
+
+    //     while(i < n || j < m){
+    //         if(i < n)
+    //         {
+    //             s += word1[i++];
+    //         }
+    //         if(j < m)
+    //         {
+    //             s += word2[j++];
+    //         }
+    //     }
+
+
+    //     return s;
+
+    // }
+
+    string mergeAlternately(string word1, string word2) {
+
+        string s;
+
+        int n = word1.size();
+        int m = word2.size();
+
+        int i = 0;
+        int j = 0;
+
+        while(i < n && j < m){
+            s += word1[i++];
+            s += word2[j++];
+        }
+
+        while(i < n)
+        {
+            s += word1[i++];
+        }
+
+        while(j < m)
+        {
+            s += word2[j++];
+        } 
+
+        return s;
+
+    }
+};

6d81114276d6af97ba26dd897d81f59f24917459
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-04-20 23:06:36 +0530

    20 April 2023

--- LeetCode/997_Find_the_Town_Judge.cpp
+++ LeetCode/997_Find_the_Town_Judge.cpp
@@ -3,57 +3,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-  // int findJudge(int n, vector<vector<int>>& trust) {
+    // int findJudge(int n, vector<vector<int>>& trust) {
 
-  //       vector<int> adj(n+1,0);
+    //       vector<int> adj(n+1,0);
 
-  //       for(int i = 0 ; i < trust.size() ; i++)
-  //       {
-  //         adj[trust[i][1]]++; 
-  //         adj[trust[i][0]]--; 
-  //       }
+    //       for(int i = 0 ; i < trust.size() ; i++)
+    //       {
+    //         adj[trust[i][1]]++;
+    //         adj[trust[i][0]]--;
+    //       }
 
-  //       int judge = -1;
+    //       int judge = -1;
 
-  //       for(int i = 1; i<adj.size(); i++)
-  //       {
-  //         cout<<i<<" "<<adj[i]<<endl;
-  //         if(adj[i] == n-1)
-  //         {
-  //           judge = i;
-  //         }
-  //       }
+    //       for(int i = 1; i<adj.size(); i++)
+    //       {
+    //         cout<<i<<" "<<adj[i]<<endl;
+    //         if(adj[i] == n-1)
+    //         {
+    //           judge = i;
+    //         }
+    //       }
 
-  //     cout<<"n = "<<n<<endl;
+    //     cout<<"n = "<<n<<endl;
 
-  //       return judge;
+    //       return judge;
 
-  //   }
-      int findJudge(int n, vector<vector<int>>& trust) {
+    //   }
+    int findJudge(int n, vector<vector<int>> &trust)
+    {
 
-        vector<int> inbound(n+1,0);
+        vector<int> inbound(n + 1, 0);
 
-        for(int i = 0 ; i < trust.size() ; i++)
+        for (int i = 0; i < trust.size(); i++)
         {
-          inbound[trust[i][0]]--;
-          inbound[trust[i][1]]++;
+            inbound[trust[i][0]]--;
+            inbound[trust[i][1]]++;
         }
 
-        for(int i = 1 ; i < n+1; i++)
+        for (int i = 1; i < n + 1; i++)
         {
-          if(inbound[i] == n-1)
-          {
-            return i;
-          }
+            if (inbound[i] == n - 1)
+            {
+                return i;
+            }
         }
 
         return -1;
-
     }
-    
- 
+
     // int findJudge(int n, vector<vector<int>>& trust) {
 
     //     vector<int> adj[n+2];

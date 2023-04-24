2ce043649d49c2686b6656b779cab7189e5e4389
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-04-24 09:36:46 +0530

    24 April 2023

---
+++ LeetCode/1046_Last_Stone_Weight.cpp
@@ -0,0 +1,37 @@
+// 24 April 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        
+        priority_queue<int> pq(stones.begin(),stones.end());
+
+        while(!pq.empty())
+        {
+            int x = pq.top();
+            pq.pop();
+
+            if(pq.empty())
+            {
+                return x;
+            }
+
+            int y = pq.top();
+            pq.pop();
+
+             if(y <= x)
+            {
+                if(y != x)
+                {
+                    pq.push(x-y);
+                }
+            }
+
+        }
+
+        return 0;
+    }
+};

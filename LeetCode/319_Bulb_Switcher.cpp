7a8c006b11fd78b98ed45556c6eb274cab085a2d
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-04-27 09:57:36 +0530

    28 April 2023

---
+++ LeetCode/319_Bulb_Switcher.cpp
@@ -0,0 +1,47 @@
+// 28 April 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // int bulbSwitch(int n) {
+        
+    //     if(n == 0 || n == 1)
+    //     {
+    //         return n;
+    //     }
+
+    //     vector<int> v(n,1);
+
+    //     for(int i = 1 ; i < n ; i++)
+    //     {
+    //         int j = i;
+
+    //         while(j < n)
+    //         {
+    //             v[j] = !v[j];
+    //             j += i+1;
+    //         }
+    //     }
+
+    //     int count = 0;
+
+
+    //     for(int i = 0;  i < n ; i++)
+    //     {
+    //         if(v[i] == 1)
+    //         {
+    //             count++;
+    //         }
+    //     }
+
+    //     return count;
+    // }
+
+      int bulbSwitch(int n) {
+
+        return sqrt(n);  
+      
+    }
+};

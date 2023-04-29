8720f45394bf2e5906005457f70557852b6a813d
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-04-29 22:01:52 +0530

    29 April 2023

---
+++ LeetCode/2656_Maximum_Sum_With_Exactly_K_Elements.cpp
@@ -0,0 +1,22 @@
+// 29 April 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int maximizeSum(vector<int>& nums, int k) {
+        int sum = 0;
+
+        int m = *max_element(nums.begin(),nums.end());
+
+        while(k--)
+        {
+            sum += m;
+            m++;
+        }
+
+        return sum;
+    }
+};

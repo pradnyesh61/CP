55053b9a57cad070c2e0c4407fd5b9421ad0008f
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-05-02 10:36:46 +0530

    2 May 2023

--- LeetCode/1822_Sign_of_the_Product_of_an_Array.cpp
+++ LeetCode/1822_Sign_of_the_Product_of_an_Array.cpp
@@ -3,36 +3,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int arraySign(vector<int>& nums) {
-      
+    int arraySign(vector<int> &nums)
+    {
+
         int neg = 0;
-        for(int i =0 ; i <nums.size() ;i++)
+        for (int i = 0; i < nums.size(); i++)
         {
-            if(nums[i] == 0)
+            if (nums[i] == 0)
             {
                 return 0;
             }
 
-            if(nums[i] < 0)
+            if (nums[i] < 0)
             {
                 neg++;
-            }    
+            }
         }
 
-        if(neg%2 == 0)
+        if (neg % 2 == 0)
         {
             return 1;
         }
 
-        return -1;  
+        return -1;
     }
 
-    //so if you do product of integer as below it will overflow because it can reach 100 raise to 1000 so above approach is best
+    // so if you do product of integer as below it will overflow because it can reach 100 raise to 1000 so above approach is best
 
     //  int arraySign(vector<int>& nums) {
-        
+
     //     long long int prod = 1;
 
     //     for(int i =0 ; i <nums.size() ;i++)
@@ -45,7 +47,7 @@ public:
     //         return 1;
     //     }
     //     if(prod < 0)
-    //         return -1;    
+    //         return -1;
 
     //     return 0;
     // }

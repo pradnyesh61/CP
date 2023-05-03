0ab9c0fbc0097a07c83b2d544d423ff344fd4ac6
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-05-03 09:04:14 +0530

    3 May 2023

--- LeetCode/2215_Find_the_Difference_of_Two_Arrays.cpp
+++ LeetCode/2215_Find_the_Difference_of_Two_Arrays.cpp
@@ -3,11 +3,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
     // vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        
+
     //     vector<vector<int>> ans(2);
 
     //     for(int i = 0 ;i < nums1.size() ; i++)
@@ -19,50 +19,46 @@ public:
     //             if(find(ans[0].begin(),ans[0].end(),k) == ans[0].end())
     //             ans[0].push_back(k);
     //         }
-            
-           
+
     //     }
 
-        
     //     for(int i = 0 ;i < nums2.size() ; i++)
     //     {
     //         int k = nums2[i];
 
     //         if(find(nums1.begin(),nums1.end(),k) == nums1.end())
     //         {
-                
+
     //             if(find(ans[1].begin(),ans[1].end(),k) == ans[1].end())
     //             ans[1].push_back(k);
     //         }
-           
+
     //     }
 
     //     return ans;
     // }
 
-    vector<int> getelements(vector<int> &nums1,vector<int> &nums2)
+    vector<int> getelements(vector<int> &nums1, vector<int> &nums2)
     {
         unordered_set<int> s;
-        unordered_set<int> s2(nums2.begin(),nums2.end());
+        unordered_set<int> s2(nums2.begin(), nums2.end());
 
-        for(int i = 0 ;i < nums1.size() ; i++)
+        for (int i = 0; i < nums1.size(); i++)
         {
-            if(s2.find(nums1[i]) == s2.end())
+            if (s2.find(nums1[i]) == s2.end())
             {
                 s.insert(nums1[i]);
             }
         }
 
-        return vector<int> (s.begin(),s.end());
-       
+        return vector<int>(s.begin(), s.end());
     }
 
+    vector<vector<int>> findDifference(vector<int> &nums1, vector<int> &nums2)
+    {
 
-     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        
         vector<vector<int>> ans(2);
 
-        return {getelements(nums1,nums2), getelements (nums2,nums1)};
-        
+        return {getelements(nums1, nums2), getelements(nums2, nums1)};
     }
 };

2ff3064aad7d37841797a43ff4aae59a45c4a7a7
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-05-12 15:58:36 +0530

    12 May 2023

--- LeetCode/2140_Solving_Questions_With_Brainpower.cpp
+++ LeetCode/2140_Solving_Questions_With_Brainpower.cpp
@@ -3,57 +3,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-
-    long long recursion(int i , vector<vector<int>>& questions)
+    long long recursion(int i, vector<vector<int>> &questions)
     {
-        if(i >= questions.size())
+        if (i >= questions.size())
         {
             return 0;
         }
 
+        long long take = questions[i][0] + recursion(i + questions[i][1] + 1, questions);
 
-        long long take = questions[i][0] + recursion(i+questions[i][1]+1,questions);
+        long long notTake = recursion(i + 1, questions);
 
-        long long notTake = recursion(i+1,questions);
-
-        return max(take,notTake);
+        return max(take, notTake);
     }
 
-     long long memoization(int i , vector<vector<int>>& questions,vector<long long> &dp)
+    long long memoization(int i, vector<vector<int>> &questions, vector<long long> &dp)
     {
 
-        if(i >= questions.size())
+        if (i >= questions.size())
         {
             return 0;
         }
 
-
-        if(dp[i] != -1)
+        if (dp[i] != -1)
         {
             return dp[i];
         }
 
+        long long take = questions[i][0] + memoization(i + questions[i][1] + 1, questions, dp);
 
-        long long take = questions[i][0] + memoization(i+questions[i][1]+1,questions,dp);
+        long long notTake = memoization(i + 1, questions, dp);
 
-        long long notTake = memoization(i+1,questions,dp);
-
-        return dp[i] = max(take,notTake);
+        return dp[i] = max(take, notTake);
     }
 
-   
+    long long mostPoints(vector<vector<int>> &questions)
+    {
 
-   
+        vector<long long> dp(questions.size(), -1);
+        // return recursion(0,questions);
 
-    long long mostPoints(vector<vector<int>>& questions) {
-
-        vector<long long> dp(questions.size(),-1);
-        //return recursion(0,questions);
-       
-        return memoization(0,questions,dp);
-
+        return memoization(0, questions, dp);
     }
 };

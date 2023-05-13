810840dfa276ac7887587fc00a5e6eba80961cb8
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-05-13 23:18:02 +0530

    13 May 2023

---
+++ LeetCode/2678_Number_of_Senior_Citizens.cpp
@@ -0,0 +1,28 @@
+// 13 May 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        
+        int count = 0; 
+
+        for(int i = 0; i < details.size(); i++)
+        {
+            string s =  details[i];
+
+            int age =  (s[11]-'0')*10 + (s[12]-'0');
+         
+             if(age > 60)
+            {
+                count++;
+            }
+
+        }
+
+        return count;
+    }
+};

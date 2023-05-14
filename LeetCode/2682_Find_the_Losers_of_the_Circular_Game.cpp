5d89100f9c5cef2c3c0dc8f943352104f1d1568e
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-05-14 20:19:38 +0530

    14 May 2023

--- LeetCode/2682_Find_the_Losers_of_the_Circular_Game.cpp
+++ LeetCode/2682_Find_the_Losers_of_the_Circular_Game.cpp
@@ -3,39 +3,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-    
-     vector<int> circularGameLosers(int n, int k) {
-        
-        vector<int> vis(n,0);
+    vector<int> circularGameLosers(int n, int k)
+    {
+
+        vector<int> vis(n, 0);
 
         int i = 0;
         int j = 1;
 
-        while( true )
+        while (true)
         {
-            if(vis[i] == 1)
+            if (vis[i] == 1)
             {
                 break;
             }
             vis[i] = 1;
-            
+
             int temp = i + (j * k);
             j++;
 
             i = temp % n; // circular queue logic
-           
-
         }
         vector<int> ans;
 
-        for(int q = 0 ; q < vis.size() ; q++)
+        for (int q = 0; q < vis.size(); q++)
         {
-            if(vis[q] == 0)
+            if (vis[q] == 0)
             {
-                ans.push_back(q+1);
+                ans.push_back(q + 1);
             }
         }
 

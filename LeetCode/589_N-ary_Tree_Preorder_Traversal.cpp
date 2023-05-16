d4e35d7c413047758f783a1b5c29578133748c56
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-05-16 23:21:03 +0530

    16 May 2023

---
+++ LeetCode/589_N-ary_Tree_Preorder_Traversal.cpp
@@ -0,0 +1,55 @@
+// 16 May 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+
+private:
+    vector<int> ans;
+
+public:
+
+    void preorde(Node* root)
+    {
+        if(root == NULL)
+        {
+            return ;
+        }
+
+        ans.push_back(root->val);
+        
+        for(int i = 0; i < root->children.size() ; i++)
+        {
+            //cout<<root->children[i]<<endl;
+            preorde(root->children[i]);
+        }
+       
+    }
+
+    vector<int> preorder(Node* root) {
+        preorde(root);
+        return ans;
+    }
+};

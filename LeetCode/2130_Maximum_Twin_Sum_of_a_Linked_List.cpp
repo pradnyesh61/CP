84200af7d1d102c197c200c6fa7230f2666fe02a
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-05-17 08:06:32 +0530

    17 May 2023

---
+++ LeetCode/2130_Maximum_Twin_Sum_of_a_Linked_List.cpp
@@ -0,0 +1,44 @@
+// 17 May 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+
+private:
+vector<int> nums;
+
+public:
+
+    int pairSum(ListNode* head) {
+
+        while(head != NULL)
+        {
+            nums.push_back(head->val);
+            head = head->next;
+        }
+
+        int maxi = 0;
+        int n = nums.size();
+        for(int i = 0 ; i < n/2 ; i++)
+        {
+            if(nums[i]+ nums[n-i-1] > maxi)
+            {
+                maxi = nums[i]+ nums[n-i-1] ;
+            }
+        }
+
+        return maxi;
+        
+    }
+};

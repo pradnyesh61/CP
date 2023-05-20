c9aa4a7bc31c6f05440014b96c5b78aca8261aa5
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-05-20 22:26:50 +0530

    20 May 2023

--- LeetCode/113_Path_Sum_II.cpp
+++ LeetCode/113_Path_Sum_II.cpp
@@ -14,20 +14,21 @@ using namespace std;
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
+    void path(TreeNode *root, int targetSum, int sum, vector<int> temp,
+              vector<vector<int>> &ans)
+    {
 
-    void path(TreeNode* root, int targetSum,int sum,vector<int> temp,
-                vector<vector<int>> &ans) {
-
-        if(root == NULL)
+        if (root == NULL)
         {
-            return ;
+            return;
         }
 
-        if(root->left == NULL && root->right == NULL)
+        if (root->left == NULL && root->right == NULL)
         {
-            if(sum+root->val == targetSum)
+            if (sum + root->val == targetSum)
             {
                 temp.push_back(root->val);
                 ans.push_back(temp);
@@ -38,28 +39,27 @@ public:
         temp.push_back(root->val);
         sum += root->val;
 
-        path(root->left,targetSum, sum, temp,ans);
+        path(root->left, targetSum, sum, temp, ans);
 
-
-        path(root->right,targetSum, sum, temp,ans);
+        path(root->right, targetSum, sum, temp, ans);
 
         temp.pop_back();
         sum -= root->val;
 
-        return;    
-
+        return;
     }
 
-    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        
+    vector<vector<int>> pathSum(TreeNode *root, int targetSum)
+    {
+
         vector<vector<int>> a;
-        if(root == NULL)
+        if (root == NULL)
         {
             return a;
         }
         vector<vector<int>> ans;
         vector<int> temp;
-        path(root,targetSum,0,temp,ans);
+        path(root, targetSum, 0, temp, ans);
         return ans;
     }
 };

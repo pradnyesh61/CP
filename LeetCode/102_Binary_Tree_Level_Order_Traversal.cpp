dcdd4c7d57ffca3b48d2ba76f6e513dc11e8788b
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-05-24 12:44:43 +0530

    24 May 2023

--- LeetCode/102_Binary_Tree_Level_Order_Traversal.cpp
+++ LeetCode/102_Binary_Tree_Level_Order_Traversal.cpp
@@ -3,51 +3,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
- struct TreeNode {
-      int val;
-     TreeNode *left;
-      TreeNode *right;
-     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-  };
- 
-class Solution {
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
 
 private:
     vector<vector<int>> order;
 
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        
-        if(root == NULL)
+    vector<vector<int>> levelOrder(TreeNode *root)
+    {
+
+        if (root == NULL)
         {
             return order;
         }
 
-        queue<TreeNode*> q;
+        queue<TreeNode *> q;
         q.push(root);
 
-        while(!q.empty())
+        while (!q.empty())
         {
             int k = q.size();
             vector<int> temp;
-            for(int i = 0 ; i <k ; i++)
+            for (int i = 0; i < k; i++)
             {
-                TreeNode* node = q.front();
+                TreeNode *node = q.front();
                 q.pop();
-                if(node != NULL){
+                if (node != NULL)
+                {
                     temp.push_back(node->val);
-                
-                    if(node->left != NULL)
-                    q.push(node->left);
-                    if(node->right != NULL)
-                    q.push(node->right);
+
+                    if (node->left != NULL)
+                        q.push(node->left);
+                    if (node->right != NULL)
+                        q.push(node->right);
                 }
             }
 
             order.push_back(temp);
-
         }
         return order;
     }

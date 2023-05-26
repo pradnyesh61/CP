82c81478f13afe4cf0bfd7f56861733725103c32
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-05-26 22:38:31 +0530

    26 May 2023

---
+++ LeetCode/637_Average_of_Levels_in_Binary_Tree.cpp
@@ -0,0 +1,59 @@
+// 26 May 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+private:
+vector<double> ans;
+
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+       if(root == NULL)
+       {
+           return ans;
+       } 
+
+       queue<TreeNode*> q;
+       q.push(root);
+
+       while(!q.empty())
+       {
+           int k = q.size();
+           double sum = 0;
+
+           for(int i = 0 ; i < k ; i++)
+           {
+                TreeNode* temp = q.front();
+               q.pop();
+
+               sum += temp->val;
+
+               if(temp->left != NULL)
+               {
+                   q.push(temp->left);
+               }
+               if(temp->right != NULL)
+               {
+                   q.push(temp->right);
+               }
+           }
+           ans.push_back((sum/k));
+       }
+
+       return ans;
+
+    }
+};

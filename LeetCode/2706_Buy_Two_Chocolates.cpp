6a0ef350da95a0cfabe4226d8e8236f84740fda6
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-05-27 22:24:31 +0530

    27 May 2023

--- LeetCode/2706_Buy_Two_Chocolates.cpp
+++ LeetCode/2706_Buy_Two_Chocolates.cpp
@@ -3,15 +3,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int buyChoco(vector<int>& prices, int money) {
-        
-        sort(prices.begin(),prices.end());
+    int buyChoco(vector<int> &prices, int money)
+    {
 
-        if(prices[0]+prices[1] <= money)
+        sort(prices.begin(), prices.end());
+
+        if (prices[0] + prices[1] <= money)
         {
-            return money - (prices[0]+prices[1]);
+            return money - (prices[0] + prices[1]);
         }
 
         return money;

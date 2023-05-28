3713d276c567d73dad8471816a519904b1ce489c
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-05-28 21:12:10 +0530

    28 May 2023

---
+++ LeetCode/2710_Remove_Trailing_Zeros_From_a_String.cpp
@@ -0,0 +1,22 @@
+// 28 May 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string removeTrailingZeros(string num) {
+
+        for(int i = num.size()-1; i>= 0; i--)
+        {
+            if(num[i] != '0')
+            {
+                break;
+            }
+
+            num.pop_back();
+        }   
+
+        return num;
+    }
+};

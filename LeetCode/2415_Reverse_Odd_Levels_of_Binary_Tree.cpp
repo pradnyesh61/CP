ad6f118a78c034b444d641571a54949745ff79e2
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-02 23:16:50 +0530

    2 June 2023

--- LeetCode/2415_Reverse_Odd_Levels_of_Binary_Tree.cpp
+++ LeetCode/2415_Reverse_Odd_Levels_of_Binary_Tree.cpp
@@ -14,34 +14,36 @@ using namespace std;
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:    
-    TreeNode* reverseOddLevels(TreeNode* root) {
-      
-      if(root == NULL)
-      {
-          return root;
-      }
+class Solution
+{
+public:
+    TreeNode *reverseOddLevels(TreeNode *root)
+    {
 
-      queue<TreeNode*> q;
-      q.push(root);
+        if (root == NULL)
+        {
+            return root;
+        }
 
-      int odd = 0;
+        queue<TreeNode *> q;
+        q.push(root);
 
-      while(!q.empty())
-      {
-          int sz = q.size();
-       //   cout<<q.front()->val<<endl;
+        int odd = 0;
 
-          if(odd%2 != 0)
-          {
-              queue<TreeNode*> q2(q);
-              vector<TreeNode*> v;
-              while(!q2.empty())
-              {
-                  v.push_back(q2.front());
-                  q2.pop();
-              }
+        while (!q.empty())
+        {
+            int sz = q.size();
+            //   cout<<q.front()->val<<endl;
+
+            if (odd % 2 != 0)
+            {
+                queue<TreeNode *> q2(q);
+                vector<TreeNode *> v;
+                while (!q2.empty())
+                {
+                    v.push_back(q2.front());
+                    q2.pop();
+                }
 
                 // for(auto it : v)
                 // {
@@ -49,35 +51,34 @@ public:
                 // }
                 // cout<<endl<<"size = "<<v.size()<<endl;
 
-              for(int i = 0; i < v.size()/2 ; i++)
-              {
-                  int temp = v[i]->val;
-              //  cout<<temp<<endl;
-               // cout<<v[i]->val<<" = "<<v[sz-i-1]->val<<endl;
-                  v[i]->val = v[sz-i-1]->val;
-                  v[sz-i-1]->val = temp;
-                //  cout<<v[i]->val<<" = "<<v[sz-i-1]->val<<endl;
-              }
-          }
-        // cout<<q.front()->val<<endl<<endl;
-          for(int i = 1 ; i <= sz ; i++)
-          {
-              TreeNode* temp = q.front();
-              q.pop();
+                for (int i = 0; i < v.size() / 2; i++)
+                {
+                    int temp = v[i]->val;
+                    //  cout<<temp<<endl;
+                    // cout<<v[i]->val<<" = "<<v[sz-i-1]->val<<endl;
+                    v[i]->val = v[sz - i - 1]->val;
+                    v[sz - i - 1]->val = temp;
+                    //  cout<<v[i]->val<<" = "<<v[sz-i-1]->val<<endl;
+                }
+            }
+            // cout<<q.front()->val<<endl<<endl;
+            for (int i = 1; i <= sz; i++)
+            {
+                TreeNode *temp = q.front();
+                q.pop();
 
-              if(temp->left != NULL)
-              {
-                  q.push(temp->left);
-              }
-              if(temp->right != NULL)
-              {
-                  q.push(temp->right);
-              }
-          }
-          odd++;
-      }
+                if (temp->left != NULL)
+                {
+                    q.push(temp->left);
+                }
+                if (temp->right != NULL)
+                {
+                    q.push(temp->right);
+                }
+            }
+            odd++;
+        }
 
-      return root;
-
+        return root;
     }
 };

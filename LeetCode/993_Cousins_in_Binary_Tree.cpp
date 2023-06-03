d04be0c95deee21ebfe4dafb9cedda71cdb9620a
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-03 22:34:09 +0530

    3 June 2023

--- LeetCode/993_Cousins_in_Binary_Tree.cpp
+++ LeetCode/993_Cousins_in_Binary_Tree.cpp
@@ -14,63 +14,63 @@ using namespace std;
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    bool isCousins(TreeNode* root, int x, int y) {
-        if(root == NULL)
+    bool isCousins(TreeNode *root, int x, int y)
+    {
+        if (root == NULL)
         {
             return false;
         }
 
-        queue<pair<TreeNode*,TreeNode*>> q;
-        q.push({root,root});
+        queue<pair<TreeNode *, TreeNode *>> q;
+        q.push({root, root});
 
-        TreeNode* a1 = NULL;
-        TreeNode* b1 = NULL;
-         
-        bool a=false,b= false;
+        TreeNode *a1 = NULL;
+        TreeNode *b1 = NULL;
 
-        while(!q.empty())
+        bool a = false, b = false;
+
+        while (!q.empty())
         {
             int sz = q.size();
 
-            while(sz--)
+            while (sz--)
             {
-                pair<TreeNode*,TreeNode*> temp = q.front();
+                pair<TreeNode *, TreeNode *> temp = q.front();
                 q.pop();
 
-                if(temp.second->val == x && a == false)
+                if (temp.second->val == x && a == false)
                 {
                     a1 = temp.first;
                     a = true;
                 }
 
-                
-                if(temp.second->val == y && b == false)
+                if (temp.second->val == y && b == false)
                 {
                     b1 = temp.first;
                     b = true;
                 }
 
-
-                if(temp.second->left != NULL)
+                if (temp.second->left != NULL)
                 {
-                    q.push({temp.second,temp.second->left});
+                    q.push({temp.second, temp.second->left});
                 }
-                
-                if(temp.second->right != NULL)
+
+                if (temp.second->right != NULL)
                 {
-                    q.push({temp.second,temp.second->right});
+                    q.push({temp.second, temp.second->right});
                 }
             }
 
-            if(a && b)
+            if (a && b)
             {
-                if(a1 != b1 && a1 !=NULL && b1 != NULL)
+                if (a1 != b1 && a1 != NULL && b1 != NULL)
                     return true;
                 return false;
             }
-            else if(a || b)
+            else if (a || b)
             {
                 return false;
             }

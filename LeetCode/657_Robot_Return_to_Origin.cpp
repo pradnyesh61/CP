5a442142b7d5da91eced15886073c0cb291c9979
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-04 22:09:16 +0530

    4 June 2023

--- LeetCode/657_Robot_Return_to_Origin.cpp
+++ LeetCode/657_Robot_Return_to_Origin.cpp
@@ -3,35 +3,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool judgeCircle(string moves) {
-        
-        pair<int,int> p(0,0);
-       
+    bool judgeCircle(string moves)
+    {
 
-        for(int i = 0; i  <moves.size(); i++)
+        pair<int, int> p(0, 0);
+
+        for (int i = 0; i < moves.size(); i++)
         {
-            if(moves[i] == 'U')
+            if (moves[i] == 'U')
             {
                 p.first--;
             }
-            else if(moves[i] == 'D')
+            else if (moves[i] == 'D')
             {
                 p.first++;
             }
-            else if(moves[i] == 'L')
+            else if (moves[i] == 'L')
             {
                 p.second--;
             }
-            else if(moves[i] == 'R')
+            else if (moves[i] == 'R')
             {
                 p.second++;
             }
         }
 
-        if(p.first == 0 && p.second == 0)
-        return true;
+        if (p.first == 0 && p.second == 0)
+            return true;
 
         return false;
     }

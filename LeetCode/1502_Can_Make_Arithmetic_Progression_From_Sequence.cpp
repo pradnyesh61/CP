4c17b39a18ea78842baa2877d67019008d8a5d7b
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-06 11:11:38 +0530

    6 June 2023

--- LeetCode/1502_Can_Make_Arithmetic_Progression_From_Sequence.cpp
+++ LeetCode/1502_Can_Make_Arithmetic_Progression_From_Sequence.cpp
@@ -3,16 +3,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool canMakeArithmeticProgression(vector<int>& arr) {
-        sort(arr.begin(),arr.end());
+    bool canMakeArithmeticProgression(vector<int> &arr)
+    {
+        sort(arr.begin(), arr.end());
 
-        int d = arr[1]-arr[0];
+        int d = arr[1] - arr[0];
 
-        for(int i = 2; i < arr.size() ;i++)
+        for (int i = 2; i < arr.size(); i++)
         {
-            if(arr[i]-arr[i-1] != d)
+            if (arr[i] - arr[i - 1] != d)
             {
                 return false;
             }

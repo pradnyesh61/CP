ec45772dc66d67bd24044b524f15b7067722b976
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-08 11:38:09 +0530

    8 June 2023

--- LeetCode/1351_Count_Negative_Numbers_in_a_Sorted_Matrix.cpp
+++ LeetCode/1351_Count_Negative_Numbers_in_a_Sorted_Matrix.cpp
@@ -2,22 +2,19 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-
-class Solution
-{
+class Solution {
 public:
-    int countNegatives(vector<vector<int>> &grid)
-    {
-
+    int countNegatives1(vector<vector<int>>& grid) {
+        
         int count = 0;
         int m = grid.size();
         int n = grid[0].size();
 
-        for (int i = m - 1; i >= 0; i--)
+        for(int i = m-1 ; i >= 0 ;i--)
         {
-            for (int j = n - 1; j >= 0; j--)
+            for(int j = n-1 ; j >= 0 ; j--)
             {
-                if (grid[i][j] >= 0)
+                if(grid[i][j] >= 0)
                 {
                     break;
                 }
@@ -25,6 +22,43 @@ public:
             }
         }
 
+        return count;
+    }
+
+     int countNegatives(vector<vector<int>>& grid) {
+        
+        int count = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+
+
+        for(int i = 0 ; i < m ;i++)
+        {
+            int start =0;
+            int end = n-1;
+
+            if(grid[i][start] < 0)
+            {
+                count += n;
+                continue;
+            }
+            while(start <= end)
+            {
+                int mid = (start+end)/2;
+                
+                if(grid[i][mid] < 0)
+                {
+                    end = mid-1;
+                }
+                else
+                if(grid[i][mid] >= 0)
+                {
+                    start = mid+1;
+                }
+            }
+            count += (n - end-1);
+            
+        }
         return count;
     }
 };

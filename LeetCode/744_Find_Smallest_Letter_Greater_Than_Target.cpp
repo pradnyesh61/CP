14b18b984dae129c5641c0e2c1222ebdc7239c71
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-09 09:08:51 +0530

    9 June 2023

--- LeetCode/744_Find_Smallest_Letter_Greater_Than_Target.cpp
+++ LeetCode/744_Find_Smallest_Letter_Greater_Than_Target.cpp
@@ -3,81 +3,76 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
+    char nextGreatestLetter(vector<char> &letters, char target)
+    {
 
-    char nextGreatestLetter(vector<char>& letters, char target) {
-        
         int n = letters.size();
-        
-        for(int i = 0 ; i < n ; i++)
+
+        for (int i = 0; i < n; i++)
         {
-            if(letters[i] == target)
+            if (letters[i] == target)
             {
-                if(i+1 < n && letters[i+1] != target)
+                if (i + 1 < n && letters[i + 1] != target)
                 {
-                    return letters[i+1];
+                    return letters[i + 1];
                 }
             }
-            if(letters[i] > target)
+            if (letters[i] > target)
             {
                 return letters[i];
             }
         }
         return letters[0];
     }
-    char nextGreatestLetter1(vector<char>& letters, char target) {
-        
+    char nextGreatestLetter1(vector<char> &letters, char target)
+    {
+
         int start = 0;
-        int end = letters.size()-1;
-        cout<<start<<" = "<<end<<endl;
-        while(start < end)
+        int end = letters.size() - 1;
+        cout << start << " = " << end << endl;
+        while (start < end)
         {
-            int mid = (start+end)/2;
-            cout<<mid<<endl;
-            if(letters[mid] == target)
+            int mid = (start + end) / 2;
+            cout << mid << endl;
+            if (letters[mid] == target)
             {
-                cout<<"Hi"<<endl;
-                if(mid+1 < letters.size())
+                cout << "Hi" << endl;
+                if (mid + 1 < letters.size())
                 {
-                    return letters[mid+1];
+                    return letters[mid + 1];
                 }
-            } 
-            else
-            if(letters[mid] < target)
-            {
-                cout<<"start"<<endl;
-                start = mid + 1 ;
             }
-            else
-            if(letters[mid] > target)
+            else if (letters[mid] < target)
             {
-                cout<<"end"<<endl;
+                cout << "start" << endl;
+                start = mid + 1;
+            }
+            else if (letters[mid] > target)
+            {
+                cout << "end" << endl;
                 end = mid - 1;
             }
-             cout<<start<<" = "<<end<<endl;
-       
+            cout << start << " = " << end << endl;
         }
 
-        if(start < letters.size())
+        if (start < letters.size())
         {
-            if(letters[start] <= target)
+            if (letters[start] <= target)
             {
-                if(start+1 < letters.size())
+                if (start + 1 < letters.size())
                 {
-                    return letters[start+1];
+                    return letters[start + 1];
                 }
             }
 
-            if(letters[start] > target)
+            if (letters[start] > target)
             {
-                
+
                 return letters[start];
             }
-            
-          
-            
-            
         }
 
         // if( start == end && end == letters.size()-1)
@@ -89,7 +84,7 @@ public:
         //     return letters[0];
 
         // }
-        
+
         return letters[0];
     }
 };

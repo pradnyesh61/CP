f4c768908b58fbbf6bab047dffb28697c0a144f2
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-10 23:22:42 +0530

    10 June 2023

--- LeetCode/2729_Check_if_The_Number_is_Fascinating.cpp
+++ LeetCode/2729_Check_if_The_Number_is_Fascinating.cpp
@@ -3,31 +3,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-
-    void check(vector<int> &v , int n)
+    void check(vector<int> &v, int n)
     {
-        cout<<n<<endl;
-        while(n)
+        cout << n << endl;
+        while (n)
         {
-            int k  = n%10;
+            int k = n % 10;
             v[k]++;
             n /= 10;
         }
     }
-     bool isFascinating(int n) {
+    bool isFascinating(int n)
+    {
 
-        
-       string s = to_string(n)+to_string(n*2)+to_string(n*3);
-        
-       int j = 1;
-       sort(s.begin(),s.end());
+        string s = to_string(n) + to_string(n * 2) + to_string(n * 3);
 
-        for(int i = 0; i < s.size() ; i++)
+        int j = 1;
+        sort(s.begin(), s.end());
+
+        for (int i = 0; i < s.size(); i++)
         {
-            if(s[i]-'0' != j)
+            if (s[i] - '0' != j)
             {
                 return false;
             }
@@ -35,29 +34,28 @@ public:
         }
 
         return true;
-
     }
 
-    bool isFascinating1(int n) {
-        
-        vector<int> v(10,0);
+    bool isFascinating1(int n)
+    {
 
-        int k = n*2;
-        int m = n*3;
+        vector<int> v(10, 0);
 
-        check(v,n);
-        check(v,n*2);
-        check(v,n*3);
+        int k = n * 2;
+        int m = n * 3;
 
-        for(int i = 1; i <= 9 ; i++)
+        check(v, n);
+        check(v, n * 2);
+        check(v, n * 3);
+
+        for (int i = 1; i <= 9; i++)
         {
-            if(v[i] != 1)
+            if (v[i] != 1)
             {
                 return false;
             }
         }
 
         return true;
-
     }
 };

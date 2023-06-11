0e519cc3fc22b451e106abca0435e66985c0cb54
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-11 22:22:26 +0530

    11 June 2023

---
+++ LeetCode/2733_Neither_Minimum_nor_Maximum.cpp
@@ -0,0 +1,19 @@
+// 11 June 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int findNonMinOrMax(vector<int>& nums) {
+        
+        if(nums.size()<= 2)
+        {
+            return -1;
+        }
+
+        sort(nums.begin(),nums.end());
+        return nums[1];
+    }
+};

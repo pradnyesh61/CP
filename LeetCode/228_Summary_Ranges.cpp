793e7231ad77763de856d884fb608a2fee4fd1f1
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-12 07:32:16 +0530

    12 June 2023

---
+++ LeetCode/228_Summary_Ranges.cpp
@@ -0,0 +1,54 @@
+// 12 June 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> v;
+    
+        if(nums.size() == 0)
+            return v;
+
+        int a = nums[0];
+        int j = a;
+        int b = a;
+       int start = a;
+        nums.push_back(a);
+        
+        for(int i = 0 ; i < nums.size() ; i++)
+        {
+            a = nums[i];
+            
+            if(a != j)
+            {
+                string s = "";
+                if(start == b)
+                {
+                    s = to_string(start);
+                }
+                else
+                {
+                    s = to_string(start)+"->"+to_string(b);
+                }
+
+                v.push_back(s);
+                start = nums[i];
+                b = start;
+               j = start;
+               if(j < INT_MAX)
+               j++;
+            }
+            else
+            {
+                b = j;
+                if(j < INT_MAX)
+                j++;
+            }
+        }
+
+        return v;
+    }
+};

b7815080280c87c5fdb7c45ba965634087284ad8
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-15 07:46:26 +0530

    15 June 2023

---
+++ LeetCode/1161_Maximum_Level_Sum_of_a_Binary_Tree.cpp
@@ -0,0 +1,59 @@
+// 15 June 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+       
+        int maxi = root->val;
+        int level = 1;
+        int lcount = 0;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty())
+        {
+            int sz = q.size();
+            int sum = 0;
+            while(sz--)
+            {
+                TreeNode* temp = q.front();
+                q.pop();
+
+                sum += temp->val;
+
+                if(temp->left != NULL)
+                {
+                    q.push(temp->left);
+                }
+
+                if(temp->right != NULL)
+                {
+                    q.push(temp->right);
+                }
+            }
+            lcount++;
+            if(maxi < sum)
+            {
+                maxi = sum;
+                level = lcount;
+            }
+        }
+
+        return level;
+    }
+};

e987da3b77c4632b1ffb30bfc5902596ac905ef1
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-16 20:10:01 +0530

    16 June 2023

---
+++ LeetCode/590_N-ary_Tree_Postorder_Traversal.cpp
@@ -0,0 +1,68 @@
+// 16 June 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+
+vector<int> v;
+public:
+
+
+    void postorder1(Node* root)
+    {
+         if(root == NULL)
+        {
+            return ;
+        }
+
+        int sz = root->children.size();
+
+        if(sz == 0)
+        {
+            v.push_back(root->val);
+            return;
+        }
+
+        for(int i = 0 ; i < sz ; i++)
+        {   
+            postorder1(root->children[i]);
+        }
+        v.push_back(root->val);
+       
+    }
+
+    vector<int> postorder(Node* root) {
+      
+
+       if(root == NULL)
+       {
+           return v;
+       }
+
+       postorder1(root);
+
+       return v;
+
+    }
+};

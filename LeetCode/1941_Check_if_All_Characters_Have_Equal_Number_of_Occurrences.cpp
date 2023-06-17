64d4faffb7ab81c7859ddd75f967e213ce0bc0c3
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-17 23:12:14 +0530

    17 June 2023

---
+++ LeetCode/1941_Check_if_All_Characters_Have_Equal_Number_of_Occurrences.cpp
@@ -0,0 +1,30 @@
+// 17 June 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool areOccurrencesEqual(string s) {
+        
+        unordered_map<char,int> m;
+
+        for(int i = 0; i< s.length(); i++)
+        {
+            m[s[i]]++;
+        }
+
+        int k = m[s[0]];
+
+        for(auto it : m)
+        {
+
+            if(it.second != k)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};

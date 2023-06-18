cd052e59e8fd4001237503e389d3d8422d25c477
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-18 21:35:32 +0530

    18 June 2023

---
+++ LeetCode/2739_Total_Distance_Traveled.cpp
@@ -0,0 +1,35 @@
+// 18 June 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int distanceTraveled(int mainTank, int additionalTank) {
+        
+        int km = 0;
+
+        while(mainTank)
+        {
+            if(mainTank >= 5)
+            {
+                km += (10*5);
+                mainTank -= 5;
+                if(additionalTank){
+                additionalTank--;
+                mainTank++;
+                }
+            }
+            else
+            {
+                km += (mainTank*10);
+                break;
+            }
+
+           // cout<<additionalTank<<" = "<<mainTank<<" = "<<km<<endl;
+        }
+
+        return km;
+    }
+};

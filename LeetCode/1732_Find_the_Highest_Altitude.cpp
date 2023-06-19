5b7c133515f916c6c0ad6b6c1c3dce7f87222cad
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-19 23:00:56 +0530

    17 June 2023

--- LeetCode/1732_Find_the_Highest_Altitude.cpp
+++ LeetCode/1732_Find_the_Highest_Altitude.cpp
@@ -3,40 +3,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-   /* int largestAltitude(vector<int>& gain) {
-        
-        gain.insert(gain.begin(),0);
-        
-        for(int i = 1 ; i < gain.size();i++)
-        {
-            gain[i] = gain[i-1]+gain[i];
-             
-            cout<<gain[i]<<endl;
-        }
-        
-        return *max_element(gain.begin(),gain.end());
-    
-    }
-    */
-    int largestAltitude(vector<int>& gain) {
-        
-       int max = 0; 
+    /* int largestAltitude(vector<int>& gain) {
+
+         gain.insert(gain.begin(),0);
+
+         for(int i = 1 ; i < gain.size();i++)
+         {
+             gain[i] = gain[i-1]+gain[i];
+
+             cout<<gain[i]<<endl;
+         }
+
+         return *max_element(gain.begin(),gain.end());
+
+     }
+     */
+    int largestAltitude(vector<int> &gain)
+    {
+
+        int max = 0;
         int sum = 0;
-        
-        for(int i = 0 ; i < gain.size();i++)
+
+        for (int i = 0; i < gain.size(); i++)
         {
             sum += gain[i];
-            
-            if(max < sum)
+
+            if (max < sum)
             {
                 max = sum;
             }
         }
-        
-        
+
         return max;
-     
     }
 };

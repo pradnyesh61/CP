1b1808dfd4d56820580f11c37c08bc7a6f319fc2
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-21 20:46:13 +0530

    21 June 2023

--- LeetCode/535_Encode_and_Decode_TinyURL.cpp
+++ LeetCode/535_Encode_and_Decode_TinyURL.cpp
@@ -3,17 +3,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-
     // Encodes a URL to a shortened URL.
-    string encode(string longUrl) {
-     return longUrl;
+    string encode(string longUrl)
+    {
+        return longUrl;
     }
 
     // Decodes a shortened URL to its original URL.
-    string decode(string shortUrl) {
+    string decode(string shortUrl)
+    {
         return shortUrl;
     }
 };

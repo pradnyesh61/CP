aa784e4bbe192be9c7e012ad13349241ed79fca5
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-22 22:45:23 +0530

    22 June 2023

--- LeetCode/714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.cpp
+++ LeetCode/714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.cpp
@@ -3,84 +3,83 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-
-
-    int  profit(int i,vector<int>& prices,int fee, int sum, int buy,int pro)
+    int profit(int i, vector<int> &prices, int fee, int sum, int buy, int pro)
     {
 
-        if(i >= prices.size())
+        if (i >= prices.size())
         {
             return pro;
         }
 
         int take = INT_MIN;
 
-        if(buy == -1 && i != prices.size()-1){
-            take = profit(i+1,prices,fee,sum+fee,prices[i],pro);
-        }
-        else if(buy != -1 && prices[i] > buy)
+        if (buy == -1 && i != prices.size() - 1)
         {
-                take = profit(i+1,prices,fee,sum+fee,-1,pro+(prices[i]-buy)-fee);
+            take = profit(i + 1, prices, fee, sum + fee, prices[i], pro);
         }
-        
+        else if (buy != -1 && prices[i] > buy)
+        {
+            take = profit(i + 1, prices, fee, sum + fee, -1, pro + (prices[i] - buy) - fee);
+        }
+
         int notTake = INT_MIN;
 
-        notTake = profit(i+1,prices,fee,sum,buy,pro); 
+        notTake = profit(i + 1, prices, fee, sum, buy, pro);
 
-        return max(take,notTake);
-
-
+        return max(take, notTake);
     }
 
-     int  memoization(int i,vector<int>& prices,int fee ,int buy,vector<vector<int>> dp)
+    int memoization(int i, vector<int> &prices, int fee, int buy, vector<vector<int>> dp)
     {
-        cout<<prices[i]<<endl;
-        if(i >= prices.size())
+        cout << prices[i] << endl;
+        if (i >= prices.size())
         {
             return 0;
         }
 
-        if(dp[i][buy] != -1)
+        if (dp[i][buy] != -1)
         {
             return dp[i][buy];
         }
 
         int profit = 0;
 
-        if(buy)
+        if (buy)
         {
-            profit = max(-prices[i]+memoization(i+1,prices,fee,0,dp) , 
-                                    memoization(i+1,prices,fee,1,dp));
+            profit = max(-prices[i] + memoization(i + 1, prices, fee, 0, dp),
+                         memoization(i + 1, prices, fee, 1, dp));
         }
-        else{
-            profit = max(prices[i]-fee + memoization(i+1,prices,fee,1,dp),
-                                        memoization(i+1,prices,fee,0,dp));
+        else
+        {
+            profit = max(prices[i] - fee + memoization(i + 1, prices, fee, 1, dp),
+                         memoization(i + 1, prices, fee, 0, dp));
         }
 
         return dp[i][buy] = profit;
-
     }
 
-    int tabulation(vector<int>& prices, int fee)
+    int tabulation(vector<int> &prices, int fee)
     {
-        vector<vector<int>> dp(prices.size()+1,vector<int>(2,0));
+        vector<vector<int>> dp(prices.size() + 1, vector<int>(2, 0));
 
-        for(int i = prices.size()-1; i >= 0 ; i--)
+        for (int i = prices.size() - 1; i >= 0; i--)
         {
-            for(int buy = 0; buy <= 1; buy ++)
-            {                
+            for (int buy = 0; buy <= 1; buy++)
+            {
                 int profit = 0;
 
-                 if(buy)
+                if (buy)
                 {
-                    profit = max(-prices[i]+dp[i+1][0] , 
-                                            dp[i+1][1] );
+                    profit = max(-prices[i] + dp[i + 1][0],
+                                 dp[i + 1][1]);
                 }
-                else{
-                    profit = max(prices[i]-fee + dp[i+1][1],
-                                                dp[i+1][0]);
+                else
+                {
+                    profit = max(prices[i] - fee + dp[i + 1][1],
+                                 dp[i + 1][0]);
                 }
 
                 dp[i][buy] = profit;
@@ -88,27 +87,27 @@ public:
         }
 
         return dp[0][1];
-
     }
 
-    int spaceOptimization(vector<int>& prices, int fee)
+    int spaceOptimization(vector<int> &prices, int fee)
     {
-        vector<int> ahead(2,0),cur(2,0);
+        vector<int> ahead(2, 0), cur(2, 0);
 
-        for(int i = prices.size()-1; i >= 0 ; i--)
+        for (int i = prices.size() - 1; i >= 0; i--)
         {
-            for(int buy = 0; buy <= 1; buy ++)
-            {                
+            for (int buy = 0; buy <= 1; buy++)
+            {
                 int profit = 0;
 
-                 if(buy)
+                if (buy)
                 {
-                    profit = max(-prices[i]+ahead[0] , 
-                                            ahead[1] );
+                    profit = max(-prices[i] + ahead[0],
+                                 ahead[1]);
                 }
-                else{
-                    profit = max(prices[i]-fee + ahead[1],
-                                                ahead[0]);
+                else
+                {
+                    profit = max(prices[i] - fee + ahead[1],
+                                 ahead[0]);
                 }
 
                 cur[buy] = profit;
@@ -120,13 +119,14 @@ public:
         return ahead[1];
     }
 
-    int maxProfit(vector<int>& prices, int fee) {
-        return spaceOptimization(prices,fee);
+    int maxProfit(vector<int> &prices, int fee)
+    {
+        return spaceOptimization(prices, fee);
 
-        vector<vector<int>> dp(prices.size()+1,vector<int> (prices.size()+1,-1));
-        
-        return tabulation(prices,fee);
-        return memoization(0,prices,fee,1,dp);
-     //   return profit(0,prices,fee,0,-1,0);
+        vector<vector<int>> dp(prices.size() + 1, vector<int>(prices.size() + 1, -1));
+
+        return tabulation(prices, fee);
+        return memoization(0, prices, fee, 1, dp);
+        //   return profit(0,prices,fee,0,-1,0);
     }
 };

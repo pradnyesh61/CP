b5352203be4cb96a85fc4ab4dff645bc76971651
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-23 23:11:15 +0530

    23 June 2023

--- LeetCode/583_Delete_Operation_for_Two_Strings.cpp
+++ LeetCode/583_Delete_Operation_for_Two_Strings.cpp
@@ -3,43 +3,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-
-    int lcs(int i, int j, string s1,string s2,vector<vector<int>>&dp)
+    int lcs(int i, int j, string s1, string s2, vector<vector<int>> &dp)
     {
 
-        if(i < 0 || j < 0)
+        if (i < 0 || j < 0)
         {
             return 0;
         }
 
-        if(dp[i][j] != -1)
+        if (dp[i][j] != -1)
         {
             return dp[i][j];
         }
 
-        if(s1[i] == s2[j])
+        if (s1[i] == s2[j])
         {
-            return dp[i][j] =  1 + lcs(i-1,j-1,s1,s2,dp);
+            return dp[i][j] = 1 + lcs(i - 1, j - 1, s1, s2, dp);
         }
-        
-        return dp[i][j] =max(lcs(i-1,j,s1,s2,dp),lcs(i,j-1,s1,s2,dp));
 
+        return dp[i][j] = max(lcs(i - 1, j, s1, s2, dp), lcs(i, j - 1, s1, s2, dp));
     }
 
+    int minDistance(string word1, string word2)
+    {
 
-
-
-    int minDistance(string word1, string word2) {
-        
         int m = word1.size();
         int n = word2.size();
 
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        int count =  lcs(m-1,n-1, word1,word2,dp);
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        int count = lcs(m - 1, n - 1, word1, word2, dp);
 
-        return (m-count)+(n-count);
+        return (m - count) + (n - count);
     }
 };

6aa4677e1dea77aae63faf4f20e0179a831b24bb
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-24 23:07:55 +0530

    24 June 2023

--- LeetCode/796_Rotate_String.cpp
+++ LeetCode/796_Rotate_String.cpp
@@ -3,19 +3,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-
-    bool check(int k, string s,string goal)
+    bool check(int k, string s, string goal)
     {
-        for(int i = 0;i < s.length();i++)
+        for (int i = 0; i < s.length(); i++)
         {
-            if(s[i] != goal[k++])
+            if (s[i] != goal[k++])
             {
                 return false;
             }
 
-            if(k == goal.size())
+            if (k == goal.size())
             {
                 k = 0;
             }
@@ -23,20 +23,20 @@ public:
         return true;
     }
 
+    bool rotateString(string s, string goal)
+    {
 
-    bool rotateString(string s, string goal) {
-        
-        if(s.size() != goal.size())
+        if (s.size() != goal.size())
         {
             return false;
         }
         char c = s[0];
-        
-        for(int i= 0 ; i <goal.size() ; i++)
+
+        for (int i = 0; i < goal.size(); i++)
         {
-            if(goal[i] == c)
+            if (goal[i] == c)
             {
-                if(check(i,s,goal))
+                if (check(i, s, goal))
                 {
                     return true;
                 }
@@ -44,7 +44,5 @@ public:
         }
 
         return false;
-
-       
     }
 };

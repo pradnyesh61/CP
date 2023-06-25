5651cec8d2cacabd3be5c3e1220e1f0d016dd432
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-25 21:41:48 +0530

    25 June 2023

---
+++ LeetCode/2748_Number_of_Beautiful_Pairs.cpp
@@ -0,0 +1,28 @@
+// 25 June 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countBeautifulPairs(vector<int>& nums) {
+        
+        int count = 0;
+        int n = nums.size();
+
+        
+        for(int i = 0; i < n-1 ; i++)
+        {
+            for(int j = i+1 ; j < n; j++)
+            {
+                string s = to_string(nums[i]);
+                if(gcd(s[0]-'0',nums[j]%10) == 1)
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};

ffd5bca311ff36c5869228ef46d5807bf6fc5d46
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-29 21:05:59 +0530

    29 June 2023

--- LeetCode/872_Leaf_Similar_Trees.cpp
+++ LeetCode/872_Leaf_Similar_Trees.cpp
@@ -14,48 +14,48 @@ using namespace std;
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
+    vector<int> v1, v2;
 
-vector<int> v1,v2;
-
-void first(TreeNode* root)
-{
-    if(root == NULL)
+    void first(TreeNode *root)
     {
-        return ;
+        if (root == NULL)
+        {
+            return;
+        }
+
+        if (root->left == NULL && root->right == NULL)
+            v1.push_back(root->val);
+
+        first(root->left);
+        first(root->right);
+
+        return;
     }
 
-    if(root->left == NULL && root->right == NULL)
-        v1.push_back(root->val);
-        
-    first(root->left);
-    first(root->right);
-
-    return ;
-}
-
-
-void second(TreeNode* root)
-{
-    if(root == NULL)
+    void second(TreeNode *root)
     {
-        return ;
+        if (root == NULL)
+        {
+            return;
+        }
+
+        if (root->left == NULL && root->right == NULL)
+            v2.push_back(root->val);
+        second(root->left);
+        second(root->right);
+
+        return;
     }
 
-    if(root->left == NULL && root->right == NULL)
-        v2.push_back(root->val);
-    second(root->left);
-    second(root->right);
-
-    return ;
-}
-
-    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+    bool leafSimilar(TreeNode *root1, TreeNode *root2)
+    {
         first(root1);
         second(root2);
 
-        if(v1 == v2)
+        if (v1 == v2)
         {
             return true;
         }

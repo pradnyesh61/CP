5ac37d45b47b229af9474e9fdf9df7d2a12c0fbb
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-06-30 23:00:03 +0530

    30 June 2023

---
+++ LeetCode/94_Binary_Tree_Inorder_Traversal.cpp
@@ -0,0 +1,34 @@
+// 30 June 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+/**
+
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+     vector<int> inorder_nodes;
+    vector<int> inorderTraversal(TreeNode* root) {
+      
+        
+        if(root == NULL)
+        {
+            return inorder_nodes;
+        }
+        
+        inorderTraversal(root->left);
+        inorder_nodes.push_back(root->val);
+        inorderTraversal(root->right);
+        
+        return inorder_nodes;
+    }
+};

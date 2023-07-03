e50b081658337ba6f259ed52351fce2a9c401f24
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-07-03 22:36:14 +0530

    3 July 2023

--- LeetCode/859_Buddy_Strings.cpp
+++ LeetCode/859_Buddy_Strings.cpp
@@ -3,55 +3,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-    bool buddyStrings(string s, string goal) {
-        
-        if(s.length() != goal.length())
+    bool buddyStrings(string s, string goal)
+    {
+
+        if (s.length() != goal.length())
         {
             return false;
         }
 
-        if(s == goal)
+        if (s == goal)
         {
-            map<char,int> m;
-            for(auto it : s)
+            map<char, int> m;
+            for (auto it : s)
             {
                 m[it]++;
-                if(m[it] >= 2)
+                if (m[it] >= 2)
                 {
                     return true;
                 }
             }
 
-    
             return false;
         }
 
-        int count =0;
-        for(int i= 0 ; i< s.length() ;i++)
+        int count = 0;
+        for (int i = 0; i < s.length(); i++)
         {
-            if(s[i] != goal[i])
+            if (s[i] != goal[i])
             {
                 count++;
             }
         }
 
-        if(count > 2)
+        if (count > 2)
         {
             return false;
         }
 
-        sort(s.begin(),s.end());
-        sort(goal.begin(),goal.end());
-          
-        if(s != goal)
+        sort(s.begin(), s.end());
+        sort(goal.begin(), goal.end());
+
+        if (s != goal)
         {
             return false;
-        }   
-        
+        }
+
         return true;
-        
     }
 };

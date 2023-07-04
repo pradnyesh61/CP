0c31fc867b3b49950f0a78ff1f8d2a28fb2512e6
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-07-04 08:19:18 +0530

    4 July 2023

--- LeetCode/137_Single_Number_II.cpp
+++ LeetCode/137_Single_Number_II.cpp
@@ -3,24 +3,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int singleNumber(vector<int>& nums) {
-        
-        sort(nums.begin(),nums.end());
+    int singleNumber(vector<int> &nums)
+    {
+
+        sort(nums.begin(), nums.end());
 
         int n = nums.size();
 
-        for(int i =0 ; i< n ;i++)
+        for (int i = 0; i < n; i++)
         {
-            if(i+2 < n && nums[i] == nums[i+2])
+            if (i + 2 < n && nums[i] == nums[i + 2])
             {
-                i = i+2;
+                i = i + 2;
             }
             else
-            return nums[i];
+                return nums[i];
         }
 
-        return nums[n-1];
+        return nums[n - 1];
     }
 };

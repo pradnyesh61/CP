c797e1b68a86c8681ef3538786c5f513b9c016b2
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-07-07 19:10:11 +0530

    7 July 2023

---
+++ LeetCode/1952_Three_Divisors.cpp
@@ -0,0 +1,28 @@
+// 7 July 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    bool isThree(int n) {
+        int count = 0;
+
+        for(int i = 1; i <= n;i++)
+        {
+            if(n%i == 0)
+            {
+                count++;
+            }
+            
+            if(count > 3)
+            {
+                return false;
+            }
+        }
+
+        return count == 3 ? true : false;
+
+    }
+};

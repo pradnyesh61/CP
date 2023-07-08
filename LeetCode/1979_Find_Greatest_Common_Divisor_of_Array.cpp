30fca10545709923e785319f21a0fa1df9c013ee
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-07-08 22:44:41 +0530

    8 July 2023

--- LeetCode/1979_Find_Greatest_Common_Divisor_of_Array.cpp
+++ LeetCode/1979_Find_Greatest_Common_Divisor_of_Array.cpp
@@ -3,11 +3,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int findGCD(vector<int>& nums) {
-        
-       sort(nums.begin(),nums.end());
-        return gcd(nums[0],nums[nums.size()-1]);
+    int findGCD(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        return gcd(nums[0], nums[nums.size() - 1]);
     }
 };

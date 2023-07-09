3e5f2be57891823b4edf6488e665af2cc89ffb9f
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-07-09 22:53:33 +0530

    9 July 2023

--- LeetCode/48_Rotate_Image.cpp
+++ LeetCode/48_Rotate_Image.cpp
@@ -3,49 +3,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    void rotate(vector<vector<int>>& matrix) {
-     
+    void rotate(vector<vector<int>> &matrix)
+    {
+
         int n = matrix.size();
-       
-     /*
-        map<int,vector<int>> m;
-        for(int i = 0; i< n ; i++)
+
+        /*
+           map<int,vector<int>> m;
+           for(int i = 0; i< n ; i++)
+           {
+               vector<int> v;
+               for(int j = n-1; j >= 0 ; j--)
+               {
+                   v.push_back(matrix[j][i]);
+               }
+
+               m[i] = v;
+           }
+
+           for(int i = 0; i< n ; i++)
+           {
+               vector<int> v = m[i];
+               for(int j = 0 ; j < n ; j++)
+               {
+                   matrix[i][j]  = v[j];
+               }
+           }
+
+           */
+
+        for (int i = 0; i < n; i++)
         {
-            vector<int> v;
-            for(int j = n-1; j >= 0 ; j--)
-            {
-                v.push_back(matrix[j][i]);
-            }
-
-            m[i] = v;
-        }
-
-        for(int i = 0; i< n ; i++)
-        {
-            vector<int> v = m[i];
-            for(int j = 0 ; j < n ; j++)
-            {
-                matrix[i][j]  = v[j];
-            }
-        }
-
-        */
-
-        for(int i = 0; i < n ; i++)
-        {
-            for(int j = i ; j < n; j++)
+            for (int j = i; j < n; j++)
             {
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
 
-        for(int i = 0; i < n ;i++)
+        for (int i = 0; i < n; i++)
         {
             reverse(matrix[i].begin(), matrix[i].end());
         }
-
-        
     }
 };

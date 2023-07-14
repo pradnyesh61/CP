124d6cefc1a8221a576d41b9bd8e6cb7d68f327b
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-07-14 22:02:28 +0530

    14 July 2023

---
+++ LeetCode/1379_Find_a_Corresponding_Node_of_a_Binary_Tree_in_a_Clone_of_That_Tree.cpp
@@ -0,0 +1,39 @@
+// 14 July 2023
+
+#include <bits/stdc++.h>
+using namespace std;/**
+
+
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    
+    TreeNode* ans = NULL;
+    
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        
+        if(original == NULL)
+        {
+            return ans;
+        }
+        
+        if(original == target)
+        {
+            ans = cloned;
+            return ans;
+        }
+        
+        getTargetCopy(original->left,cloned->left,target);
+        getTargetCopy(original->right,cloned->right,target);
+        
+       return ans;
+    }
+};

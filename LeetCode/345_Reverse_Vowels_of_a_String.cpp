d9e99669c86a053c8b4ef66e75808466dbf8b17d
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-07-15 22:59:23 +0530

    15 July 2023

--- LeetCode/345_Reverse_Vowels_of_a_String.cpp
+++ LeetCode/345_Reverse_Vowels_of_a_String.cpp
@@ -1,38 +1,40 @@
 // 15 July 2023
 
 #include <bits/stdc++.h>
-using namespace std; 
+using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    string reverseVowels(string s) {
-        
+    string reverseVowels(string s)
+    {
+
         int i = 0;
-        int j = s.length()-1;
-        
-        vector<char> v ={'a','e','i','o','u','A','E','I','O','U'};
-        
-        while(i < j)
+        int j = s.length() - 1;
+
+        vector<char> v = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+
+        while (i < j)
         {
-        
-            if(!(count(v.begin(), v.end(), s[i])))
+
+            if (!(count(v.begin(), v.end(), s[i])))
             {
                 i++;
             }
-            
-            if(!(count(v.begin(), v.end(), s[j])))
+
+            if (!(count(v.begin(), v.end(), s[j])))
             {
                 j--;
             }
-            
-            if(count(v.begin(), v.end(), s[i]) && count(v.begin(), v.end(), s[j]))
+
+            if (count(v.begin(), v.end(), s[i]) && count(v.begin(), v.end(), s[j]))
             {
-                swap(s[i],s[j]);
+                swap(s[i], s[j]);
                 i++;
                 j--;
             }
         }
-                         
+
         return s;
     }
 };

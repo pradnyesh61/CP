75b4e959d4ed04f5fd78e65daa30cb9209105ad8
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-07-16 22:02:23 +0530

    16 July 2023

--- LeetCode/653_Two_Sum_IV_Input_is_a_BST.cpp
+++ LeetCode/653_Two_Sum_IV_Input_is_a_BST.cpp
@@ -14,56 +14,54 @@ using namespace std;
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    
-    void vadd(vector<int> & v,TreeNode* root)
+    void vadd(vector<int> &v, TreeNode *root)
     {
-        if(root == NULL)
+        if (root == NULL)
         {
             return;
         }
-        
-        
-        vadd(v,root->left);
-        v.push_back(root->val);
-        vadd(v,root->right);
-        
-        return ;
-    }
-    
-    bool findTarget(TreeNode* root, int k) {
-        
-        vector<int> v;
-        
-        vadd(v,root);
-       
-        int i = 0;
-        int j = v.size()-1;
-        int sum;
-        
-        while(i<j)
-        {
-            sum = v[i]+v[j];
-            
 
-            if(sum == k)
+        vadd(v, root->left);
+        v.push_back(root->val);
+        vadd(v, root->right);
+
+        return;
+    }
+
+    bool findTarget(TreeNode *root, int k)
+    {
+
+        vector<int> v;
+
+        vadd(v, root);
+
+        int i = 0;
+        int j = v.size() - 1;
+        int sum;
+
+        while (i < j)
+        {
+            sum = v[i] + v[j];
+
+            if (sum == k)
             {
                 return true;
             }
-            else if(sum > k)
+            else if (sum > k)
             {
                 sum -= v[j];
                 j--;
             }
-            else if(sum < k)
+            else if (sum < k)
             {
                 sum -= v[i];
                 i++;
             }
         }
-        
-        
+
         return false;
     }
 };

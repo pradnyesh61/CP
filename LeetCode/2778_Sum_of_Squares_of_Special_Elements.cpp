83423588b10b6d4f8fd8d9e4309b7942ff7949bb
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-07-20 21:25:11 +0530

    20 July 2023

---
+++ LeetCode/2778_Sum_of_Squares_of_Special_Elements.cpp
@@ -0,0 +1,25 @@
+// 20 July 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int sumOfSquares(vector<int>& nums) {
+        
+
+        int sq = 0;
+
+        int n = nums.size();
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(n % (i+1) == 0)
+            {
+                sq +=  (nums[i]*nums[i]);
+            }
+        }
+
+        return sq;
+    }
+};

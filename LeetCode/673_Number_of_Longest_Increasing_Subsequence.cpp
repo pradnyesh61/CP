8a6476b6313663cbfc9b271141667eef05788844
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-07-21 23:51:25 +0530

    21 July 2023

--- LeetCode/673_Number_of_Longest_Increasing_Subsequence.cpp
+++ LeetCode/673_Number_of_Longest_Increasing_Subsequence.cpp
@@ -3,27 +3,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
+    int maxi = INT_MIN;
+    int count = 0;
 
-int maxi = INT_MIN;
-int count = 0;
-
-
-    void longsub(int i, vector<int> temp, vector<int>& nums,int sum)
+    void longsub(int i, vector<int> temp, vector<int> &nums, int sum)
     {
 
         int n = temp.size();
 
-        if(i == nums.size())
+        if (i == nums.size())
         {
-            if(n >= maxi)
+            if (n >= maxi)
             {
-                if(n == maxi)
+                if (n == maxi)
                 {
                     count++;
                 }
-                else{
+                else
+                {
                     count = 1;
                     maxi = n;
                 }
@@ -31,31 +31,31 @@ int count = 0;
             return;
         }
 
-        if( n == 0  || n > 0 && temp[n-1] < nums[i] )
+        if (n == 0 || n > 0 && temp[n - 1] < nums[i])
         {
             temp.push_back(nums[i]);
-            longsub(i+1,temp,nums,nums[i]);
+            longsub(i + 1, temp, nums, nums[i]);
             temp.pop_back();
         }
 
-        longsub(i+1,temp,nums,sum);
-
+        longsub(i + 1, temp, nums, sum);
     }
 
-   void memoization(int i, vector<int> temp, vector<int>& nums,int sum,vector<int>& dp)
+    void memoization(int i, vector<int> temp, vector<int> &nums, int sum, vector<int> &dp)
     {
 
         int n = temp.size();
 
-        if(i == nums.size())
+        if (i == nums.size())
         {
-            if(n >= maxi)
+            if (n >= maxi)
             {
-                if(n == maxi)
+                if (n == maxi)
                 {
                     count++;
                 }
-                else{
+                else
+                {
                     count = 1;
                     maxi = n;
                 }
@@ -64,45 +64,50 @@ int count = 0;
             return;
         }
 
-        if(dp[i] != -1)
+        if (dp[i] != -1)
         {
             dp[i];
         }
 
-        if( n == 0  || n > 0 && temp[n-1] < nums[i] )
+        if (n == 0 || n > 0 && temp[n - 1] < nums[i])
         {
             temp.push_back(nums[i]);
-            memoization(i+1,temp,nums,nums[i],dp);
+            memoization(i + 1, temp, nums, nums[i], dp);
             temp.pop_back();
         }
 
-        memoization(i+1,temp,nums,sum,dp);
-
+        memoization(i + 1, temp, nums, sum, dp);
     }
 
-    int findNumberOfLISq(vector<int>& nums) {
-        
+    int findNumberOfLISq(vector<int> &nums)
+    {
+
         vector<int> temp;
-        //longsub(0,temp,nums,0);
-        vector<int> dp(nums.size()+1,-1);
-        memoization(0,temp,nums,0,dp);
+        // longsub(0,temp,nums,0);
+        vector<int> dp(nums.size() + 1, -1);
+        memoization(0, temp, nums, 0, dp);
         return count;
     }
 
-  
-    int findNumberOfLIS(std::vector<int>& nums) {
+    int findNumberOfLIS(std::vector<int> &nums)
+    {
         int n = nums.size();
         vector<int> length(n, 1);
         vector<int> count(n, 1);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    if (length[j] + 1 > length[i]) {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[j] < nums[i])
+                {
+                    if (length[j] + 1 > length[i])
+                    {
                         length[i] = length[j] + 1;
                         count[i] = 0;
                     }
-                    if (length[j] + 1 == length[i]) {
+                    if (length[j] + 1 == length[i])
+                    {
                         count[i] += count[j];
                     }
                 }
@@ -112,13 +117,14 @@ int count = 0;
         int maxLength = *max_element(length.begin(), length.end());
         int result = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (length[i] == maxLength) {
+        for (int i = 0; i < n; i++)
+        {
+            if (length[i] == maxLength)
+            {
                 result += count[i];
             }
         }
 
         return result;
     }
-
 };

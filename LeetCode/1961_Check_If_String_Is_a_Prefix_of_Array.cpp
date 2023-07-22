6414c386b39863e228bb5f778438d5dcdf7f307b
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-07-22 21:46:32 +0530

    22 July 2023

---
+++ LeetCode/1961_Check_If_String_Is_a_Prefix_of_Array.cpp
@@ -0,0 +1,48 @@
+// 22 July 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    bool isPrefixString(string s, vector<string>& words) {
+        
+        int k =0;
+
+        int i = 0;
+
+        while(i < s.length())
+        {   
+            for(int j = 0 ;j < words[k].size(); j++)
+            {
+                cout<<s[i]<<" = "<<words[k][j]<<endl;
+                if(i >= s.length())
+                {
+                    if(j < words[k].size())
+                    {
+                        return false;
+                    }
+                    return true;
+                }
+
+                if(s[i] != words[k][j])
+                {
+                    return false;
+                }
+                i++;
+            }
+            k++;
+            if(k >= words.size())
+            {
+                break;
+            }
+        }
+
+        if(i== s.length())
+        {
+                return true;
+        }
+        return false;
+    }
+};

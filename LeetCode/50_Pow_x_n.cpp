536d3e69a18d3fc227cf84876903301dd52a11e9
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-07-24 23:28:34 +0530

    24 July 2023

--- LeetCode/50_Pow_x_n.cpp
+++ LeetCode/50_Pow_x_n.cpp
@@ -3,40 +3,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-   
-
-    double myp(double x,int n)
+    double myp(double x, int n)
     {
-         if(n == 1)
+        if (n == 1)
         {
             return x;
         }
-      
-       return myPow(x,n-1) * x ;
 
+        return myPow(x, n - 1) * x;
     }
 
-    double myPow(double x, int n) {
-        
-        return pow(x,n);
-       if(n == 1)
-       {
-           return x;
-       }
+    double myPow(double x, int n)
+    {
 
-       if(n <0)
-       {
-           n = n*-1;
-           x= 1/x;
-       }
+        return pow(x, n);
+        if (n == 1)
+        {
+            return x;
+        }
 
-       if(n%2 ==0)
-        return myPow(x*x,n/2);
+        if (n < 0)
+        {
+            n = n * -1;
+            x = 1 / x;
+        }
 
-        return x*myPow(x*x,n/2);
+        if (n % 2 == 0)
+            return myPow(x * x, n / 2);
 
-       
+        return x * myPow(x * x, n / 2);
     }
 };

8a1deb8c369f39bbb568ce3d4fccb7d3601004a9
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-07-25 19:45:13 +0530

    25 July 2023

--- LeetCode/852_Peak_Index_in_a_Mountain_Array.cpp
+++ LeetCode/852_Peak_Index_in_a_Mountain_Array.cpp
@@ -3,28 +3,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-    
+class Solution
+{
+
 public:
-    int peakIndexInMountainArray(vector<int>& arr) {
-        
-        int i =0;
+    int peakIndexInMountainArray(vector<int> &arr)
+    {
+
+        int i = 0;
         int n = arr.size();
 
-        int j = n-1;
+        int j = n - 1;
 
         int maxi = INT_MIN;
         int ind = 0;
 
-        while(i <= j)
+        while (i <= j)
         {
-            if(arr[i] > maxi)
+            if (arr[i] > maxi)
             {
                 maxi = arr[i];
                 ind = i;
             }
-            
-            if(arr[j] > maxi)
+
+            if (arr[j] > maxi)
             {
                 ind = j;
                 maxi = arr[j];

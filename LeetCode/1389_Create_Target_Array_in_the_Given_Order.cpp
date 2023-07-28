9552434a9f0841ad7963cacd899ff6fc160cc1d1
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-07-28 23:07:18 +0530

    28 July 2023

--- LeetCode/1389_Create_Target_Array_in_the_Given_Order.cpp
+++ LeetCode/1389_Create_Target_Array_in_the_Given_Order.cpp
@@ -3,16 +3,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
-        
+    vector<int> createTargetArray(vector<int> &nums, vector<int> &index)
+    {
+
         int n = nums.size();
         vector<int> v;
 
-        for(int i = 0 ; i <n;i++)
+        for (int i = 0; i < n; i++)
         {
-            v.insert(v.begin()+index[i], nums[i]);
+            v.insert(v.begin() + index[i], nums[i]);
         }
 
         return v;

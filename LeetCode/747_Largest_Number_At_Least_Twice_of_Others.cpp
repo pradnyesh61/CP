060022a0ca7958f480caca10cfc07b36b87f6e49
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-07-29 13:12:09 +0530

    29 July 2023

--- LeetCode/747_Largest_Number_At_Least_Twice_of_Others.cpp
+++ LeetCode/747_Largest_Number_At_Least_Twice_of_Others.cpp
@@ -1,22 +1,21 @@
-
 // 29 July 2023
 
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int dominantIndex(vector<int>& nums) {
-        
-        
+    int dominantIndex(vector<int> &nums)
+    {
+
         int n = nums.size();
-        int l = max_element(nums.begin(),nums.end())-nums.begin();
+        int l = max_element(nums.begin(), nums.end()) - nums.begin();
 
-
-        for(int i = 0; i < n;i++)
+        for (int i = 0; i < n; i++)
         {
-            
-            if(i != l && nums[i]*2 > nums[l])
+
+            if (i != l && nums[i] * 2 > nums[l])
             {
                 return -1;
             }

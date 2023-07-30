e1d6ab401e4ce5437ad7f006abf07bb8167b3a68
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-07-30 21:24:36 +0530

    30 July 2023

--- LeetCode/2798_Number_of_Employees_Who_Met_the_Target.cpp
+++ LeetCode/2798_Number_of_Employees_Who_Met_the_Target.cpp
@@ -3,17 +3,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
-        
+    int numberOfEmployeesWhoMetTarget(vector<int> &hours, int target)
+    {
+
         int count = 0;
 
         int n = hours.size();
 
-        for(int i = 0 ; i< n; i++)
+        for (int i = 0; i < n; i++)
         {
-            if(hours[i] >= target)
+            if (hours[i] >= target)
             {
                 count++;
             }

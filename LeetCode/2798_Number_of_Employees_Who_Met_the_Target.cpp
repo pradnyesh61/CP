e40456e57d1120762cfe18a5dc1ec205ca5cbe57
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-07-30 21:24:03 +0530

    30 July 2023

---
+++ LeetCode/2798_Number_of_Employees_Who_Met_the_Target.cpp
@@ -0,0 +1,24 @@
+// 30 July 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        
+        int count = 0;
+
+        int n = hours.size();
+
+        for(int i = 0 ; i< n; i++)
+        {
+            if(hours[i] >= target)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};

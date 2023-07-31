f9ebaa95ccdb30a3cfec090e96472213c545b60b
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-07-31 21:00:20 +0530

    31 July 2023

---
+++ LeetCode/709_To_Lower_Case.cpp
@@ -0,0 +1,13 @@
+// 31 July 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    string toLowerCase(string s) {
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        return s;
+    }
+};

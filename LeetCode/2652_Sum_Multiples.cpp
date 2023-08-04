c660d285c6416e111668cd73cdf8a0d8621f190d
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-08-04 23:34:09 +0530

    4 July 2023

--- LeetCode/2652_Sum_Multiples.cpp
+++ LeetCode/2652_Sum_Multiples.cpp
@@ -3,16 +3,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-    int sumOfMultiples(int n) {
-        
+    int sumOfMultiples(int n)
+    {
+
         int sum = 0;
 
-        for(int i = 1; i<= n; i++)
+        for (int i = 1; i <= n; i++)
         {
-            if(i%3 == 0 || i%5==0 || i%7==0)
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
             {
                 sum += i;
             }

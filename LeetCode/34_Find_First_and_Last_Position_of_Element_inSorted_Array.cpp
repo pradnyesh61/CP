21139c76d60c76c95cabb29b493a829c94e53546
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-08-05 22:18:55 +0530

    5 August 2023

--- LeetCode/34_Find_First_and_Last_Position_of_Element_inSorted_Array.cpp
+++ LeetCode/34_Find_First_and_Last_Position_of_Element_inSorted_Array.cpp
@@ -3,37 +3,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        
+    vector<int> searchRange(vector<int> &nums, int target)
+    {
+
         int start = -1;
         int end = -1;
 
         int n = nums.size();
 
-       for(int i = 0 ; i < n ; i++)
-       {
-           if(nums[i] == target)
-           {
-               start = i;
-               break;
-           }
-       }
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] == target)
+            {
+                start = i;
+                break;
+            }
+        }
 
-       for(int j = n-1 ; j >=0  ; j--)
-       {
-           if(nums[j] == target)
-           {
-               end = j;
-               break;
-           }
-       }
+        for (int j = n - 1; j >= 0; j--)
+        {
+            if (nums[j] == target)
+            {
+                end = j;
+                break;
+            }
+        }
 
-
-
-       return {start,end};
-
-
+        return {start, end};
     }
 };

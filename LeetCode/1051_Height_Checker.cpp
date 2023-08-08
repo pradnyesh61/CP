5acf9b2a92212b8832f30717694e652fcb8f8d81
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-08-08 21:54:48 +0530

    8 August 2023

--- LeetCode/1051_Height_Checker.cpp
+++ LeetCode/1051_Height_Checker.cpp
@@ -3,19 +3,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-    int heightChecker(vector<int>& heights) {
-        
-        vector<int> v= heights;
-        sort(heights.begin(),heights.end());
+    int heightChecker(vector<int> &heights)
+    {
+
+        vector<int> v = heights;
+        sort(heights.begin(), heights.end());
 
         int count = 0;
 
-        for(int i = 0 ; i< heights.size() ; i++)
+        for (int i = 0; i < heights.size(); i++)
         {
-            if(heights[i]!= v[i])
+            if (heights[i] != v[i])
             {
                 count++;
             }

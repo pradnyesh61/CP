3981198ab51dd6afcd4fa910461b5ffb278a16e8
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-08-09 22:24:51 +0530

    9 August 2023

---
+++ LeetCode/905_Sort_Array_By_Parity.cpp
@@ -0,0 +1,30 @@
+// 9 August 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        vector<int> even;
+        vector<int> odd;
+
+        for(int i  =0; i < nums.size();i++)
+        {
+            if(nums[i]%2 == 0)
+            {
+                even.push_back(nums[i]);
+            }else{
+                odd.push_back(nums[i]);
+            }
+
+        }
+
+        for(auto i : odd)
+        {
+            even.push_back(i);
+        }
+
+        return even;
+    }
+};

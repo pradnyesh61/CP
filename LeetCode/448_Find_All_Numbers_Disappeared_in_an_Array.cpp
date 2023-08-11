361b80f0d8a25b57fdee034481ffe2ec35b9b127
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-08-11 23:00:01 +0530

    11 August 2023

--- LeetCode/448_Find_All_Numbers_Disappeared_in_an_Array.cpp
+++ LeetCode/448_Find_All_Numbers_Disappeared_in_an_Array.cpp
@@ -4,15 +4,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> findDisappearedNumbers(vector<int>& nums) {
-     sort(begin(nums), end(nums));
+    vector<int> findDisappearedNumbers(vector<int> &nums)
+    {
+        sort(begin(nums), end(nums));
         vector<int> ans;
-        for(int i = 1; i <= size(nums); i++) 
-            if(!binary_search(begin(nums), end(nums), i))
+        for (int i = 1; i <= size(nums); i++)
+            if (!binary_search(begin(nums), end(nums), i))
                 ans.push_back(i);
         return ans;
     }
-
 };

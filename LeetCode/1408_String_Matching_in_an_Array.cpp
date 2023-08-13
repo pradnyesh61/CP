f3c267c6ce23ce54f51b866cd13e7f0294813792
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-08-13 23:46:50 +0530

    13 August 2023

--- LeetCode/1408_String_Matching_in_an_Array.cpp
+++ LeetCode/1408_String_Matching_in_an_Array.cpp
@@ -1,44 +1,46 @@
 // 13 August 2023
 #include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    vector<string> stringMatching(vector<string>& words) {
-          set<string> s;
+    vector<string> stringMatching(vector<string> &words)
+    {
+        set<string> s;
 
-        for(int i =0;i<words.size();i++)
+        for (int i = 0; i < words.size(); i++)
         {
-            string sw=words[i];
+            string sw = words[i];
             int l = sw.size();
 
-            for(int j= 0; j < words.size() ;j++)
+            for (int j = 0; j < words.size(); j++)
             {
-                if(i == j)
+                if (i == j)
                 {
                     continue;
                 }
 
-                for(int m = 0; m < words[j].size();m++)
+                for (int m = 0; m < words[j].size(); m++)
                 {
-                    if(sw[0] == words[j][m])
+                    if (sw[0] == words[j][m])
                     {
                         int k = words[j].size();
-                        int sz = min(k,l);
+                        int sz = min(k, l);
 
-                        k =0;
+                        k = 0;
 
                         int n = m;
-                        while(k < sz && n < words[j].size())
+                        while (k < sz && n < words[j].size())
                         {
-                                if(sw[k] != words[j][n])
-                                {
-                                    break;
-                                }
-                                n++;
-                                k++;
+                            if (sw[k] != words[j][n])
+                            {
+                                break;
+                            }
+                            n++;
+                            k++;
                         }
 
-                        if( k == sw.size())
+                        if (k == sw.size())
                         {
                             s.insert(sw);
                         }
@@ -47,21 +49,21 @@ public:
             }
         }
 
-        vector<string> ans(s.begin(),s.end());
+        vector<string> ans(s.begin(), s.end());
 
         return ans;
     }
 };
 
 int main()
-    {
-        Solution obj;
-        vector<string>  words = {"mass","as","hero","superhero"};
-        vector<string> s = obj.stringMatching(words);
+{
+    Solution obj;
+    vector<string> words = {"mass", "as", "hero", "superhero"};
+    vector<string> s = obj.stringMatching(words);
 
-        for(auto i : s)
-        {
-            cout<<i<<" ";
-        }
-        cout<<endl;
+    for (auto i : s)
+    {
+        cout << i << " ";
     }
+    cout << endl;
+}

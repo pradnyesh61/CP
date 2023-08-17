b8bd305353f9b3864e2a63c5a8458527fe1f54d0
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-08-17 12:30:58 +0530

    17 August 2023

--- LeetCode/542_01_Matrix.cpp
+++ LeetCode/542_01_Matrix.cpp
@@ -3,122 +3,118 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        
+    vector<vector<int>> updateMatrix(vector<vector<int>> &mat)
+    {
 
-        queue<pair<int,pair<int,int>>> q;
-
+        queue<pair<int, pair<int, int>>> q;
 
         int n = mat[0].size();
         int m = mat.size();
-        
-        vector<vector<int>> vis(m, vector<int> (n,-1));
 
-          int count = 0;
+        vector<vector<int>> vis(m, vector<int>(n, -1));
 
+        int count = 0;
 
-        for(int i = 0 ; i< m ;i++)
+        for (int i = 0; i < m; i++)
         {
-            for(int j = 0; j < n ; j++)
+            for (int j = 0; j < n; j++)
             {
-                if(mat[i][j] == 0)
+                if (mat[i][j] == 0)
                 {
                     vis[i][j] = 1;
                     count++;
-                    q.push( {0, {i,j} } );
+                    q.push({0, {i, j}});
                 }
             }
         }
 
-       /* for(auto it : mat)
+        /* for(auto it : mat)
+         {
+             for(auto itt : it)
+             {
+                 cout<<itt<<" ";
+             }
+             cout<<endl;
+         }
+ */
+
+        int box = n * m;
+
+        vector<int> col = {-1, 0, +1, 0};
+        vector<int> row = {0, +1, 0, -1};
+
+        while (!q.empty())
         {
-            for(auto itt : it)
-            {
-                cout<<itt<<" ";
-            }
-            cout<<endl;
-        }
-*/
-
-      
-        int box = n*m;
-
-        vector<int> col = {-1,0,+1,0};
-        vector<int> row = {0,+1,0,-1};
-
-        while(!q.empty())
-        {
-            if(count >= box)
+            if (count >= box)
             {
                 break;
             }
-          //  cout<<"count = "<<count<<endl;
+            //  cout<<"count = "<<count<<endl;
 
             int sz = q.size();
 
-            for(int k = 1; k <= sz ; k++)
+            for (int k = 1; k <= sz; k++)
             {
-                pair<int,pair<int,int>> p = q.front();
+                pair<int, pair<int, int>> p = q.front();
                 int i = p.second.first;
                 int j = p.second.second;
                 int c = p.first + 1;
                 q.pop();
 
-                for(int l = 0; l < col.size();l++)
+                for (int l = 0; l < col.size(); l++)
                 {
                     int icol = i + col[l];
                     int jrow = j + row[l];
-/*
-                     cout<<" --------"<<endl;
-                    for(auto it : mat)
-                        {
-                            for(auto itt : it)
-                            {
-                                cout<<itt<<" ";
-                            }
-                            cout<<endl;
-                        }
-                        cout<<endl;
-*/
-                    if(icol >= 0 && icol < m && jrow >= 0 && jrow < n)
+                    /*
+                                         cout<<" --------"<<endl;
+                                        for(auto it : mat)
+                                            {
+                                                for(auto itt : it)
+                                                {
+                                                    cout<<itt<<" ";
+                                                }
+                                                cout<<endl;
+                                            }
+                                            cout<<endl;
+                    */
+                    if (icol >= 0 && icol < m && jrow >= 0 && jrow < n)
                     {
-                        if(mat[icol][jrow] == 1 && vis[icol][jrow] == -1)
+                        if (mat[icol][jrow] == 1 && vis[icol][jrow] == -1)
                         {
                             vis[icol][jrow] = 1;
-                //            cout<<"1 = mat = "<<mat[icol][jrow]<<endl;
+                            //            cout<<"1 = mat = "<<mat[icol][jrow]<<endl;
                             mat[icol][jrow] = c;
-                            q.push({c,{icol,jrow}});
+                            q.push({c, {icol, jrow}});
                             count++;
                         }
-                        else if((vis[icol][jrow] != -1 && mat[icol][jrow] == 1) ||
-                                 (mat[icol][jrow] != 0 && mat[icol][jrow] != 1)){
+                        else if ((vis[icol][jrow] != -1 && mat[icol][jrow] == 1) ||
+                                 (mat[icol][jrow] != 0 && mat[icol][jrow] != 1))
+                        {
 
-                  //          cout<<"2 = mat = "<<mat[icol][jrow]<<endl;
+                            //          cout<<"2 = mat = "<<mat[icol][jrow]<<endl;
                             int mini = min(mat[icol][jrow], c);
                             mat[icol][jrow] = mini;
-                           // q.push({mini, {icol,jrow}});
+                            // q.push({mini, {icol,jrow}});
                         }
                     }
-              /*        for(auto it : mat)
-                        {
-                            for(auto itt : it)
-                            {
-                                cout<<itt<<" ";
-                            }
-                            cout<<endl;
-                        }
-                         cout<<" --------"<<endl;
-                        cout<<endl;
-                */
+                    /*        for(auto it : mat)
+                              {
+                                  for(auto itt : it)
+                                  {
+                                      cout<<itt<<" ";
+                                  }
+                                  cout<<endl;
+                              }
+                               cout<<" --------"<<endl;
+                              cout<<endl;
+                      */
                 }
-
             }
-
         }
 
         return mat;
-
     }
 };

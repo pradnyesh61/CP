36d01a85b6b03757671814f18de43752a6327548
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-08-20 23:17:32 +0530

    20 August 2023

---
+++ LeetCode/643_Maximum_Average_Subarray_I.cpp
@@ -0,0 +1,30 @@
+// 20 August 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        
+        double sum = 0;
+        
+        for(int i = 0; i < k ; i++)
+        {   
+            sum = sum + nums[i];
+        }
+
+        double maxi = sum;
+       
+            for(int i = k; i < nums.size() ;i++)
+            {
+                sum += nums[i] - nums[i-k];
+                maxi = max(maxi,sum);
+                
+            }
+        
+
+
+        return maxi/k;
+    }
+};

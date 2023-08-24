505bf5975ea9c2f7c9005aab5e781a070d928dac
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-08-24 22:37:02 +0530

    24 August 2023

--- LeetCode/409_Longest_Palindrome.cpp
+++ LeetCode/409_Longest_Palindrome.cpp
@@ -3,33 +3,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
+    int longestPalindrome(string s)
+    {
 
+        map<char, int> m;
 
-    int longestPalindrome(string s) {
-       
-       map<char,int> m;
+        for (auto it : s)
+        {
+            m[it]++;
+        }
 
-       for(auto it : s)
-       {
-           m[it]++;
-       }
+        int count = 0;
+        int evencount = 0;
 
+        for (auto it : m)
+        {
+            count += (it.second / 2) * 2;
 
-       int count = 0;
-       int evencount = 0;
-
-      
-       for(auto it : m)
-       {
-           count += (it.second/2) * 2;
-
-           if(count% 2 == 0 && it.second % 2 == 1)
-           {
-               count++;
-           }
-       }
-       return count;
+            if (count % 2 == 0 && it.second % 2 == 1)
+            {
+                count++;
+            }
+        }
+        return count;
     }
 };

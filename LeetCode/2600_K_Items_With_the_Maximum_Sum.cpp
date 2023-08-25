7e47fd0942b38b120b9f372b9f8208bbff1abfae
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-08-25 22:43:24 +0530

    15 August 2023

---
+++ LeetCode/2600_K_Items_With_the_Maximum_Sum.cpp
@@ -0,0 +1,39 @@
+// 25 August 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        
+        int maxi = 0;
+
+        while(k)
+        {
+
+            while(numOnes-- && k)
+            {
+                k--;
+                maxi += 1;
+            }
+
+
+            while(numZeros && k)
+            {
+                k--;
+                maxi += 0;
+                numZeros--;
+            }
+
+            while(numNegOnes && k)
+            {
+                k--;
+                numNegOnes--;
+                maxi += (-1);
+            }
+        }
+
+        return maxi;
+    }
+};

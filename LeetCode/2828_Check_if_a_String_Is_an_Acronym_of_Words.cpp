fc8c8bb744f71b7625b9e5702a61dfdaafc70ef5
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-08-26 22:26:51 +0530

    26 August 2023

--- LeetCode/2828_Check_if_a_String_Is_an_Acronym_of_Words.cpp
+++ LeetCode/2828_Check_if_a_String_Is_an_Acronym_of_Words.cpp
@@ -3,23 +3,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-    bool isAcronym(vector<string>& words, string s) {
-        
+    bool isAcronym(vector<string> &words, string s)
+    {
+
         string ans = "";
 
-        for(int i = 0 ; i < words.size() ;i++)
+        for (int i = 0; i < words.size(); i++)
         {
             ans += words[i][0];
-          
         }
 
-          if(ans == s)
-            {
-                return true;
-            }
+        if (ans == s)
+        {
+            return true;
+        }
 
         return false;
     }

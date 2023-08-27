d3c1767b7b8668491f53a5267c85cc9482f54c31
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-08-27 22:13:23 +0530

    27 August 2023

--- LeetCode/2833_Furthest_Point_From_Origin.cpp
+++ LeetCode/2833_Furthest_Point_From_Origin.cpp
@@ -2,65 +2,68 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    int furthestDistanceFromOrigin(string moves) {
-        
+    int furthestDistanceFromOrigin(string moves)
+    {
+
         int mov = 0;
 
-         char c = moves[0];
+        char c = moves[0];
 
         int left = 0;
         int right = 0;
         int under = 0;
 
-            for(auto it : moves)
-            {
-                if(it == 'L')
-                {
-                    left++;
-                }
-                else if(it == 'R')
-                {
-                    right++;
-                }
-                else{
-                    under++;
-                }
-            }
-
-             if(left > right)
-            {
-                c = 'L'; 
-            }
-            else{
-                c= 'R';
-            }
-      
-
-     // cout<<left<<" - "<<right<<" + "<<under<<" = "<<(abs(left-right)+under);
-
-      return abs(left-right)+under;
-     /*   for(int  i = 0; i < moves.size();i++)
+        for (auto it : moves)
         {
-            if(moves[i] == 'L')
+            if (it == 'L')
             {
-                mov--;
+                left++;
             }
-            else if(moves[i] == 'R')
+            else if (it == 'R')
             {
-                mov++;
+                right++;
             }
-            else if(moves[i] == '_' && c == 'L'){
-                mov--;
-            }
-            else if(moves[i] == '_' && c == 'R'){
-                mov++;
+            else
+            {
+                under++;
             }
         }
 
-        return abs(mov);
+        if (left > right)
+        {
+            c = 'L';
+        }
+        else
+        {
+            c = 'R';
+        }
 
-        */
+        // cout<<left<<" - "<<right<<" + "<<under<<" = "<<(abs(left-right)+under);
+
+        return abs(left - right) + under;
+        /*   for(int  i = 0; i < moves.size();i++)
+           {
+               if(moves[i] == 'L')
+               {
+                   mov--;
+               }
+               else if(moves[i] == 'R')
+               {
+                   mov++;
+               }
+               else if(moves[i] == '_' && c == 'L'){
+                   mov--;
+               }
+               else if(moves[i] == '_' && c == 'R'){
+                   mov++;
+               }
+           }
+
+           return abs(mov);
+
+           */
     }
 };

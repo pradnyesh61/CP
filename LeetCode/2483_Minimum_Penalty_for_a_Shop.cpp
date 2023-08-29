914864a2d757ffab32930208deab8289b385d61f
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-08-29 22:47:41 +0530

    29 August 2023

--- LeetCode/2483_Minimum_Penalty_for_a_Shop.cpp
+++ LeetCode/2483_Minimum_Penalty_for_a_Shop.cpp
@@ -3,142 +3,148 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-    int bestClosingTime(string customers) {
+    int bestClosingTime(string customers)
+    {
 
         int curPenalty = count(customers.begin(), customers.end(), 'Y');
         int minPenalty = curPenalty;
         int earliestHour = 0;
 
-        for (int i = 0; i < customers.size(); i++) {
+        for (int i = 0; i < customers.size(); i++)
+        {
             char ch = customers[i];
-            if (ch == 'Y') {
+            if (ch == 'Y')
+            {
                 curPenalty--;
-            } else {
+            }
+            else
+            {
                 curPenalty++;
             }
 
-            if (curPenalty < minPenalty) {
+            if (curPenalty < minPenalty)
+            {
                 earliestHour = i + 1;
                 minPenalty = curPenalty;
             }
         }
 
         return earliestHour;
-/*   
-        int maxiy = 0;
-        int maxin = 0;
+        /*
+                int maxiy = 0;
+                int maxin = 0;
 
-        for(auto it : customers)
-        {
-            if(it == 'Y')
-            {
-                maxiy++;
-            }
-            else{
-                maxin++;
-            }
-        }
-
-        int ind  = 0;
-       
-        int cy = 0;
-        int cn = 0;
-
-        vector<int> v;
-
-
-        for(int i = 0 ;i < customers.size(); i++)
-        {
-           
-
-            int temp = (maxiy-cy) + cn;
-
-            if(customers[i] == 'Y')
-            {
-                cy++;
-            }
-            else{
-                cn++;
-            }
-           v.push_back(temp);
-
-        }
-
-
-         int ans  = v[0];
-
-
-        for(int i = 0; i< v.size();i++)
-        {
-            cout<<v[i]<<" ";
-            if(ans > v[i])
-            {
-                ans = v[i];
-                ind = i;
-               
-            }
-        }
-
-        if(maxiy == v.size())
-        {
-            return v.size();
-        }
-
-        
-        return ind;
-
-/*
-        cout<<maxiy<<" = "<<maxin<<endl;
-
-        int ans = INT_MAX;
-
-    cout<<endl;
-    int ind = 0;
-
-    int nc = 0;
-
-      for(int i = 0; i < customers.size() ; i++)
-        {
-            int temp =0;
-            if(customers[i] == 'Y')
-            {
-                temp = abs(maxiy-i);
-                if(nc)
+                for(auto it : customers)
                 {
-                    temp = temp + nc;
+                    if(it == 'Y')
+                    {
+                        maxiy++;
+                    }
+                    else{
+                        maxin++;
+                    }
                 }
-                cout<<"temp="<<temp<<" "<<endl;
-                   if(ans > temp)
-                   {
-                       ans = temp;
+
+                int ind  = 0;
+
+                int cy = 0;
+                int cn = 0;
+
+                vector<int> v;
+
+
+                for(int i = 0 ;i < customers.size(); i++)
+                {
+
+
+                    int temp = (maxiy-cy) + cn;
+
+                    if(customers[i] == 'Y')
+                    {
+                        cy++;
+                    }
+                    else{
+                        cn++;
+                    }
+                   v.push_back(temp);
+
+                }
+
+
+                 int ans  = v[0];
+
+
+                for(int i = 0; i< v.size();i++)
+                {
+                    cout<<v[i]<<" ";
+                    if(ans > v[i])
+                    {
+                        ans = v[i];
                         ind = i;
-                        cout<<"ind="<<" = "<<ind<<endl;
-                   }
-                
-            }
-            else{
-                nc++;
-                temp = abs(maxin - i);
-                cout<<"temp="<<temp<<" "<<endl;
-                   if(ans > temp)
-                   {
-                       ans = temp;
-                        ind = i;
-                        cout<<"ind="<<" = "<<ind<<endl;
-                   }
-                
-                cout<<" n ="<<temp<<" = "<<i<<endl;
-            }
-            cout<<"ans = "<<ans<<endl<<endl;
 
-        }
+                    }
+                }
+
+                if(maxiy == v.size())
+                {
+                    return v.size();
+                }
 
 
-       return ind;
+                return ind;
 
-       */
+        /*
+                cout<<maxiy<<" = "<<maxin<<endl;
+
+                int ans = INT_MAX;
+
+            cout<<endl;
+            int ind = 0;
+
+            int nc = 0;
+
+              for(int i = 0; i < customers.size() ; i++)
+                {
+                    int temp =0;
+                    if(customers[i] == 'Y')
+                    {
+                        temp = abs(maxiy-i);
+                        if(nc)
+                        {
+                            temp = temp + nc;
+                        }
+                        cout<<"temp="<<temp<<" "<<endl;
+                           if(ans > temp)
+                           {
+                               ans = temp;
+                                ind = i;
+                                cout<<"ind="<<" = "<<ind<<endl;
+                           }
+
+                    }
+                    else{
+                        nc++;
+                        temp = abs(maxin - i);
+                        cout<<"temp="<<temp<<" "<<endl;
+                           if(ans > temp)
+                           {
+                               ans = temp;
+                                ind = i;
+                                cout<<"ind="<<" = "<<ind<<endl;
+                           }
+
+                        cout<<" n ="<<temp<<" = "<<i<<endl;
+                    }
+                    cout<<"ans = "<<ans<<endl<<endl;
+
+                }
+
+
+               return ind;
+
+               */
     }
 };

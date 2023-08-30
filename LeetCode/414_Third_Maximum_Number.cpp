b6ecf99e7935d2f17f20ff20cadeba743b0f96f5
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-08-30 22:33:53 +0530

    30 August 2023

---
+++ LeetCode/414_Third_Maximum_Number.cpp
@@ -0,0 +1,33 @@
+// 30 August 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        
+        set<int> s(nums.begin(),nums.end());
+
+        priority_queue<int> pq(s.begin(),s.end());
+
+        int ans = 0;
+
+        int max = pq.top();
+        int a = 0;
+
+        while(!pq.empty())
+        {
+            ans = pq.top();
+            a++;
+            if(a == 3)
+            {
+                return ans;
+            }
+            pq.pop();
+        }
+
+        return max;
+    }
+};

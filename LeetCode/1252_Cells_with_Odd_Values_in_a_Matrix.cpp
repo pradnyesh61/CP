d9c38d5a5e2c5f757e6ac7c28cb5d252c9dc21ee
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-08-31 22:08:16 +0530

    31 August 2023

---
+++ LeetCode/1252_Cells_with_Odd_Values_in_a_Matrix.cpp
@@ -0,0 +1,44 @@
+// 31 August 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int oddCells(int m, int n, vector<vector<int>>& indices) {
+        
+
+        vector<vector<int>> mat(m,vector<int> (n,0));
+            
+        for(int i = 0; i<indices.size() ;i++)
+        {
+        
+
+            for(int j = 0 ; j < n; j++)
+            {
+                mat[ indices[i][0]][j]++;
+
+            }
+            for(int j= 0 ; j < m; j++)
+            {
+                mat[j][ indices[i][1]]++;
+            }
+        }
+
+        int count = 0;
+
+        for(auto it : mat)
+        {
+            for(auto i : it)
+            {
+                if(i%2 != 0)
+                {
+                    count++;
+                }
+            }
+        }
+    return count;
+    }
+
+};

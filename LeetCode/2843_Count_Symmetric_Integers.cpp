12db3205f58d50b20213b360cc81fbf379a1246a
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-09-03 23:25:50 +0530

    3 September 2023

---
+++ LeetCode/2843_Count_Symmetric_Integers.cpp
@@ -0,0 +1,35 @@
+// 3 September 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countSymmetricIntegers(int low, int high) {
+        
+        int count = 0;
+
+        for(int i = low ; i<= high; i++)
+        {
+            string s = to_string(i);
+ int n = s.size();
+            if(n%2 == 0)
+            {
+                int t = 0;
+               
+                for(int j = 0; j < n/2; j++)
+                {
+                    t += (s[j])-(s[n-j-1]);
+                }
+
+                if(t == 0)
+                {
+                    count++;
+                }
+            }
+            
+        }
+
+        return count;
+    }
+};

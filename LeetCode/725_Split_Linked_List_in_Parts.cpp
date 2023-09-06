25a15222f6bd16d2eba7809daebb8a7b9a43f699
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-09-06 20:03:17 +0530

    6 September 2023

---
+++ LeetCode/725_Split_Linked_List_in_Parts.cpp
@@ -0,0 +1,78 @@
+// 6 September 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        
+        ListNode* temp = head;
+        int count = 0;
+
+        while(temp != NULL)
+        {
+            count++;
+            temp = temp->next;
+        }
+
+            vector<ListNode*> v;
+
+            temp = head;
+
+            while(k)
+            {
+                int t = ceil((float)count/k);
+                ListNode* list = NULL;
+                ListNode* listtemp = list;
+
+                if(t == 1)
+                {
+                    if(temp != NULL)
+                    {
+                       list = new ListNode(temp->val);
+                       temp = temp->next;
+                        count--;
+                        listtemp = list;
+                    }
+                     
+                }
+                else
+                {
+                    while(t)
+                    {
+                    
+                        if(list == NULL)
+                        {
+                            list = new ListNode(temp->val);
+                            listtemp = list;   
+                        }
+                        else
+                        {
+                            list->next = new ListNode(temp->val);
+                            list = list->next;
+                        }
+                        temp = temp->next;
+                        t--;
+                        count--;
+                    } 
+                    
+                }
+                v.push_back(listtemp);
+                k--;
+            }
+
+            return v;
+        
+    }
+};

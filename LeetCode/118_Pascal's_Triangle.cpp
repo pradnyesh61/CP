9ac1954685bac304c2eca5a538f3cd67c81151cd
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-09-08 19:54:08 +0530

    8 September 2023

--- LeetCode/118_Pascal's_Triangle.cpp
+++ LeetCode/118_Pascal's_Triangle.cpp
@@ -2,43 +2,40 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-
-class Solution
-{
+class Solution {
 public:
-    vector<vector<int>> generate(int n)
-    {
-
+    vector<vector<int>> generate(int n) {
+        
         vector<vector<int>> ans;
 
-        for (int i = 0; i < n; i++)
+        for(int i = 0 ; i < n ; i++)
         {
             vector<int> v;
-            if (i == 0)
-            {
-                v.push_back(1);
-            }
-            else if (i == 1)
-            {
-                v.push_back(1);
-                v.push_back(1);
-            }
-            else
-            {
-                for (int j = 0; j <= i; j++)
-                {
-                    if (j == 0 || j == i)
+               if(i == 0)
+               {
+                   v.push_back(1);
+               }
+               else if(i== 1)
+               {
+                   v.push_back(1);
+                   v.push_back(1);
+               }
+               else
+               {
+                    for(int j = 0 ; j <= i ;j++)
                     {
-                        v.push_back(1);
-                        continue;
+                        if(j == 0 || j == i)
+                        {
+                            v.push_back(1);
+                            continue;
+                        }
+    
+                        v.push_back(ans[i-1][j-1] + ans[i-1][j]);
                     }
-
-                    int sum = ans[i - 1][j - 1] + ans[i - 1][j];
-                    v.push_back(sum);
-                }
-            }
+               }
 
             ans.push_back(v);
+
         }
 
         return ans;

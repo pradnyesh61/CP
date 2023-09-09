043507e20c93d9fdfccb69a5fda4d30e3743ba72
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-09-09 22:23:29 +0530

    9 September 2023

--- LeetCode/377_Combination_Sum_IV.cpp
+++ LeetCode/377_Combination_Sum_IV.cpp
@@ -3,89 +3,86 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-
-    int combo(int ind, vector<int> & nums,int target,set<vector<int>>& ans,vector<int> v,
-    vector<int>& dp)
+    int combo(int ind, vector<int> &nums, int target, set<vector<int>> &ans, vector<int> v,
+              vector<int> &dp)
     {
-        if(target == 0)
+        if (target == 0)
         {
             return 1;
         }
-        if(target < 0 || ind >= nums.size())
+        if (target < 0 || ind >= nums.size())
         {
             return 0;
         }
 
-        if(dp[target] !=-1)
+        if (dp[target] != -1)
         {
             return dp[target];
         }
-    
+
         int sum = 0;
-        for(int j =ind; j < nums.size() ; j++)
+        for (int j = ind; j < nums.size(); j++)
+        {
             {
+                swap(nums[j], nums[ind]);
+
+                for (int i = ind; i < nums.size(); i++)
                 {
-                    swap(nums[j],nums[ind]);
-                    
-                    for(int i = ind ; i < nums.size() ; i++)
+                    if (target > 0)
                     {
-                        if(target > 0)
-                        {
-                            v.push_back(nums[i]);
-                            sum += combo(i,nums,target-nums[i],ans,v,dp);
-                            v.pop_back();
-                        }
+                        v.push_back(nums[i]);
+                        sum += combo(i, nums, target - nums[i], ans, v, dp);
+                        v.pop_back();
                     }
-                    swap(nums[j],nums[ind]);
                 }
+                swap(nums[j], nums[ind]);
             }
-           // cout<<sum<<endl;
-            return dp[target] =  sum;
+        }
+        // cout<<sum<<endl;
+        return dp[target] = sum;
     }
 
-
-    int combinationSum4(vector<int>& nums, int target) {
-    
-        vector<int> dp(target+1, -1);
-        return helper(nums,target,dp); 
-    }
-
-    int helper(vector<int> nums, int target,vector<int>& dp)
+    int combinationSum4(vector<int> &nums, int target)
     {
-        if(target == 0)
+
+        vector<int> dp(target + 1, -1);
+        return helper(nums, target, dp);
+    }
+
+    int helper(vector<int> nums, int target, vector<int> &dp)
+    {
+        if (target == 0)
         {
             return 1;
         }
 
-        if(dp[target] != -1)
+        if (dp[target] != -1)
         {
             return dp[target];
         }
 
         dp[target] = 0;
 
-        for(auto it : nums)
+        for (auto it : nums)
         {
-            if(it <= target)
+            if (it <= target)
             {
-                dp[target] += helper(nums,target-it,dp);
+                dp[target] += helper(nums, target - it, dp);
             }
         }
 
         return dp[target];
-
     }
 
-    int combinationSum(vector<int>& nums, int target) {
-        
+    int combinationSum(vector<int> &nums, int target)
+    {
+
         vector<int> v;
         set<vector<int>> ans;
-        vector<int> dp(target+1, -1);
-        return combo(0,nums,target,ans,v,dp);
-       
+        vector<int> dp(target + 1, -1);
+        return combo(0, nums, target, ans, v, dp);
     }
-
-
 };

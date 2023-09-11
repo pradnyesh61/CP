6443d89fc8077f50bf4eed2549457d8ce3b7ff0c
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-09-11 17:58:04 +0530

    11 September 2023

--- LeetCode/1282_Group_the_People_Given_the_Group_Size_They_Belong_To.cpp
+++ LeetCode/1282_Group_the_People_Given_the_Group_Size_They_Belong_To.cpp
@@ -3,37 +3,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+    vector<vector<int>> groupThePeople(vector<int> &groupSizes)
+    {
         vector<vector<int>> ans;
-        map<int,vector<int>> m;
+        map<int, vector<int>> m;
 
-        for(int i = 0; i < groupSizes.size() ; i++)
+        for (int i = 0; i < groupSizes.size(); i++)
         {
             m[groupSizes[i]].push_back(i);
         }
 
-        for(auto it : m)
+        for (auto it : m)
         {
             int k = it.second.size();
             int j = 0;
 
-            while(j < k)
+            while (j < k)
             {
                 int t = it.first;
                 vector<int> v;
-               
-                while(t--)
+
+                while (t--)
                 {
                     v.push_back(it.second[j]);
                     j++;
                 }
-                
+
                 ans.push_back(v);
             }
-           
         }
 
         return ans;

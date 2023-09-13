36bcec3e76c759a458f743d0a7913d6ee0057f28
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-09-13 23:44:06 +0530

    13 September 2023

--- LeetCode/162_Find_Peak_Element.cpp
+++ LeetCode/162_Find_Peak_Element.cpp
@@ -3,16 +3,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int findPeakElement(vector<int>& nums) {
-        
-        int maxi = INT_MIN;
-        int ind = 0 ;
+    int findPeakElement(vector<int> &nums)
+    {
 
-        for(int i = 0; i < nums.size();i++)
+        int maxi = INT_MIN;
+        int ind = 0;
+
+        for (int i = 0; i < nums.size(); i++)
         {
-            if(maxi < nums[i])
+            if (maxi < nums[i])
             {
                 maxi = nums[i];
                 ind = i;

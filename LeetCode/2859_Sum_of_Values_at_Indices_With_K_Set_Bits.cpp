8b85aad20d597dffb2283bf0ebd98f4063adb714
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-09-17 17:11:53 +0530

    17 September 2023

--- LeetCode/2859_Sum_of_Values_at_Indices_With_K_Set_Bits.cpp
+++ LeetCode/2859_Sum_of_Values_at_Indices_With_K_Set_Bits.cpp
@@ -3,32 +3,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int sumIndicesWithKSetBits(vector<int>& nums, int k) {
-        
+    int sumIndicesWithKSetBits(vector<int> &nums, int k)
+    {
+
         int sum = 0;
-        for(int i = 0 ; i < nums.size(); i++)
+        for (int i = 0; i < nums.size(); i++)
         {
             int bits = 0;
 
             int n = i;
 
-            while(n)
+            while (n)
             {
-                if(n%2 == 1)
+                if (n % 2 == 1)
                 {
                     bits++;
                 }
 
-                n = n/2;
+                n = n / 2;
             }
 
-            if(bits == k)
+            if (bits == k)
             {
                 sum += nums[i];
             }
-
         }
         return sum;
     }

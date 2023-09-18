3e7089b6b87e6fe9dc6ee1f1c7af644286a41f8b
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-09-18 21:43:22 +0530

    18 September 2023

--- LeetCode/1337_The_K_Weakest_Rows_in_a_Matrix.cpp
+++ LeetCode/1337_The_K_Weakest_Rows_in_a_Matrix.cpp
@@ -3,37 +3,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+    vector<int> kWeakestRows(vector<vector<int>> &mat, int k)
+    {
         vector<int> v(mat.size());
 
-        for(int i = 0 ; i < mat.size();i++)
+        for (int i = 0; i < mat.size(); i++)
         {
-            for(auto it : mat[i]){
-                if(it)
+            for (auto it : mat[i])
+            {
+                if (it)
                 {
                     v[i]++;
                 }
             }
         }
 
-        set<pair<int,int>> pq;
+        set<pair<int, int>> pq;
 
-        for(int i = 0; i < v.size(); i++)
+        for (int i = 0; i < v.size(); i++)
         {
-            pq.insert({v[i],i});
+            pq.insert({v[i], i});
         }
 
         vector<int> ans;
 
-       for(auto it : pq)
-       {
-           if(!k--)
-               break;
-           ans.push_back(it.second);
-
-       }
+        for (auto it : pq)
+        {
+            if (!k--)
+                break;
+            ans.push_back(it.second);
+        }
 
         return ans;
     }

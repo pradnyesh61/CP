0361358401fc49cf6edf84398b3afaead9da10b6
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-09-24 23:42:23 +0530

    24 September 2023

---
+++ LeetCode/2864_Maximum_Odd_Binary_Number.cpp
@@ -0,0 +1,38 @@
+// 24 September 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        
+        int count = 0;
+
+        for(int i = 0; i < s.size();i++)
+        {
+            if(s[i] == '1')
+            {
+                count++;
+            }
+
+            s[i] = '0';
+        }
+
+        s[s.size()-1] = '1';
+
+        if(count == 1)
+        {
+            return s;
+        }
+
+        count--;
+
+        for(int i = 0; i < count;i++)
+        {
+            s[i] = '1';
+        }
+
+        return s;
+    }
+};

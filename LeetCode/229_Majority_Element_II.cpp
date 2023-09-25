6fa9f17b888bf01658548b9c2b63dd5ff0b952a8
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-09-26 04:23:11 +0530

    26 September 2023

--- LeetCode/229_Majority_Element_II.cpp
+++ LeetCode/229_Majority_Element_II.cpp
@@ -3,31 +3,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-    vector<int> majorityElement(vector<int>& nums) {
-        
+    vector<int> majorityElement(vector<int> &nums)
+    {
 
-        map<int,int> m;
+        map<int, int> m;
 
-        for(auto it:nums)
+        for (auto it : nums)
         {
             m[it]++;
         }
 
         vector<int> ans;
 
-        int n = (nums.size()/3);
+        int n = (nums.size() / 3);
 
-        for(auto it:m)
+        for (auto it : m)
         {
-            if(it.second > n)
+            if (it.second > n)
             {
                 ans.push_back(it.first);
             }
         }
-        
+
         return ans;
     }
 };

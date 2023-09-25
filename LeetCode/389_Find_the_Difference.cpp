a33c823eb1bde8c215581218cf3d51c8c4db7bbf
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-09-25 19:53:30 +0530

    25 September 2023

--- LeetCode/389_Find_the_Difference.cpp
+++ LeetCode/389_Find_the_Difference.cpp
@@ -3,38 +3,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-   /* 
-    char findTheDifference(string s, string t) {
-        
-          sort(t.begin(),t.end());
-            sort(s.begin(),s.end());
-            
-            for(int i = 0 ; i < s.length() ; i++)
-            {
-                
-                if(s[i] != t[i])
-                {
-                    return t[i];
-                }
-            }
-        
-        
-        return t[s.length()];
-        
-        
-    }*/
-    
-     
-    char findTheDifference(string s, string t) {
-           
-            for(int i = 0 ; i < s.length() ; i++)
-            {
-                t[i+1] += t[i] - s[i];
-            }
-        
-        return t[t.length()-1];
+    /*
+     char findTheDifference(string s, string t) {
+
+           sort(t.begin(),t.end());
+             sort(s.begin(),s.end());
+
+             for(int i = 0 ; i < s.length() ; i++)
+             {
+
+                 if(s[i] != t[i])
+                 {
+                     return t[i];
+                 }
+             }
+
+
+         return t[s.length()];
+
+
+     }*/
+
+    char findTheDifference(string s, string t)
+    {
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            t[i + 1] += t[i] - s[i];
+        }
+
+        return t[t.length() - 1];
     }
 };

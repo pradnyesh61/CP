951240b12c93d383cd6677497fcbf0308d930420
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-09-25 19:53:06 +0530

    25 September 2023

---
+++ LeetCode/389_Find_the_Difference.cpp
@@ -0,0 +1,40 @@
+// 25 September 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+   /* 
+    char findTheDifference(string s, string t) {
+        
+          sort(t.begin(),t.end());
+            sort(s.begin(),s.end());
+            
+            for(int i = 0 ; i < s.length() ; i++)
+            {
+                
+                if(s[i] != t[i])
+                {
+                    return t[i];
+                }
+            }
+        
+        
+        return t[s.length()];
+        
+        
+    }*/
+    
+     
+    char findTheDifference(string s, string t) {
+           
+            for(int i = 0 ; i < s.length() ; i++)
+            {
+                t[i+1] += t[i] - s[i];
+            }
+        
+        return t[t.length()-1];
+    }
+};

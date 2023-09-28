c2bc522c2baf8680661abf775dc8b53d6d831923
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-09-28 23:02:01 +0530

    28 September 2023

---
+++ LeetCode/1748_Sum_of_Unique_Elements.cpp
@@ -0,0 +1,29 @@
+// 28 September 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        
+        map<int,int> m;
+        for(auto it : nums)
+        {
+            m[it]++;
+        }
+
+        int sum = 0;
+
+        for(auto it : m)
+        {
+            if(it.second == 1)
+            {
+                sum += it.first;
+            }
+        }
+
+        return sum;
+    }
+};

dd88c6d542f7ae6358ddd434e12f463a64d19230
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-09-30 23:14:11 +0530

    30 September 2023

---
+++ LeetCode/1481_Least_Number_of_Unique_Integers_after_K_Removals.cpp
@@ -0,0 +1,50 @@
+// 30 September 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+
+
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        
+        map<int,int> m;
+
+        for(auto it: arr)
+        {
+            m[it]++;
+        }
+
+        vector<int> v;
+
+        for(auto it : m)
+        {
+            v.push_back(it.second);
+        }
+
+        sort(v.begin(),v.end());
+
+        for(int i = 0 ; i < v.size() ; i++)
+        {
+            while(v[i] > 0 && k > 0)
+            {
+                v[i]--;
+                k--;
+            }
+        }
+
+        int count = 0;
+
+        for(auto it : v)
+        {
+            if(it != 0)
+            {
+                count++;
+            }
+        }
+ 
+        return count;
+
+    }
+};

776d278fb331d949022f20197849c34ba848f3b7
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-10-01 23:53:54 +0530

    1 October 2023

--- LeetCode/557_Reverse_Words_in_a_String_III.cpp
+++ LeetCode/557_Reverse_Words_in_a_String_III.cpp
@@ -3,19 +3,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-    string reverseWords(string s) {
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] != ' ') {  
+    string reverseWords(string s)
+    {
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] != ' ')
+            {
                 int j = i;
-                for (; j < s.length() && s[j] != ' '; j++) { } 
+                for (; j < s.length() && s[j] != ' '; j++)
+                {
+                }
                 reverse(s.begin() + i, s.begin() + j);
                 i = j - 1;
             }
         }
-        
+
         return s;
     }
 };

4ddbfe878d0c5fe3af6959cbec74c06b5d07694d
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-10-02 22:44:52 +0530

    2 October 2023

--- LeetCode/2038_Remove_Colored_Pieces_if_Both_Neighbors_are_the_Same_Color.cpp
+++ LeetCode/2038_Remove_Colored_Pieces_if_Both_Neighbors_are_the_Same_Color.cpp
@@ -3,77 +3,77 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-
- bool winnerOfGame(string colors) {
-
-     int count = 0;
-
-    int n = colors.size();
-    int a = 0;
-    int b = 0;
-
-     for(int i = 1 ; i < colors.size() ; i++)
+    bool winnerOfGame(string colors)
     {
-            if(colors[i-1] == colors[i] && colors[i+1]== colors[i])
+
+        int count = 0;
+
+        int n = colors.size();
+        int a = 0;
+        int b = 0;
+
+        for (int i = 1; i < colors.size(); i++)
+        {
+            if (colors[i - 1] == colors[i] && colors[i + 1] == colors[i])
             {
-                if(colors[i] == 'A')
+                if (colors[i] == 'A')
                 {
                     a++;
                 }
-                else{
+                else
+                {
                     b++;
                 }
             }
+        }
+
+        return a > b;
     }
 
-    return a > b;
+    /*
+      bool winnerOfGame1(string colors) {
 
- }
-
-/*
-  bool winnerOfGame1(string colors) {
-        
-        bool alice = true;
-        while(true)
-        {
-           if(alice)
+            bool alice = true;
+            while(true)
             {
-                for(int j = 1 ;j < colors.size()-1; j++)
+               if(alice)
                 {
-                    if(colors[j-1] == 'A' && colors[j] == 'A' && colors[j+1] == 'A')
+                    for(int j = 1 ;j < colors.size()-1; j++)
                     {
-                        alice = false;
-                        colors.erase(colors.begin()+j);
-                        break;
+                        if(colors[j-1] == 'A' && colors[j] == 'A' && colors[j+1] == 'A')
+                        {
+                            alice = false;
+                            colors.erase(colors.begin()+j);
+                            break;
+                        }
+                    }
+                    if(alice)
+                    {
+                        return false;
                     }
                 }
-                if(alice)
+                else
                 {
-                    return false;
-                }
-            }
-            else
-            {
-                 for(int j = 1 ;j < colors.size()-1; j++)
-                {
-                    if(colors[j-1] == 'B' && colors[j] == 'B' && colors[j+1] == 'B')
+                     for(int j = 1 ;j < colors.size()-1; j++)
                     {
-                        alice = true;
-                        
-                        colors.erase(colors.begin()+j);
-                        break;
-                    }
-                }
+                        if(colors[j-1] == 'B' && colors[j] == 'B' && colors[j+1] == 'B')
+                        {
+                            alice = true;
 
-                if(alice == false)
-                {
-                    return true;
+                            colors.erase(colors.begin()+j);
+                            break;
+                        }
+                    }
+
+                    if(alice == false)
+                    {
+                        return true;
+                    }
                 }
             }
         }
-    }
-    */
-  
+        */
 };

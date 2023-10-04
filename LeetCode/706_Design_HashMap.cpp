ca9a19debe35fe578246ab501e9896c226666d28
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-10-04 18:37:50 +0530

    4 October 2023

--- LeetCode/706_Design_HashMap.cpp
+++ LeetCode/706_Design_HashMap.cpp
@@ -3,48 +3,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class MyHashMap {
+class MyHashMap
+{
 public:
-
     vector<vector<int>> v;
 
-    MyHashMap() {
-        
+    MyHashMap()
+    {
     }
-    
-    void put(int key, int value) {
-       for(int i = 0 ; i< v.size(); i++)
-       {
-           if(v[i][0] == key)
-           {
-               v[i][1] = value;
-               return;
-           }
-       }
-       
-       vector<int> v1(2);
-       v1[0] = key;
-       v1[1] = value;
-       v.push_back(v1);
-       
+
+    void put(int key, int value)
+    {
+        for (int i = 0; i < v.size(); i++)
+        {
+            if (v[i][0] == key)
+            {
+                v[i][1] = value;
+                return;
+            }
+        }
+
+        vector<int> v1(2);
+        v1[0] = key;
+        v1[1] = value;
+        v.push_back(v1);
     }
-    
-    int get(int key) {
-        for(auto it : v){
-            if(it[0] == key)
+
+    int get(int key)
+    {
+        for (auto it : v)
+        {
+            if (it[0] == key)
             {
                 return it[1];
             }
         }
         return -1;
     }
-    
-    void remove(int key) {
-        for(int i = 0 ;i < v.size() ;i++)
+
+    void remove(int key)
+    {
+        for (int i = 0; i < v.size(); i++)
         {
-            if(v[i][0] == key)
+            if (v[i][0] == key)
             {
-                v.erase(v.begin()+i);
+                v.erase(v.begin() + i);
             }
         }
     }

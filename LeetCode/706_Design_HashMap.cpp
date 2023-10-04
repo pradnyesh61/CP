deff06baf9c2ee249c60e4de431bc6cb9053e606
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-10-04 18:34:56 +0530

    4 October 2023

---
+++ LeetCode/706_Design_HashMap.cpp
@@ -0,0 +1,59 @@
+// 4 October 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyHashMap {
+public:
+
+    vector<vector<int>> v;
+
+    MyHashMap() {
+        
+    }
+    
+    void put(int key, int value) {
+       for(int i = 0 ; i< v.size(); i++)
+       {
+           if(v[i][0] == key)
+           {
+               v[i][1] = value;
+               return;
+           }
+       }
+       
+       vector<int> v1(2);
+       v1[0] = key;
+       v1[1] = value;
+       v.push_back(v1);
+       
+    }
+    
+    int get(int key) {
+        for(auto it : v){
+            if(it[0] == key)
+            {
+                return it[1];
+            }
+        }
+        return -1;
+    }
+    
+    void remove(int key) {
+        for(int i = 0 ;i < v.size() ;i++)
+        {
+            if(v[i][0] == key)
+            {
+                v.erase(v.begin()+i);
+            }
+        }
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */

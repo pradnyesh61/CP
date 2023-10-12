82d118066dfd9f653b1654e80282854739aeda35
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-10-12 23:34:01 +0530

    12 October 2023

--- LeetCode/929_Unique_Email_Addresses.cpp
+++ LeetCode/929_Unique_Email_Addresses.cpp
@@ -3,10 +3,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
- class Solution {
+class Solution
+{
 public:
     // int numUniqueEmails1(vector<string>& emails) {
-        
+
     //     set<string> st;
 
     //     for(it : emails)
@@ -18,23 +19,27 @@ public:
     //         for(int i = 0 ; i < s.size() ; i++)
     //         {
     //             if(s[i] == '')
-    //         } 
+    //         }
     //     }
     // }
 
-    int numUniqueEmails(vector<string>& emails) {
-    unordered_set<string> st;
-    for(string &email : emails) {
-        string cleanEmail;
-        for(char c : email) {
-            if(c == '+' || c == '@') break;
-            if(c == '.') continue;
-            cleanEmail += c;
+    int numUniqueEmails(vector<string> &emails)
+    {
+        unordered_set<string> st;
+        for (string &email : emails)
+        {
+            string cleanEmail;
+            for (char c : email)
+            {
+                if (c == '+' || c == '@')
+                    break;
+                if (c == '.')
+                    continue;
+                cleanEmail += c;
+            }
+            cleanEmail += email.substr(email.find('@'));
+            st.insert(cleanEmail);
         }
-        cleanEmail += email.substr(email.find('@'));
-        st.insert(cleanEmail);
+        return st.size();
     }
-    return st.size();
-}
 };
-

1a79699b3df532bae1f1a34b3aa553616d747095
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-10-13 11:24:49 +0530

    13 October 2023

--- LeetCode/2807_Insert_Greatest_Common_Divisors_in_Linked_List.cpp
+++ LeetCode/2807_Insert_Greatest_Common_Divisors_in_Linked_List.cpp
@@ -3,35 +3,35 @@
 #include <bits/stdc++.h>
 using namespace std
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
+    /**
+     * Definition for singly-linked list.
+     * struct ListNode {
+     *     int val;
+     *     ListNode *next;
+     *     ListNode() : val(0), next(nullptr) {}
+     *     ListNode(int x) : val(x), next(nullptr) {}
+     *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+     * };
+     */
+    class Solution
+{
 public:
-    ListNode* insertGreatestCommonDivisors(ListNode* head) {
-        
-        if(head == NULL || head->next == NULL)
+    ListNode *insertGreatestCommonDivisors(ListNode *head)
+    {
+
+        if (head == NULL || head->next == NULL)
         {
             return head;
         }
 
-    
+        ListNode *curr = head;
+        ListNode *temp = head->next;
 
-        ListNode* curr = head;
-        ListNode* temp = head->next;
-
-        while(temp != NULL)
+        while (temp != NULL)
         {
-           int a = curr->val;
-           int b = temp->val;
-           int ans = gcd(a,b);
+            int a = curr->val;
+            int b = temp->val;
+            int ans = gcd(a, b);
 
             curr->next = new ListNode(ans);
             curr = curr->next;

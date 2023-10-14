6a9fb4d913ee9b0fcf00c75626057ef08cd022e9
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-10-14 23:37:58 +0530

    14 October 2023

--- LeetCode/2899_Last_Visited_Integers.cpp
+++ LeetCode/2899_Last_Visited_Integers.cpp
@@ -3,9 +3,11 @@
 #include <bits/stdc++.h>
 using namespace std
 
-class Solution {
+    class Solution
+{
 public:
-    vector<int> lastVisitedIntegers(vector<string>& words) {
+    vector<int> lastVisitedIntegers(vector<string> &words)
+    {
         vector<int> nums;
         vector<int> nums_reverse;
 
@@ -13,38 +15,37 @@ public:
 
         int count = 0;
 
-        for(auto it : words)
+        for (auto it : words)
         {
-           
-          if(it == "prev")
+
+            if (it == "prev")
             {
                 count++;
-                if(count > nums.size())
+                if (count > nums.size())
                 {
                     ans.push_back(-1);
                 }
-                else{
+                else
+                {
                     nums_reverse = nums;
-                    reverse(nums_reverse.begin(),nums_reverse.end());
-                    
-                    cout<<endl;
-                    for(auto i : nums_reverse)
-                    {
-                        cout<<i<<" ";
-                    }
-                    cout<<endl;
-                    ans.push_back(nums_reverse[count-1]);
-                }
+                    reverse(nums_reverse.begin(), nums_reverse.end());
 
+                    cout << endl;
+                    for (auto i : nums_reverse)
+                    {
+                        cout << i << " ";
+                    }
+                    cout << endl;
+                    ans.push_back(nums_reverse[count - 1]);
+                }
             }
-            else{
+            else
+            {
                 count = 0;
                 nums.push_back(stoi(it));
             }
-            
         }
 
         return ans;
-
     }
 };

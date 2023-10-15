38a165b305c427123efbb9668f192a5bdad3b21f
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-10-15 23:16:00 +0530

    15 October 2023

---
+++ LeetCode/2903_Find_Indices_With_Index_and_Value_Difference_I.cpp
@@ -0,0 +1,25 @@
+// 15 October 2023
+
+#include <bits/stdc++.h>
+using namespace std
+
+class Solution {
+public:
+    vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
+
+        int n = nums.size();
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            for(int j = 0 ; j < n; j++)
+            {
+                if(abs(i-j) >= indexDifference && abs(nums[i]-nums[j] >= valueDifference))
+                {
+                    return {i,j};
+                }
+            }
+        }
+        
+        return {-1,-1};
+    }
+};

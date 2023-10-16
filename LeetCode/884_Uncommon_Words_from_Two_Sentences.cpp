c058731ce7e4c0bc70ba501b07024cf62640f63d
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-10-16 23:24:52 +0530

    16 October 2023

--- LeetCode/884_Uncommon_Words_from_Two_Sentences.cpp
+++ LeetCode/884_Uncommon_Words_from_Two_Sentences.cpp
@@ -3,39 +3,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-
-    map<string,int> m;
+    map<string, int> m;
 
     void words(string s)
     {
         string temp = "";
         s.push_back(' ');
 
-        for(auto it : s)
+        for (auto it : s)
         {
-            if(it == ' ')
+            if (it == ' ')
             {
                 m[temp]++;
                 temp = "";
             }
-            else{
+            else
+            {
                 temp += it;
             }
         }
     }
 
-    vector<string> uncommonFromSentences(string s1, string s2) {
+    vector<string> uncommonFromSentences(string s1, string s2)
+    {
         words(s1);
         words(s2);
 
         vector<string> ans;
 
-        for(auto it: m)
+        for (auto it : m)
         {
-            if(it.second == 1)
+            if (it.second == 1)
             {
                 ans.push_back(it.first);
             }

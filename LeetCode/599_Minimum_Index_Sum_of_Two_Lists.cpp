f49c1e98aa28b4a96809e3325d011d71ea8abab2
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-10-17 22:45:48 +0530

    17 October 2023

---
+++ LeetCode/599_Minimum_Index_Sum_of_Two_Lists.cpp
@@ -0,0 +1,43 @@
+// 17 October 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+
+     map<string,int> m;
+
+     int mini = INT_MAX;
+
+     for(int i = 0; i < list1.size(); i++)
+     {
+         for(int j = 0; j < list2.size() ;j++)
+         {
+             if(list1[i] == list2[j])
+             {
+                 if(mini > i+j)
+                 {
+                     mini = i+j;
+                 }
+
+                 m[list1[i]] = i+j;
+             }
+         }
+     }   
+
+    vector<string> v;
+
+    for(auto it : m)
+    {
+        if(it.second == mini)
+        {
+            v.push_back(it.first);
+        }
+    }
+
+    return v;
+
+    }
+};

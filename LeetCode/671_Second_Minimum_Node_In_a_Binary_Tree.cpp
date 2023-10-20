18214e7f36bc9fbbdaa09579a2e33eb9253647c0
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-10-21 00:01:24 +0530

    20 October 2023

--- LeetCode/671_Second_Minimum_Node_In_a_Binary_Tree.cpp
+++ LeetCode/671_Second_Minimum_Node_In_a_Binary_Tree.cpp
@@ -46,4 +46,51 @@ void traverse(TreeNode* root)
         return *s[s.begin()+1];
 
     }
+};/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+set<int> v;
+
+void traverse(TreeNode* root)
+{
+    if(root == NULL)
+    {
+        return ;
+    }
+
+    cout<<root->val<<endl;
+
+    v.insert(root->val);
+    traverse(root->left);
+    traverse(root->right);
+    return;
+}
+
+
+    int findSecondMinimumValue(TreeNode* root) {
+       traverse(root); 
+        
+        for(auto it : v)
+{
+    cout<<it<<" ";
+}
+        if(v.size() < 2)
+        return -1;
+
+  auto it = v.begin();
+  it++;
+        return *it;
+
+    }
 };

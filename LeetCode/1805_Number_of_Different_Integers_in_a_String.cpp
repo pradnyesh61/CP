a57907d6767511841f5c9fcd530ea58e6ff0c001
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-10-21 23:03:33 +0530

    21 October 2023

--- LeetCode/1805_Number_of_Different_Integers_in_a_String.cpp
+++ LeetCode/1805_Number_of_Different_Integers_in_a_String.cpp
@@ -3,30 +3,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int numDifferentIntegers(string word) {
-        
+    int numDifferentIntegers(string word)
+    {
+
         set<string> s;
 
         string t = "";
 
         word.push_back('a');
 
-       for(int i = 0; i < word.size() ;i++)
+        for (int i = 0; i < word.size(); i++)
         {
-            if(word[i] >= '0' && word[i] <= '9')
+            if (word[i] >= '0' && word[i] <= '9')
             {
                 t += word[i];
             }
-            else{
-                if(t != ""){
-                
-                const regex pattern("^0+(?!$)");
-                string str = regex_replace(t, pattern, "");
+            else
+            {
+                if (t != "")
+                {
 
-                s.insert(str);
-                t = "";
+                    const regex pattern("^0+(?!$)");
+                    string str = regex_replace(t, pattern, "");
+
+                    s.insert(str);
+                    t = "";
                 }
             }
         }

381a9491c4d383a89ec5baa3ea6565c4e2c36455
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-10-23 20:42:35 +0530

    23 October 2023

---
+++ LeetCode/342_Power_of_Four.cpp
@@ -0,0 +1,23 @@
+// 23 October 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        
+        if(n > 0)
+        {
+            int k = log2(n)/2;
+            
+            if(k*2 == log2(n))
+            {
+                return true;
+            }
+        }
+        
+       return false; 
+       
+    }
+};

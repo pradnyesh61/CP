3553796145a8abcb5ad06adf52e965921a5b6f3f
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-10-24 20:56:32 +0530

    24 October 2023

---
+++ LeetCode/515_Find_Largest_Value_in_Each_Tree_Row.cpp
@@ -0,0 +1,63 @@
+// 24 October 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+/**
+ * 
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        
+        vector<int> ans;
+
+        if(root ==NULL)
+        {
+            return ans;
+        }
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty())
+        {
+            int sz = q.size();
+            int maxi = INT_MIN;
+
+            for(int i = 0 ; i < sz; i++)
+            {
+                TreeNode* temp = q.front();
+                q.pop();
+
+                if(maxi < temp->val)
+                {
+                    maxi = temp->val;
+                }
+
+                if(temp->left != NULL)
+                {
+                    q.push(temp->left);
+                }
+
+                if(temp->right != NULL)
+                {
+                    q.push(temp->right);
+                }
+            }
+
+            ans.push_back(maxi);
+        }
+
+        return ans;
+
+    }
+};

7254e598d19dfde3263f6fdcdaaeccec39de19b6
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-10-31 22:04:18 +0530

    31 October 2023

---
+++ LeetCode/2433_Find_The_Original_Array_of_Prefix_Xor.cpp
@@ -0,0 +1,24 @@
+// 31 October 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+
+        vector<int> ans;       
+
+        int x = pref[0];
+        ans.push_back(x);
+
+        for(int i = 1; i < pref.size();i++)
+        {
+            ans.push_back(pref[i-1]^pref[i]);
+        }
+
+        return ans;
+
+
+    }
+};

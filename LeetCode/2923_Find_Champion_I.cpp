4c5633905466c65fc89807f6cf7c5e37619b0cd8
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-11-09 23:11:01 +0530

    11 November 2023

--- LeetCode/2923_Find_Champion_I.cpp
+++ LeetCode/2923_Find_Champion_I.cpp
@@ -3,26 +3,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-    int findChampion(vector<vector<int>>& grid) {
-        
-        int id =0;
+    int findChampion(vector<vector<int>> &grid)
+    {
+
+        int id = 0;
         int ct = 0;
 
-        for(int i = 0; i < grid.size();i++)
+        for (int i = 0; i < grid.size(); i++)
         {
             int count = 0;
-            for(int j = 0 ; j < grid[i].size(); j++)
+            for (int j = 0; j < grid[i].size(); j++)
             {
-                if(i != j && grid[i][j] ==1)
+                if (i != j && grid[i][j] == 1)
                 {
                     count++;
                 }
             }
 
-            if(count > ct)
+            if (count > ct)
             {
                 ct = count;
                 id = i;

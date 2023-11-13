88ecbf795d3f83f4a8c502c69dd8e9d2722e4898
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-11-13 22:05:53 +0530

    13 November 2023

--- LeetCode/2785_Sort_Vowels_ina_String_Solution.cpp
+++ LeetCode/2785_Sort_Vowels_ina_String_Solution.cpp
@@ -3,51 +3,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-
-    string sortVowels(string s) {
-        
-       
+    string sortVowels(string s)
+    {
 
         vector<char> Cap;
         vector<char> small;
 
-        for(auto it : s)
+        for (auto it : s)
         {
-            if(it == 'A' || it == 'E' || it == 'I' || it == 'O' || it == 'U')
+            if (it == 'A' || it == 'E' || it == 'I' || it == 'O' || it == 'U')
             {
                 Cap.push_back(it);
             }
-            else if(it == 'a' || it == 'e' || it == 'i' ||it == 'o' ||it == 'u')
+            else if (it == 'a' || it == 'e' || it == 'i' || it == 'o' || it == 'u')
             {
                 small.push_back(it);
             }
         }
 
-        sort(Cap.begin(),Cap.end());
-        sort(small.begin(),small.end());
+        sort(Cap.begin(), Cap.end());
+        sort(small.begin(), small.end());
 
-        for(auto it : small)
+        for (auto it : small)
         {
             Cap.push_back(it);
         }
 
         int j = 0;
 
-        for(int i = 0; i < s.size(); i++)
+        for (int i = 0; i < s.size(); i++)
         {
-        if(s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] ==  'U' || s[i] == 'a' || s[i] == 'e' || s[i] == 'i' ||s[i] == 'o' ||s[i] == 'u')
+            if (s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' || s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
             {
-                if(j < Cap.size())
+                if (j < Cap.size())
                 {
                     s[i] = Cap[j];
                     j++;
-                }       
+                }
             }
         }
-
-
 
         return s;
     }

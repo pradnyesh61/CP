243c684da3d6244224e379d489b30a263a03d850
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-11-15 23:27:10 +0530

    15 November 2023

---
+++ LeetCode/1846_Maximum_Element_After_Decreasing_and_Rearranging.cpp
@@ -0,0 +1,27 @@
+// 15 November 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+
+        sort(arr.begin(),arr.end());
+
+       
+        arr[0] = 1;
+        
+
+        for(int i = 1 ; i < arr.size() ;i++)
+        {
+            if(abs(arr[i]-arr[i-1]) > 1)
+            {
+                arr[i] = arr[i-1]+1;
+            }
+        }
+
+        return arr[arr.size()-1];
+    }
+};

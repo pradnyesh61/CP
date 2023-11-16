f9929f239dfd6a10925949e5fc44eaafb628ef7a
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-11-16 21:30:49 +0530

    16 November 2023

--- LeetCode/1980_Find_Unique_Binary_String.cpp
+++ LeetCode/1980_Find_Unique_Binary_String.cpp
@@ -3,42 +3,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-
-void subset(int i, int n,string s,string nums,vector<string> st,string &ans, int leng,bool &check)
+    void subset(int i, int n, string s, string nums, vector<string> st, string &ans, int leng, bool &check)
     {
-        if(i >= n)
+        if (i >= n)
         {
-            if(s.length() == leng)
+            if (s.length() == leng)
             {
-                if(find(st.begin(),st.end(),s) == st.end())
+                if (find(st.begin(), st.end(), s) == st.end())
                 {
                     check = true;
                     ans = s;
-                }   
+                }
             }
 
             return;
         }
 
-        if(check)
+        if (check)
         {
             return;
         }
 
         s += nums[i];
-        subset(i+1,n,s,nums,st,ans,leng,check);
+        subset(i + 1, n, s, nums, st, ans, leng, check);
         s.pop_back();
-        subset(i+1,n,s,nums,st,ans,leng,check);
-        
+        subset(i + 1, n, s, nums, st, ans, leng, check);
     }
 
-    string findDifferentBinaryString1(vector<string>& nums) {
-        
-        if(nums.size() == 1 )
+    string findDifferentBinaryString1(vector<string> &nums)
+    {
+
+        if (nums.size() == 1)
         {
-            if(nums[0] == "1")
+            if (nums[0] == "1")
             {
                 return "0";
             }
@@ -48,65 +48,66 @@ void subset(int i, int n,string s,string nums,vector<string> st,string &ans, int
         string ans;
         string num = "";
 
-       for(int i = 0 ; i< nums.size() ; i++)
+        for (int i = 0; i < nums.size(); i++)
         {
             num += nums[i];
         }
 
-        string s="";
+        string s = "";
         bool check = false;
 
-        subset(0,num.length(),s,num,nums,ans,nums[0].length(),check);
+        subset(0, num.length(), s, num, nums, ans, nums[0].length(), check);
 
-        
-       if(ans == "")
-       {
-        reverse(num.begin(), num.end());
-        subset(0,num.length(),s,num,nums,ans,nums[0].length(),check);
-       }
+        if (ans == "")
+        {
+            reverse(num.begin(), num.end());
+            subset(0, num.length(), s, num, nums, ans, nums[0].length(), check);
+        }
         return ans;
     }
 
     string changechar(int i, string ans, vector<string> &nums)
     {
-        if(i == nums.size())
+        if (i == nums.size())
         {
-            if(find(nums.begin(),nums.end(),ans) == nums.end())
+            if (find(nums.begin(), nums.end(), ans) == nums.end())
             {
                 return ans;
             }
-            
+
             return "";
         }
 
-        if(find(nums.begin(),nums.end(),ans) == nums.end())
+        if (find(nums.begin(), nums.end(), ans) == nums.end())
         {
             return ans;
         }
 
         string s = ans;
         s[i] = '1';
-       string a = changechar(i+1,s,nums);
-        if(a != "")
+        string a = changechar(i + 1, s, nums);
+        if (a != "")
         {
             return a;
         }
-        cout<<"a = "<<a<<endl;
-       string b = changechar(i,ans,nums);
-        if(b != "")
+        cout << "a = " << a << endl;
+        string b = changechar(i, ans, nums);
+        if (b != "")
         {
             return b;
         }
-        cout<<"b = "<<b<<endl<<endl;
+        cout << "b = " << b << endl
+             << endl;
 
         return "";
     }
 
-     string findDifferentBinaryString(vector<string>& nums) {
-        
-        if(nums.size() == 1 )
+    string findDifferentBinaryString(vector<string> &nums)
+    {
+
+        if (nums.size() == 1)
         {
-            if(nums[0] == "1")
+            if (nums[0] == "1")
             {
                 return "0";
             }
@@ -116,12 +117,11 @@ void subset(int i, int n,string s,string nums,vector<string> st,string &ans, int
         string ans;
         string num = "";
 
-       for(int i = 0 ; i< nums.size() ; i++)
+        for (int i = 0; i < nums.size(); i++)
         {
             num += '0';
         }
 
-        return changechar(0,num,nums);
-
+        return changechar(0, num, nums);
     }
 };

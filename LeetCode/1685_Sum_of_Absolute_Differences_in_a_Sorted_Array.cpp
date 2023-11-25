977ee14176ae57e19b6938cd6623824080260885
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-11-25 22:38:28 +0530

    25 Novermber 2023

--- LeetCode/1685_Sum_of_Absolute_Differences_in_a_Sorted_Array.cpp
+++ LeetCode/1685_Sum_of_Absolute_Differences_in_a_Sorted_Array.cpp
@@ -3,36 +3,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
-
+class Solution
+{
 
 public:
-
-  vector<int> g1etSumAbsoluteDifferences(vector<int>& nums) {
+    vector<int> g1etSumAbsoluteDifferences(vector<int> &nums)
+    {
 
         vector<int> ans;
 
         int n = nums.size();
 
-        for(int i= 0 ; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
             int sum = 0;
-            for(int j = 0; j < n; j++)
+            for (int j = 0; j < n; j++)
             {
-                int k = nums[i]-nums[j];
+                int k = nums[i] - nums[j];
                 sum = sum + abs(k);
             }
-            
-            ans.push_back(sum);
 
+            ans.push_back(sum);
         }
         return ans;
     }
 
-
     // vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
-        
+
     //     int sum = 0;
 
     //     for(auto it : nums)
@@ -52,28 +49,30 @@ public:
     //     return ans;
     // }
 
-  
-    vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
+    vector<int> getSumAbsoluteDifferences(vector<int> &nums)
+    {
         int n = nums.size();
         vector<int> prefix = {nums[0]};
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n; i++)
+        {
             prefix.push_back(prefix[i - 1] + nums[i]);
         }
-        
+
         vector<int> ans;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             int leftSum = prefix[i] - nums[i];
             int rightSum = prefix[n - 1] - prefix[i];
-            
+
             int leftCount = i;
             int rightCount = n - 1 - i;
-            
+
             int leftTotal = leftCount * nums[i] - leftSum;
             int rightTotal = rightSum - rightCount * nums[i];
-            
+
             ans.push_back(leftTotal + rightTotal);
         }
-        
+
         return ans;
     }
 };

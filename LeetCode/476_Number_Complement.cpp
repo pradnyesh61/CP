69810db18e8ee3ad30e00852b186fefe8189a102
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-11-28 23:13:17 +0530

    28 November 2023

--- LeetCode/476_Number_Complement.cpp
+++ LeetCode/476_Number_Complement.cpp
@@ -3,25 +3,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int findComplement(int num) {
-        
+    int findComplement(int num)
+    {
+
         string s = "";
 
-        while(num)
+        while (num)
         {
-           int rem = num%2;
-            s += rem+'0';
-            num /=2;
+            int rem = num % 2;
+            s += rem + '0';
+            num /= 2;
         }
 
-        reverse(s.begin(),s.end());
-  cout<<s<<endl;
-      
-        for(int i = 0; i < s.size(); i++)
+        reverse(s.begin(), s.end());
+        cout << s << endl;
+
+        for (int i = 0; i < s.size(); i++)
         {
-            if(s[i] == '0')
+            if (s[i] == '0')
             {
                 s[i] = '1';
             }
@@ -33,10 +35,10 @@ public:
 
         int i = 0;
 
-        for(; i < s.size(); i++)
+        for (; i < s.size(); i++)
         {
-            cout<<s[i]<<" -- ";
-            if(s[i] != '0')
+            cout << s[i] << " -- ";
+            if (s[i] != '0')
             {
                 i--;
                 break;
@@ -46,22 +48,23 @@ public:
         int n = 0;
         int j = 0;
 
-        cout<<s.size()-1<<" > "<<i<<endl;
+        cout << s.size() - 1 << " > " << i << endl;
 
-        for(int k = s.size()-1 ; k > i; k--)
+        for (int k = s.size() - 1; k > i; k--)
         {
-            cout<<"k ="<<k<<"  s[k]="<<s[k]<<"  j= "<<j;
-            int p = pow(2,j);
-            cout<<" p="<<p;
-            if(s[k] != '0')
+            cout << "k =" << k << "  s[k]=" << s[k] << "  j= " << j;
+            int p = pow(2, j);
+            cout << " p=" << p;
+            if (s[k] != '0')
             {
-                n = n +( 1 * p);
+                n = n + (1 * p);
             }
-            else{
-                 n = n +( 0 * p);
+            else
+            {
+                n = n + (0 * p);
             }
 
-cout<<" n ="<<n<<endl;
+            cout << " n =" << n << endl;
             j++;
         }
 

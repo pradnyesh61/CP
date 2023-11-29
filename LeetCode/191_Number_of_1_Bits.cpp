dab3fad190a494646df84e006ae33282285ab3a1
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-11-29 22:57:55 +0530

    29 November 2023

---
+++ LeetCode/191_Number_of_1_Bits.cpp
@@ -0,0 +1,26 @@
+// 29 November 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        
+        int count = 0;
+
+        while(n)
+        {
+            if(n % 2)
+            {
+                count++;
+            }
+            
+            n /= 2;
+            
+        }
+
+        return count;
+    }
+};
+

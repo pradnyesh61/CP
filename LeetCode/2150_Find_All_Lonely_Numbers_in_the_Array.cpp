145d8d20757643738b3eba6e4d9dfec893544560
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-12-03 22:38:59 +0530

    3 Decemeber 2023

--- LeetCode/2150_Find_All_Lonely_Numbers_in_the_Array.cpp
+++ LeetCode/2150_Find_All_Lonely_Numbers_in_the_Array.cpp
@@ -3,24 +3,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-
+class Solution
+{
 
 public:
-    vector<int> findLonely(vector<int>& nums) {
-        
-        unordered_map<int,int> numbers;
+    vector<int> findLonely(vector<int> &nums)
+    {
 
-        for(auto it : nums)
+        unordered_map<int, int> numbers;
+
+        for (auto it : nums)
         {
             numbers[it]++;
         }
 
         vector<int> ans;
 
-        for(auto it : nums)
+        for (auto it : nums)
         {
-            if( !numbers[it-1] && !numbers[it+1] && numbers[it] == 1)
+            if (!numbers[it - 1] && !numbers[it + 1] && numbers[it] == 1)
             {
                 ans.push_back(it);
             }

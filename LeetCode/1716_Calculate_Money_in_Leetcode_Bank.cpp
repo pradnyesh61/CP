0226d813a23adb0442826b848545bf7ad8aea6c9
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-12-06 23:04:37 +0530

    6 December 2023

--- LeetCode/1716_Calculate_Money_in_Leetcode_Bank.cpp
+++ LeetCode/1716_Calculate_Money_in_Leetcode_Bank.cpp
@@ -3,30 +3,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-    int totalMoney(int n) {
-        
+    int totalMoney(int n)
+    {
+
         int day = 0;
         long count = 0;
         long monday = 1;
         int mon = 1;
 
-        while(n--)
+        while (n--)
         {
-            if(day == 7)
+            if (day == 7)
             {
                 mon++;
-                monday  = mon;
+                monday = mon;
                 day = 0;
-                
             }
             count += monday;
-             cout<<monday<<endl;   
+            cout << monday << endl;
             monday++;
             day++;
-        
         }
 
         return count;

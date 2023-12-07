f595b6c64b949f05bd68541c256edc421c07b72f
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-12-07 12:34:37 +0530

    7 December 2023

---
+++ LeetCode/1903_Largest_Odd_Number_in_String.cpp
@@ -0,0 +1,68 @@
+// 7 December 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    // string largestOddNumber(string num) {
+        
+    //     long long n = stold(num);
+        
+    //    long long largest = INT_MIN;
+        
+
+    //     while(n)
+    //     {
+    //         if(n%2 != 0)
+    //         {
+    //             if(largest < n){
+    //                 largest = n;
+    //                 break;
+    //             }
+    //         }
+    //         else{
+                
+    //             int rem = n%10;
+    //             if(rem%2 != 0 && largest < rem)
+    //             {
+    //                 largest = rem;
+    //             }     
+    //         }
+
+    //         n = n/10;
+
+    //     }
+
+    //     if(largest == INT_MIN)
+    //     {
+    //         return "";
+    //     }
+
+    //     num ="";
+    //     num  = to_string(largest);
+
+    //     return num;
+
+    // }
+
+      string largestOddNumber(string num) {
+        
+        int largest = INT_MIN;
+
+        for(int i = num.size()-1; i>=0 ; i--)
+        {
+            int k = num[i]-'0';
+            
+            if(k%2 != 0 && largest < k)
+            {
+                return num.substr(0,i+1);
+            }
+           
+        }
+
+        return "";
+
+    }
+};

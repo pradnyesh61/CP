8f11fb31fb5ec1c145fc0b63fd995881a44a7703
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-12-12 19:00:15 +0530

    12 December 2023

---
+++ LeetCode/1464_Maximum_Product_of_Two_Elements_in_an_Array.cpp
@@ -0,0 +1,17 @@
+// 12 December 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        sort(nums.begin(),nums.end());
+
+        return (nums[n-1]-1)*(nums[n-2]-1);
+    }
+};

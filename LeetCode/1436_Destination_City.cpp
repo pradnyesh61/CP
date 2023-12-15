359252b862e09511550a1e4d04f010f15717217c
pradnyesh61 <53476709+pradnyesh61@users.noreply.github.com>
2023-12-15 18:04:06 +0530

    15 December 2023

--- LeetCode/1436_Destination_City.cpp
+++ LeetCode/1436_Destination_City.cpp
@@ -3,24 +3,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    string destCity(vector<vector<string>>& paths) {
-        
+    string destCity(vector<vector<string>> &paths)
+    {
+
         set<string> city;
 
-        map<string,int> m;
+        map<string, int> m;
 
-        for(int i =0 ; i < paths.size();i++)
+        for (int i = 0; i < paths.size(); i++)
         {
             city.insert(paths[i][0]);
             city.insert(paths[i][1]);
-            m[paths[i][0]]++;  
+            m[paths[i][0]]++;
         }
 
-        for(auto it : city)
+        for (auto it : city)
         {
-            if(!m[it])
+            if (!m[it])
             {
                 return it;
             }
